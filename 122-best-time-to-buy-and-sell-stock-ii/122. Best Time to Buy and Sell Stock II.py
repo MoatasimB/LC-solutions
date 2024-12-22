0d40e19250e6be8283d295a83e967ebba859c54b
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2024-12-21 19:18:54 -0500

    Runtime: 3 ms (39.21%), Memory: 18.7 MB (10.02%) - Gitcode

--- 122-best-time-to-buy-and-sell-stock-ii/122. Best Time to Buy and Sell Stock II.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122. Best Time to Buy and Sell Stock II.py
@@ -1,21 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = {}
-        def dfs(i, holding):
-            if (i,holding) in dp:
-                return dp[(i, holding)]
-            if i == len(prices):
-                return 0
-            
-            ans = 0
-            ans += dfs(i+1, holding)
-            if holding:
-                ans= max(ans, prices[i] + dfs(i+1, False))
-            else:
-                ans = max(ans, -prices[i] + dfs(i+1, True))
-            
-            dp[(i,holding)] = ans
-            return ans
-        
-        return dfs(0,False)
+        ans = 0
 
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                ans += prices[i] - prices[i-1]
+        
+        return ans
