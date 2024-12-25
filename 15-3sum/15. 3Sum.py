08b405cdb2f66711135a65e4bb20dcd1baddffd8
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2024-12-24 21:14:40 -0500

    Runtime: 586 ms (55.56%), Memory: 20.8 MB (14.03%) - Gitcode

---
+++ 15-3sum/15. 3Sum.py
@@ -0,0 +1,33 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        nums.sort()
+        n = len(nums)
+        i = 0
+        ans = []
+        def tSum(left, right, num):
+            while left < right:
+                if num + nums[left] + nums[right] > 0:
+                    right -= 1
+                elif num + nums[left] + nums[right] < 0:
+                    left +=1
+                else:
+                    ans.append([num, nums[left], nums[right]])
+                    right -=1
+                    left +=1
+                    while left < right and nums[left] == nums[left-1]:
+                        left +=1
+
+
+        while i < n:
+            curr = nums[i]
+            i+=1
+            tSum(i, n-1, curr)
+            while i < len(nums) - 1 and nums[i] == nums[i-1]:
+                i+=1
+        
+        return ans
+
+
+        
+ 
