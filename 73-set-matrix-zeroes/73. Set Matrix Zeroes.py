908fafd75a55c0ff6e195405ab8708349799b991
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2024-12-26 22:51:54 -0500

    Runtime: 7 ms (22.87%), Memory: 18.6 MB (7.51%) - Gitcode

--- 73-set-matrix-zeroes/73. Set Matrix Zeroes.py
+++ 73-set-matrix-zeroes/73. Set Matrix Zeroes.py
@@ -3,32 +3,18 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-
+        
+        rows = set()
+        cols = set()
         m = len(matrix)
         n = len(matrix[0])
-
-        col0 = 1
-
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
-                    if j == 0:
-                        col0 = 0
-                        matrix[i][0] = 0
-                    else:
-                        matrix[0][j] = 0
-                        matrix[i][0] = 0
-        x = matrix[0][0]
-        for i in range(1,m):
-            for j in range(1,n):
-                if matrix[i][0]==0 or matrix[0][j]==0:
+                    rows.add(i)
+                    cols.add(j)
+        
+        for i in range(m):
+            for j in range(n):
+                if i in rows or j in cols:
                     matrix[i][j] = 0
-
-        if col0 == 0:
-            for i in range(m):
-                matrix[i][0] = 0
-        
-        if x == 0:
-            for i in range(n):
-                matrix[0][i] = 0
-        
