2203d7d1a670b97e785192a92e894cf98bff0e2a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2024-12-27 22:51:45 -0500

    Runtime: 19 ms (37.80%), Memory: 22.9 MB (6.04%) - Gitcode

--- 49-group-anagrams/49. Group Anagrams.py
+++ 49-group-anagrams/49. Group Anagrams.py
@@ -1,20 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
+        mpp = defaultdict(list)
 
-        for s in strs:
-            count = [0] * 26 #placeholders for all letters
-            for ch in s:
-                count[ord(ch) - ord("a")] +=1 #creates a unique list w freq of all the characters in the word
-            res[tuple(count)].append(s) #we use that unique list as a key because all anagrams will have the same unique list and then we append that word
-        return res.values()
-
-
-        # from collections import defaultdict
-        anagrams = defaultdict(list)
-
-        for word in strs:
-            key = "".join(sorted(word))
-            anagrams[key].append(word)
-
-        return anagrams.values()
+        for i in range(len(strs)):
+            key = [0] * 26
+            for ch in strs[i]:
+                key[ord(ch) - ord('a')] +=1
+            
+            mpp[tuple(key)].append(strs[i])
+        ans = [val for val in mpp.values()]
+        return ans
