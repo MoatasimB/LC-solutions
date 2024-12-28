e9366fb7da6366fc0b1336f4c4ef857894a1d05a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2024-12-27 22:34:45 -0500

    Runtime: 6 ms (64.83%), Memory: 18.1 MB (8.98%) - Gitcode

--- 205-isomorphic-strings/205. Isomorphic Strings.py
+++ 205-isomorphic-strings/205. Isomorphic Strings.py
@@ -1,17 +1,20 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mapping_s_t = {}
-        mapping_t_s = {}
+        
+        def transform(x):
 
-        for i in range(len(s)):
-
-            sch = s[i]
-            tch = t[i]
-
-            if s[i] not in mapping_s_t and t[i] not in mapping_t_s:
-                mapping_s_t[s[i]] = t[i]
-                mapping_t_s[t[i]] = s[i]
-            else:
-                if mapping_s_t.get(s[i]) != t[i] or mapping_t_s.get(t[i]) != s[i]:
-                    return False
-        return True
+            mpp = {}
+            id = 0
+            ans = []
+            for ch in x:
+                if ch not in mpp:
+                    mpp[ch] = str(id)
+                    ans.append(mpp[ch])
+                    id +=1
+                else:
+                    ans.append(mpp[ch])
+            # print(ans)
+            
+            return " ".join(ans)
+        
+        return transform(s) == transform(t)
