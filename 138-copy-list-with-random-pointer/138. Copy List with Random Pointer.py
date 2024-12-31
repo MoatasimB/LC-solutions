d0c1b1e568abebbe0545f831694537c4ed1b6c32
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2024-12-30 23:55:48 -0500

    Runtime: 45 ms (15.98%), Memory: 18.8 MB (7.49%) - Gitcode

---
+++ 138-copy-list-with-random-pointer/138. Copy List with Random Pointer.py
@@ -0,0 +1,38 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        
+        nodes = {}
+        # nodes[None] = None
+
+        dummy = head
+        n = Node(-1,None)
+        new = n
+
+        while dummy:
+            NewNode = Node(dummy.val)
+            new.next = NewNode
+            nodes[dummy] = NewNode
+            new = new.next
+            dummy = dummy.next
+        
+        dummy = head
+        new = n.next
+
+        while dummy:
+            if dummy.random in nodes:
+                new.random = nodes[dummy.random]
+            else:
+                new.random = None
+            dummy = dummy.next
+            new = new.next
+        
+        return n.next
