3ae45f82f9aa81104be82fe9289acba4d316ee76
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2024-12-31 23:59:24 -0500

    Runtime: 34 ms (0.38%), Memory: 16.5 MB (96.68%) - Gitcode

--- 100-same-tree/100. Same Tree.py
+++ 100-same-tree/100. Same Tree.py
@@ -7,22 +7,18 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         
-        def dfs(rootX, rootY):
-
-            if rootX and not rootY:
-                return False
-            if rootY and not rootX:
-                return False
-            
-            if not rootX and not rootY:
+        def dfs(root1, root2):
+            if not root1 and not root2:
                 return True
-            
-            if rootX.val != rootY.val:
+            if (root1 and not root2) or (not root1 and root2) or (root1.val != root2.val):
                 return False
-
-            left = dfs(rootX.left, rootY.left)
-            right = dfs(rootX.right, rootY.right)
+            
+            left = dfs(root1.left, root2.left)
+            right = dfs(root1.right, root2.right)
 
             return left and right
-        
+
         return dfs(p,q)
+
+            
+            
