790f66571b1921c5f043ffc610bb5c8f55125f60
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-02 23:50:31 -0500

    Runtime: 247 ms (54.05%), Memory: 19.2 MB (76.92%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,30 +1,34 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        m = len(grid)
-        n = len(grid[0])
-        
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        dirs = [(0,1), (0,-1), (1,0), (-1,0)]
+        seen = [[False] * COLS for _ in range(ROWS)]
         def valid(r,c):
-            return 0<=r<m and 0<=c<n
-
-        dirs = [(0,1), (1,0), (0,-1), (-1,0)]
-
+            return 0<=r<ROWS and 0<=c<COLS
+        
         def dfs(r,c):
-
             for dx, dy in dirs:
                 new_r = r + dx
                 new_c = c + dy
 
-                if valid(new_r,new_c) and grid[new_r][new_c] == "1":
-                    grid[new_r][new_c] = "0"
-                    dfs(new_r,new_c)
+                if valid(new_r, new_c) and grid[new_r][new_c] == "1" and not seen[new_r][new_c]:
+                    seen[new_r][new_c] = True
+                    dfs(new_r, new_c)
         
-        count = 0
 
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
-                    grid[i][j] == "0"
-                    count +=1
+        ans = 0
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == "1" and not seen[i][j]:
+                    ans +=1
+                    seen[i][j] = True
                     dfs(i,j)
-        return count
+        
+        return ans
+            
+
+
+            
