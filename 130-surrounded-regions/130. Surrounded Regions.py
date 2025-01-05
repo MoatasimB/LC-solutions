d5355cb9c54055bfb2247f153675648a90f7ee47
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-04 23:22:55 -0500

    Runtime: 7 ms (61.15%), Memory: 20.6 MB (95.76%) - Gitcode

--- 130-surrounded-regions/130. Surrounded Regions.py
+++ 130-surrounded-regions/130. Surrounded Regions.py
@@ -5,31 +5,38 @@ class Solution:
         """
         m = len(board)
         n = len(board[0])
-        dirs = [(0,1), (1,0), (-1,0), (0,-1)]
 
+        
         def valid(r,c):
             return 0<=r<m and 0<=c<n
 
+        dirs = [(0,1), (0,-1), (1,0), (-1,0)]
+        
         def dfs(r,c):
-
             for dx, dy in dirs:
-                nr = r + dx
-                nc = c + dy
-
-                if valid(nr,nc) and board[nr][nc] == 'O':
-                    board[nr][nc] = 1
-                    dfs(nr,nc)
+                new_r = r+dx
+                new_c = c+dy
+                if valid(new_r, new_c) and board[new_r][new_c]=='O':
+                    board[new_r][new_c] = 'E'
+                    dfs(new_r, new_c)
+            
+        
+        def border(r,c):
+            return r==0 or r==m-1 or c==0 or c==n-1
+        for i in range(m):
+            for j in range(n):
+                if border(i,j) and board[i][j] == 'O':
+                    board[i][j] = 'E'
+                    dfs(i,j)
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'E':
+                    board[i][j] = 'O'
+                else:
+                    board[i][j] = 'X'
         
 
-        for r in range(m):
-            for c in range(n):
-                if (r == 0 or r == m-1 or c==0 or c==n-1) and board[r][c]=='O':
-                    board[r][c] = 1
-                    dfs(r,c)
-        print(board)
-        for r in range(m):
-            for c in range(n):
-                if board[r][c] == 1:
-                    board[r][c] = 'O'
-                else:
-                    board[r][c] = 'X'
+
+
+
