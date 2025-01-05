0a005c5993eca1071d7f78f68c027ef29fce49d7
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-05 01:14:59 -0500

    Runtime: 4 ms (52.81%), Memory: 19.2 MB (22.29%) - Gitcode

--- 210-course-schedule-ii/210. Course Schedule II.py
+++ 210-course-schedule-ii/210. Course Schedule II.py
@@ -1,29 +1,27 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        indegree = [0] * numCourses
         adj = [[] for _ in range(numCourses)]
 
         for second, first in prerequisites:
+            indegree[second] +=1
             adj[first].append(second)
         
-        ans = []
-        seen = [False for _ in range(numCourses)]
-        cycle = False
-        def dfs(node, currPath): 
-            nonlocal cycle
-            seen[node] = True
-            currPath[node] = True
-            for nei in adj[node]:
-                if not seen[nei]:
-                    dfs(nei,currPath)
-                elif currPath[nei]:
-                    cycle = True
-            
-            currPath[node] = False
-            ans.append(node)
-        
-        for i in range(numCourses):
-            if not seen[i]:
-                dfs(i, [False for _ in range(numCourses)])
-        return ans[::-1] if not cycle else []
+        q = deque()
 
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                q.append(i)
+        ans = []
+        while q:
+            node = q.popleft()
+
+            ans.append(node)
             
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        
+        return ans if len(ans) == numCourses else []
