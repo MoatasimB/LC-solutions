c7bd8dd2292d15233d53dd4ea77d60fc4d912363
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-05 00:44:16 -0500

    Runtime: 2 ms (91.39%), Memory: 19.1 MB (21.56%) - Gitcode

---
+++ 207-course-schedule/207. Course Schedule.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indegree = [0] * numCourses
+        adj = defaultdict(list)
+        for second, first in prerequisites:
+            indegree[second] +=1
+            adj[first].append(second)
+        
+
+        q = deque()
+    
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        while q:
+            node = q.popleft()
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        print(indegree)
+        return sum(indegree) == 0
+
