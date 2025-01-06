bee69a5963fc45a0e9c7e4946fd7ded74e149553
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-05 23:19:04 -0500

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (6.98%) - Gitcode

---
+++ 17-letter-combinations-of-a-phone-number/17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        mpp = {
+            "2":"abc",
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz",
+        }
+        if not digits:
+            return []
+        ans = []
+        def dfs(i, curr):
+            if i == len(digits):
+                ans.append("".join(curr[:]))
+                return
+            
+            # for j in range(i, len(digits)):
+            for ch in mpp[digits[i]]:
+                curr.append(ch)
+
+                dfs(i+1, curr)
+
+                curr.pop()
+        dfs(0,[])
+        return ans
