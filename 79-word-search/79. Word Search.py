5c045292fddbc3402c47b5a0dff10f105929ecbb
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-07 00:16:58 -0500

    Runtime: 5780 ms (17.83%), Memory: 18.1 MB (6.57%) - Gitcode

--- 79-word-search/79. Word Search.py
+++ 79-word-search/79. Word Search.py
@@ -3,36 +3,31 @@ class Solution:
         
         m = len(board)
         n = len(board[0])
-
+        
         def valid(r,c):
             return 0<=r<m and 0<=c<n
         
-        dirs = [[0,1],[0,-1],[1,0],[-1,0]]
+        dirs = [(0,1), (0,-1), (1,0), (-1,0)]
 
-        def dfs(r,c,i, curr, seen):
-            # print(seen)
-            if "".join(curr) == word:
+        def dfs(r, c, i, seen):
+            if i == len(word):
                 return True
-            
             for dx, dy in dirs:
-                nr,nc = r + dx, c + dy
-
-                if valid(nr,nc) and (nr, nc) not in seen and board[nr][nc] == word[i]:
+                nr = dx + r
+                nc = dy + c
+                if valid(nr,nc) and (nr,nc) not in seen and board[nr][nc] == word[i]:
                     seen.add((nr,nc))
-                    curr.append(board[nr][nc])
-                    if dfs(nr,nc,i+1,curr,seen):
+                    if dfs(nr,nc,i+1, seen):
                         return True
                     
                     seen.remove((nr,nc))
-                    curr.pop()
-            return False
         
+
         for i in range(m):
             for j in range(n):
-                if board[i][j] == word[0] and dfs(i,j,1,[board[i][j]], set([(i,j)])):
-                    return True
+                if board[i][j] == word[0]:
+                    if dfs(i,j,1, set([(i,j)])):
+                        return True
         
         return False
-                    
-
 
