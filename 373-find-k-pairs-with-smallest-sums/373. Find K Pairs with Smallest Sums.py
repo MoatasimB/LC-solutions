f7e506bc159ce058cb47a3317b1e4fcc2a64ff45
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-12 00:30:31 -0500

    Runtime: 104 ms (51.73%), Memory: 34.9 MB (37.43%) - Gitcode

--- 373-find-k-pairs-with-smallest-sums/373. Find K Pairs with Smallest Sums.py
+++ 373-find-k-pairs-with-smallest-sums/373. Find K Pairs with Smallest Sums.py
@@ -1,27 +1,25 @@
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        n = len(nums1)
-        m = len(nums2)
-
-        ans = []
-
-        min_heap = []
+        
+        minH = []
         seen = set()
 
-        heapq.heappush(min_heap, (nums1[0] + nums2[0], 0, 0))
+        heapq.heappush(minH, (nums1[0] + nums2[0], 0,0))
         seen.add((0,0))
-
-        for _ in range(k):
-            _, i, j = heapq.heappop(min_heap)
-
+        
+        ans = []
+        while k:
+            s, i, j = heapq.heappop(minH)
             ans.append([nums1[i], nums2[j]])
 
-            if i+1 < n and (i+1, j) not in seen:
+            if i + 1 < len(nums1) and (i+1, j) not in seen:
+                heapq.heappush(minH, (nums1[i+1] + nums2[j], i+1, j))
                 seen.add((i+1, j))
-                heapq.heappush(min_heap, (nums1[i+1]+ nums2[j], i+1, j))
 
-            if j+1 < m and (i, j+1) not in seen:
+            if j + 1 < len(nums2) and (i, j+1) not in seen:
+                heapq.heappush(minH, (nums1[i] + nums2[j+1], i, j+1))
                 seen.add((i, j+1))
-                heapq.heappush(min_heap, (nums1[i]+ nums2[j+1], i, j+1))
+            
+            k-=1
         
         return ans
