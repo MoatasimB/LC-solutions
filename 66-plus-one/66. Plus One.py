2014bad63b3e887a31abd7424511bf2c28fbc4bc
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-12 18:52:10 -0500

    Runtime: 0 ms (100.00%), Memory: 17.7 MB (34.95%) - Gitcode

--- 66-plus-one/66. Plus One.py
+++ 66-plus-one/66. Plus One.py
@@ -2,20 +2,13 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
 
-        carry = 1
+        for i in range(len(digits) -1 , -1 , -1):
 
-        for i in range(len(digits)-1, -1, -1):
-            digits[i] = digits[i] + carry
-
-            if digits[i] >= 10:
-                digits[i] = digits[i] % 10
+            if digits[i] == 9:
+                digits[i] = 0
             else:
-                carry = 0
-                break
-
-        if digits[0] >= 10 or carry:
-            digits[0] = digits[0] % 10
-            return [1] + digits 
-        else:
-            return digits           
-
+                digits[i] +=1
+                return digits
+            
+        
+        return [1] + digits
