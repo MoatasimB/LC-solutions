c508d4f7b2fafc9832db3c476d398240594da7cb
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-12 18:58:30 -0500

    Runtime: 935 ms (17.71%), Memory: 17.9 MB (14.11%) - Gitcode

--- 69-sqrtx/69. Sqrt(x).py
+++ 69-sqrtx/69. Sqrt(x).py
@@ -1,11 +1,13 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         
-        if x == 0:
-            return 0
-        if x == 1 or x == 2:
-            return 1
-        for i in range(x):
-            if i*i > x:
+        # if x == 0:
+        #     return 0
+        # if x == 1 or x == 2:
+        #     return 1
+        for i in range(x+1):
+            if i*i >= x:
+                if i*i == x:
+                    return i
                 return i-1
-        
+        return 0
