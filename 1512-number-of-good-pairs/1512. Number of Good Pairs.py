d9276d8bb0b0a4bdd18a1957effccf9346363fdd
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-11 22:54:54 -0500

    Runtime: 0 ms (100.00%), Memory: 17.6 MB (38.91%) - Gitcode

--- 1512-number-of-good-pairs/1512. Number of Good Pairs.py
+++ 1512-number-of-good-pairs/1512. Number of Good Pairs.py
@@ -1,17 +1,9 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         
-        count = {}
-
-        for i in range(len(nums)):
-            if nums[i] in count:
-                count[nums[i]] += 1
-            else:
-                count[nums[i]] = 1
+        count = defaultdict(int)
         ans = 0
-        # print(count)
-
-        for k,v in count.items():
-            if v > 1:
-                ans += int((v * (v-1)) / 2)
+        for num in nums:
+            ans += count[num]
+            count[num] += 1
         return ans
