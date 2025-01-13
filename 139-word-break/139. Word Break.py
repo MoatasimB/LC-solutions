3668bf507e0dce3da085ac87d6e43d3091b6b952
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-12 21:22:20 -0500

    Runtime: 6 ms (26.79%), Memory: 18 MB (23.07%) - Gitcode

--- 139-word-break/139. Word Break.py
+++ 139-word-break/139. Word Break.py
@@ -6,15 +6,15 @@ class Solution:
 
             if s[i:] in wordDict:
                 return True
-            # if i == len(s):
-            #     return False
             if i in dp:
                 return dp[i]
+            
             for j in range(i, len(s)):
                 if s[i:j+1] in wordDict:
                     if dfs(j+1):
                         dp[i] = True
                         return True
+            
             dp[i] = False
             return False
         return dfs(0)
