c373eeddbd4b107cec224dac2f812a5e489d4de0
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-12 22:53:17 -0500

    Runtime: 1822 ms (34.66%), Memory: 18.3 MB (13.01%) - Gitcode

--- 300-longest-increasing-subsequence/300. Longest Increasing Subsequence.py
+++ 300-longest-increasing-subsequence/300. Longest Increasing Subsequence.py
@@ -1,25 +1,36 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
-        dp = {}
-        dp[0] = 1
+        # dp = {}
+        # dp[0] = 1
 
-        def dfs(i):
+        # def dfs(i):
 
-            if i == 0:
-                return 1
-            if i in dp:
-                return dp[i]
-            curr = float("-inf")
-            for j in range(i-1, -1, -1):
-                if nums[j] < nums[i]:
-                    curr = max(curr, 1 + dfs(j))
+        #     if i == 0:
+        #         return 1
+        #     if i in dp:
+        #         return dp[i]
+        #     curr = float("-inf")
+        #     for j in range(i-1, -1, -1):
+        #         if nums[j] < nums[i]:
+        #             curr = max(curr, 1 + dfs(j))
             
-            dp[i] = curr if curr != float('-inf') else 1
+        #     dp[i] = curr if curr != float('-inf') else 1
             
-            return dp[i]
+        #     return dp[i]
         
-        for i in range(len(nums)-1, -1,-1):
-            dfs(i)
+        # for i in range(len(nums)-1, -1,-1):
+        #     dfs(i)
 
+        # return max(dp.values())
+
+        dp = {}
+        for i in range(len(nums)):
+            curr = float("-inf")
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    curr = max(curr, 1 + dp[j])
+
+            dp[i] = curr if curr != float('-inf') else 1
+        
         return max(dp.values())
