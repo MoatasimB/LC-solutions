dd6aab96a7806978e3ebe010f2fd0d074116ecef
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-12 22:26:04 -0500

    Runtime: 799 ms (45.66%), Memory: 18.1 MB (56.84%) - Gitcode

--- 322-coin-change/322. Coin Change.py
+++ 322-coin-change/322. Coin Change.py
@@ -1,24 +1,39 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        dp = {}
+        # dp = {}
 
-        def dfs(n):
-            if n == 0:
-                return 0
-            if n < 0:
-                return float('inf')
+        # def dfs(n):
+        #     if n == 0:
+        #         return 0
+        #     # if n < 0:
+        #     #     return float('inf')
             
-            if n in dp:
-                return dp[n]
+        #     if n in dp:
+        #         return dp[n]
             
-            curr = float('inf')
-            for c in coins:
-                curr = min(curr, 1 + dfs(n-c))
+        #     curr = float('inf')
+        #     for c in coins:
+        #         if n - c >= 0:
+        #             curr = min(curr, 1 + dfs(n-c))
             
-            dp[n] = curr
+        #     dp[n] = curr
 
-            return dp[n]
+        #     return dp[n]
         
-        x = dfs(amount)
-        return x if x != float('inf') else -1
+        # x = dfs(amount)
+        # return x if x != float('inf') else -1
+
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, amount + 1):
+
+            for c in coins:
+                if i - c >= 0:
+                    dp[i] = min(dp[i],  1 + dp[i-c])
+
+        return dp[amount] if dp[amount] != float('inf') else -1
+
+
+        # 0 1 2 3 4 5 6 7 8 9 10 11
+        # 0 1
