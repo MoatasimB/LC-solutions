8add1a9692425b2aa2e4d17bdd8392556e91a832
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-12 23:07:53 -0500

    Runtime: 30 ms (24.67%), Memory: 21.1 MB (17.17%) - Gitcode

--- 64-minimum-path-sum/64. Minimum Path Sum.py
+++ 64-minimum-path-sum/64. Minimum Path Sum.py
@@ -1,18 +1,34 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        dp = {}
-        def dfs(i,j):
-            if (i,j) in dp:
-                return dp[(i,j)]
-            if i==0 and j==0:
-                return grid[i][j]
-            if i < 0 or j < 0:
-                return float('inf')
+        # dp = {}
+        # def dfs(i,j):
+        #     if (i,j) in dp:
+        #         return dp[(i,j)]
+        #     if i==0 and j==0:
+        #         return grid[i][j]
+        #     if i < 0 or j < 0:
+        #         return float('inf')
             
-            left = grid[i][j] + dfs(i, j-1)
-            up = grid[i][j] + dfs(i-1, j)
+        #     left = grid[i][j] + dfs(i, j-1)
+        #     up = grid[i][j] + dfs(i-1, j)
 
-            dp[(i,j)] = min(left, up)
-            return dp[(i,j)]
+        #     dp[(i,j)] = min(left, up)
+        #     return dp[(i,j)]
         
-        return dfs(len(grid) - 1, len(grid[0]) - 1)
+        # return dfs(len(grid) - 1, len(grid[0]) - 1)
+
+        dp = [[float('inf')] * len(grid[0]) for _ in range(len(grid))]
+        dp[0][0] = grid[0][0]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i == 0 and j == 0:
+                    continue
+                left = float('inf')
+                up = float('inf')
+                if j > 0:
+                    left = dp[i][j-1]
+                if i > 0:
+                    up = dp[i-1][j]
+                dp[i][j] = grid[i][j] + min(up, left)
+        print(dp)
+        return dp[len(grid) - 1][len(grid[0])-1]
