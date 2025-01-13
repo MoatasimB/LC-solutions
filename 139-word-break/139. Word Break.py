5145248c4899f008d538ae53fb746d4cc8299942
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-12 21:21:50 -0500

    Runtime: 7 ms (23.53%), Memory: 17.9 MB (31.84%) - Gitcode

---
+++ 139-word-break/139. Word Break.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordDict = set(wordDict)
+        dp = {}
+        def dfs(i):
+
+            if s[i:] in wordDict:
+                return True
+            if i == len(s):
+                return False
+            if i in dp:
+                return dp[i]
+            for j in range(i, len(s)):
+                if s[i:j+1] in wordDict:
+                    if dfs(j+1):
+                        dp[i] = True
+                        return True
+            dp[i] = False
+            return False
+        return dfs(0)
