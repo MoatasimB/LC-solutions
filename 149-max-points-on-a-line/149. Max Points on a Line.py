7f94050ace466abf47933f386e7383da70d11698
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-12 20:16:41 -0500

    Runtime: 20 ms (93.55%), Memory: 17.9 MB (51.76%) - Gitcode

---
+++ 149-max-points-on-a-line/149. Max Points on a Line.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        
+        ans = 1
+        for i in range(len(points)):
+            # curr = 0 
+            dic = defaultdict(int)
+            for j in range(i + 1, len(points)):
+                curr = 1
+                pointA = points[i]
+                pointB = points[j]
+                if pointA[0] != pointB[0]:
+                    slope = (pointA[1] - pointB[1]) / (pointA[0] - pointB[0])
+                else:
+                    slope = float('inf')
+                if slope not in dic:
+                    curr += 1
+                    dic[slope] = 2
+                    # seen.add(slope)
+                else:
+                    dic[slope] += 1
+                    curr = dic[slope]
+                
+                ans = max(ans, curr)
+        
+        return ans
