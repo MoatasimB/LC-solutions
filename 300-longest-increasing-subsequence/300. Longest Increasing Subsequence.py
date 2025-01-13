3dbf9ac4ce0b9240c72e735776da02b2b4fb676f
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-12 22:46:22 -0500

    Runtime: 2718 ms (8.01%), Memory: 20.5 MB (5.01%) - Gitcode

---
+++ 300-longest-increasing-subsequence/300. Longest Increasing Subsequence.py
@@ -0,0 +1,30 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        dp = {}
+
+        def dfs(i):
+
+            if i == 0:
+                dp[0] = 1
+                return 1
+            if i in dp:
+                return dp[i]
+            curr = float("-inf")
+            for j in range(i-1, -1, -1):
+                if nums[j] < nums[i]:
+                    curr = max(curr, 1 + dfs(j))
+            
+            if curr != float("-inf"):
+                dp[i] = curr
+            
+            if i in dp:
+                return dp[i]
+            
+            dp[i] = 1
+            return dp[i]
+        
+        for i in range(len(nums)-1, -1,-1):
+            dfs(i)
+
+        return max(dp.values())
