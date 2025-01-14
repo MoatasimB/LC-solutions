57cabc8fdb86a2709a5bdbb9ca6bbf9791ac34d5
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-13 23:27:24 -0500

    Runtime: 3 ms (26.16%), Memory: 18 MB (14.71%) - Gitcode

--- 63-unique-paths-ii/63. Unique Paths II.py
+++ 63-unique-paths-ii/63. Unique Paths II.py
@@ -21,23 +21,29 @@ class Solution:
         # return dfs(len(obstacleGrid)-1, len(obstacleGrid[0]) - 1)
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
-        dp = [[0]*n for _ in range(m)]
-
-        dp[0][0] = 1 if obstacleGrid[0][0] != 1 else 0
-
-        for i in range(m):
+        # dp = [[0]*n for _ in range(m)]
+        
+        
+        prev = [0] * n
+        prev[0] = 1 if obstacleGrid[0][0] != 1 else 0
+        
+        for i in range(1,n):
+            if obstacleGrid[0][i] == 1:
+                continue
+            prev[i] = prev[i-1]
+        
+        for i in range(1,m):
+            curr = [0] * n
             for j in range(n):
                 if obstacleGrid[i][j] == 1:
                     continue
-                if i==0 and j==0:
-                    continue
-                up = 0
-                if i > 0:
-                    up = dp[i-1][j]
+
+         
+                up = prev[j]
                 left = 0
                 if j > 0:
-                    left = dp[i][j-1]
+                    left = curr[j-1]
                 
-                dp[i][j] = up + left
-
-        return dp[m-1][n-1]
+                curr[j] = up + left
+            prev = curr
+        return prev[n-1]
