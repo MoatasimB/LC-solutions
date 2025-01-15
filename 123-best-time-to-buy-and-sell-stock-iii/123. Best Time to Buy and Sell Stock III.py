c7f58af4c24f5a38ac5f14ddd3be04be09dcd50b
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-14 22:58:53 -0500

    Runtime: 247 ms (60.70%), Memory: 32.2 MB (34.16%) - Gitcode

--- 123-best-time-to-buy-and-sell-stock-iii/123. Best Time to Buy and Sell Stock III.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123. Best Time to Buy and Sell Stock III.py
@@ -1,22 +1,31 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = {}
-        def dfs(i, holding, k):
-            if (i,holding,k) in dp:
-                return dp[(i,holding,k)]
-            
-            if k == -1:
-                return 0
-            if i == len(prices):
-                return 0
-            
-            curr = float('-inf')
-            if holding:
-                curr = max(curr, prices[i] + dfs(i+1, False, k), dfs(i+1, True, k))
-            
-            else:
-                curr = max(curr, dfs(i+1, False, k) ,dfs(i+1, True, k-1) - prices[i])
-            dp[(i,holding,k)] = curr
-            return curr
         
-        return dfs(0, False, 2)
+        bestLeft = [0] * len(prices)
+        bestRight = [0] * (len(prices) + 1
+)
+        left = prices[0]
+        for i in range(1, len(bestLeft)):
+            if prices[i] > left:
+                bestLeft[i] = max(bestLeft[i-1], prices[i]-left)
+            else:
+                left = min(left, prices[i])
+                bestLeft[i] = bestLeft[i-1]
+        
+
+        right = prices[-1]
+        for i in range(len(bestRight)-2, -1, -1):
+            if right > prices[i]:
+                bestRight[i] = max(bestRight[i-1], right - prices[i])
+            else:
+                right = max(right, prices[i])
+                bestRight[i] = bestRight[i-1]
+        
+
+        ans = 0
+
+        for i in range(len(bestLeft)):
+            ans = max(ans, bestLeft[i] + bestRight[i+1])
+        
+        return ans
+        
