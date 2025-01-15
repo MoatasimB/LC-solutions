ccabae80318f195cef4c72413881aa19fb7ffc05
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-14 22:24:15 -0500

    Runtime: 49 ms (79.76%), Memory: 17.7 MB (95.87%) - Gitcode

--- 72-edit-distance/72. Edit Distance.py
+++ 72-edit-distance/72. Edit Distance.py
@@ -22,24 +22,35 @@ class Solution:
         # return dfs(len(word1) - 1, len(word2) - 1)
 
 
-        dp = [[0]*(len(word2) + 1) for _ in range(len(word1) + 1)]
+        # dp = [[0]*(len(word2) + 1) for _ in range(len(word1) + 1)]
 
-        for i in range(len(word2) + 1):
-            dp[0][i] = i
+        # for i in range(len(word2) + 1):
+        #     dp[0][i] = i
         
-        for i in range(len(word1) + 1):
-            dp[i][0] = i
+        # for i in range(len(word1) + 1):
+        #     dp[i][0] = i
         
-        # dp[0][0] = 0
         
+        # for i in range(1, len(word1) + 1):
+        #     for j in range(1, len(word2) + 1):
+
+        #         if word1[i-1] == word2[j-1]:
+        #             dp[i][j] = dp[i-1][j-1]
+        #         else:
+        #             dp[i][j] = 1 + min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
+        
+        # return dp[len(word1)][len(word2)]
+
+        prev = [i for i in range(len(word2)+1)]
+
         for i in range(1, len(word1) + 1):
+            curr = [0] * (len(word2) + 1)
+            curr[0] = i
             for j in range(1, len(word2) + 1):
-
                 if word1[i-1] == word2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
+                    curr[j] = prev[j-1]
                 else:
-                    dp[i][j] = 1 + min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
+                    curr[j] = 1 + min(prev[j-1], prev[j], curr[j-1])
+            prev = curr
         
-        return dp[len(word1)][len(word2)]
-
-        prev = [0] * (len(word2) + 1)
+        return prev[len(word2)]
