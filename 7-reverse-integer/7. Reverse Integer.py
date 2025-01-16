38789b70d6d75e0945217462269506e7236b4e31
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-15 23:26:12 -0500

    Runtime: 38 ms (55.52%), Memory: 18 MB (9.09%) - Gitcode

--- 7-reverse-integer/7. Reverse Integer.py
+++ 7-reverse-integer/7. Reverse Integer.py
@@ -1,25 +1,18 @@
 class Solution:
     def reverse(self, x: int) -> int:
         
-        lst = []
-        negative = x < 0
+        sign = -1 if x < 0 else 1
+
+        ans = 0
         x = abs(x)
-        
         while x:
             digit = x % 10
-            x = x // 10
-            lst.append(digit)
-        
-        power = 1
-        ans = 0
-        for i in range(len(lst)-1,-1,-1):
-            if ans + (lst[i] * power) > 2**31:
+
+            ans = ans * 10 + digit
+
+            if ans > (2**31) - 1:
                 return 0
-            ans += lst[i] * power
-            power *=10
+            
+            x = x // 10
         
-        
-        if negative:
-            return -1 * ans
-        
-        return ans
+        return sign * ans
