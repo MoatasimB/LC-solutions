61ec4ce0a9adcec66f295954292d174fdf98539a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-15 22:07:40 -0500

    Runtime: 8 ms (94.66%), Memory: 18.6 MB (45.83%) - Gitcode

--- 55-jump-game/55. Jump Game.py
+++ 55-jump-game/55. Jump Game.py
@@ -1,15 +1,17 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return True
         
         goal = len(nums) - 1
 
         i = len(nums) - 2
 
-        while i >= 0:
-            if nums[i] + i >= goal:
-                print(nums[i], i, goal)
-                goal = i
-            i -= 1
-        
-        return goal <=0
+        while i >=0:
 
+            if nums[i] + i >= goal:
+                goal = i
+            
+            i -=1
+        
+        return goal == 0
