680ac2a1500bb59069371eff67aad8f1c668886a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-15 22:18:13 -0500

    Runtime: 0 ms (100.00%), Memory: 18 MB (5.12%) - Gitcode

--- 62-unique-paths/62. Unique Paths.py
+++ 62-unique-paths/62. Unique Paths.py
@@ -1,6 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
+
+        return math.comb(m+n-2, m-1)
         dp = [[-1]*n for _ in range(m)]
 
         def dfs(i,j):
