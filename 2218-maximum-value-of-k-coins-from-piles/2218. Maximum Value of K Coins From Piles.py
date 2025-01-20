fb82cf8da9288a729e960f80691a7e4e756db75e
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-19 20:47:18 -0500

    Runtime: 5035 ms (28.24%), Memory: 322.3 MB (38.76%) - Gitcode

---
+++ 2218-maximum-value-of-k-coins-from-piles/2218. Maximum Value of K Coins From Piles.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
+        n = len(piles)
+
+        for pile in piles:
+            for j in range(1, len(pile)):
+                pile[j] += pile[j-1]
+
+        dp = {}
+        def dfs(i, k):
+
+            if i >= n:
+                return 0
+            if k <= 0:
+                return 0
+            if (i,k) in dp:
+                return dp[(i,k)]
+            ans = 0
+            for j in range(len(piles[i])):
+                if j + 1 <= k:
+                    ans = max(ans, piles[i][j] + dfs(i+1, k-j-1))
+            ans = max(ans, dfs(i+1, k))
+            dp[(i,k)] = ans
+            return ans
+        
+        return dfs(0,k)
