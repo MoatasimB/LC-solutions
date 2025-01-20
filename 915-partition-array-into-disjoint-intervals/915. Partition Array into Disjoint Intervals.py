e8b51a9968a080970f36fa386261a39b9d1c74f5
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-19 23:17:16 -0500

    Runtime: 134 ms (46.24%), Memory: 29.2 MB (30.82%) - Gitcode

--- 915-partition-array-into-disjoint-intervals/915. Partition Array into Disjoint Intervals.py
+++ 915-partition-array-into-disjoint-intervals/915. Partition Array into Disjoint Intervals.py
@@ -1,16 +1,13 @@
 class Solution:
     def partitionDisjoint(self, nums: List[int]) -> int:
         n = len(nums)
-        leftMax = [nums[0]] * n
         rightMin = [nums[-1]] * n
 
-        for i in range(1, n):
-            leftMax[i] = max(nums[i], leftMax[i-1])
+        
         
         for i in range(n-2, -1, -1):
             rightMin[i] = min(nums[i], rightMin[i+1])
         
-        # print(leftMax, rightMin)
         curr = nums[0]
         for i in range(1, n):
             if curr <= rightMin[i]:
