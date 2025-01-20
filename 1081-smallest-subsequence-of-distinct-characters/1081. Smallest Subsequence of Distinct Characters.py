0575a32713aa5c865cb63fe7961862ad2822d263
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-20 01:00:14 -0500

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (21.55%) - Gitcode

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081. Smallest Subsequence of Distinct Characters.py
@@ -0,0 +1,20 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        
+        lastOcc = {}
+        for i in range(len(s)):
+            lastOcc[s[i]] = i
+        
+        stack = []
+        seen = set()
+
+        for i in range(len(s)):
+            if s[i] not in seen:
+                while stack and stack[-1] >= s[i] and i < lastOcc[stack[-1]]:
+                    x = stack.pop()
+                    seen.remove(x)
+                
+                seen.add(s[i])
+                stack.append(s[i])
+        
+        return "".join(stack)
