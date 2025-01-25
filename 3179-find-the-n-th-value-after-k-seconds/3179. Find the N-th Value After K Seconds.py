db310c93b9d6f570b6427d9d5c5c1643c646c462
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-24 22:10:02 -0500

    Runtime: 36 ms (82.10%), Memory: 16.5 MB (100.00%) - Gitcode

--- 3179-find-the-n-th-value-after-k-seconds/3179. Find the N-th Value After K Seconds.py
+++ 3179-find-the-n-th-value-after-k-seconds/3179. Find the N-th Value After K Seconds.py
@@ -1,10 +1,5 @@
 class Solution:
     def valueAfterKSeconds(self, n: int, k: int) -> int:
-        
-        arr = [1] * n
 
-        for i in range(k):
-            for j in range(1, len(arr)):
-                arr[j] = arr[j-1] + arr[j]
+        return comb(k+n-1,n-1) %1_000_000_007
         
-        return arr[-1] % (10**9 + 7)
