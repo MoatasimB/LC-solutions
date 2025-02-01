65bdd3e4c37060c08795d2a4bd92fa3979f51742
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-01-31 22:58:10 -0500

    Runtime: 75 ms (31.43%), Memory: 33.5 MB (16.62%) - Gitcode

--- 2270-number-of-ways-to-split-array/2270. Number of Ways to Split Array.py
+++ 2270-number-of-ways-to-split-array/2270. Number of Ways to Split Array.py
@@ -5,13 +5,11 @@ class Solution:
 
         for i in range(1, len(nums)):
             pre.append(pre[-1] + nums[i])
-        print(pre)
+
         ans = 0
         for i in range(len(nums) - 1):
             left = pre[i]
-            right = nums[i+1] + pre[-1] - pre[i+1]
-            # right = pre[-1] - left
-            print(left, right)
+            right = pre[-1] - left
             if left >= right:
                 ans +=1
         
