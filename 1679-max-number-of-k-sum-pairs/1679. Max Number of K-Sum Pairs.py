5745e95c0cf6bb224f2cafde512b87da6b77eda7
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-02-13 00:48:30 -0500

    Runtime: 515 ms (37.89%), Memory: 29.7 MB (88.42%) - Gitcode

---
+++ 1679-max-number-of-k-sum-pairs/1679. Max Number of K-Sum Pairs.py
@@ -0,0 +1,57 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        
+        nums.sort()
+        ans = 0
+
+        i = 0
+        j = len(nums) - 1
+
+        while i < j:
+            if nums[i] + nums[j] == k:
+                ans += 1
+                i += 1
+                j -= 1
+                continue
+            elif nums[i] + nums[j] > k:
+                j -= 1
+                continue
+            else:
+                i += 1
+        return ans
+        # seen = [False] * len(nums)
+        # dic = {}
+        # ans = 0
+        # for i in range(len(nums)):
+
+        #     if (k - nums[i]) in dic:
+        #         other = dic[k-nums[i]]
+        #         if not seen[other] and not seen[i]:
+        #             seen[other] = True
+        #             seen[i] = True
+        #             ans += 1
+        #     dic[nums[i]] = i
+        
+        # return ans
+
+        used = set()
+        ans = 0
+        counts = defaultdict(int)
+
+        for num in nums:
+            counts[num] += 1
+        
+
+        for key, val in counts.items():
+
+            if (k - key) in counts and (k - key) not in used:
+                print(key, k-key)
+                if key == k - key:
+                    ans += (val * val - 1) // 2
+                    used.add(key)
+                    continue
+                ans += min(val, counts[k-key])
+                used.add(key)
+                used.add(k-key)
+        
+        return ans if k not in counts else ans - counts[k]
