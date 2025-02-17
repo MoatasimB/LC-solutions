5bcbfefb4636d8e70c2b3fc20217be4f85c95348
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-02-17 02:33:23 -0500

    Runtime: 2814 ms (100.00%), Memory: 47.4 MB (100.00%) - Gitcode

---
+++ 3453-separate-squares-i/3453. Separate Squares I.py
@@ -0,0 +1,41 @@
+class Solution:
+    def separateSquares(self, squares: List[List[int]]) -> float:
+        def check(mid):
+            areaAbove = 0
+            areaBelow = 0
+
+            for x,y,l in squares:
+                top = y + l
+                if top <= mid:
+                    areaBelow += l * l
+                elif y >= mid:
+                    areaAbove += l * l
+                else:
+                    topLength = top - mid
+                    areaAbove += topLength * l
+
+                    bottomLength = mid - y
+                    areaBelow += bottomLength * l
+            print(areaAbove, areaBelow)
+            if areaAbove - areaBelow == 0:
+                return 1
+            if areaAbove > areaBelow:
+                return 2
+            return 3
+        
+        l = 0
+        r = 2 * 10**9
+        ans = float('inf')
+        while r - l > 10**(-5):
+            mid = (l+r) / 2.0
+            print(l, mid, r)
+            x = check(mid)
+            if x == 1:
+                ans = min(ans, mid)
+                r = mid
+            elif x == 2:
+                l = mid
+            else:
+                r = mid
+        
+        return (l + r) / 2
