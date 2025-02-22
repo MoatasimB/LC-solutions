5550f1d86ca4734d2fbd68a6ba19b57491273242
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-02-21 22:48:38 -0500

    Runtime: 568 ms (13.56%), Memory: 39.9 MB (10.69%) - Gitcode

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
@@ -2,22 +2,24 @@ class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         
 
-        s = []
-        l = []
+        minH = []
+        maxH = []
+
+        l = 0
         ans = 0
-        left = 0
         for r in range(len(nums)):
 
-            heapq.heappush(s, (nums[r], r))
-            heapq.heappush(l, (-nums[r], r))
+            heapq.heappush(minH, (nums[r], r))
+            heapq.heappush(maxH, (-nums[r], r))
 
-            while abs(s[0][0] - (-l[0][0])) > limit:
-                print(left)
-                left = min(s[0][1], l[0][1]) + 1
-                while s[0][1] < left:
-                    heapq.heappop(s)
-                while l[0][1] < left:
-                    heapq.heappop(l)
-            
-            ans = max(ans, r - left + 1)
+            while abs(-maxH[0][0] - minH[0][0]) > limit:
+                l = min(maxH[0][1], minH[0][1])
+                while minH[0][1] <= l:
+                    heapq.heappop(minH)
+                while maxH[0][1] <= l:
+                    heapq.heappop(maxH)
+                l += 1
+            ans = max(ans, abs(r - l) + 1)
+        
         return ans
+
