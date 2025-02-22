468e82005d46291b511da25d4c52d006fd2cba26
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-02-21 22:48:49 -0500

    Runtime: 549 ms (16.30%), Memory: 39.6 MB (20.59%) - Gitcode

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
@@ -19,7 +19,7 @@ class Solution:
                 while maxH[0][1] <= l:
                     heapq.heappop(maxH)
                 l += 1
-            ans = max(ans, abs(r - l) + 1)
+            ans = max(ans, r-l + 1)
         
         return ans
 
