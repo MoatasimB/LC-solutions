03545800f29e1e7a5ee4fafd582f359e8d7ce361
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-02-23 00:37:21 -0500

    Runtime: 343 ms (36.05%), Memory: 48.2 MB (93.31%) - Gitcode

--- 1865-finding-pairs-with-a-certain-sum/1865. Finding Pairs With a Certain Sum.py
+++ 1865-finding-pairs-with-a-certain-sum/1865. Finding Pairs With a Certain Sum.py
@@ -3,20 +3,18 @@ class FindSumPairs:
     def __init__(self, nums1: List[int], nums2: List[int]):
         self.n1 = nums1
         self.n2 = nums2
-        self.idx_num = {}
         self.num_counts = defaultdict(int)
 
         for i in range(len(self.n2)):
-            self.idx_num[i] = self.n2[i]
             self.num_counts[self.n2[i]] += 1
 
 
     def add(self, index: int, val: int) -> None:
-        num = self.idx_num[index]
+        num = self.n2[index]
         self.num_counts[num] -= 1
-        # if self.num_counts[num] == 0:
-        #     del self.num_counts[num]
-        self.idx_num[index] = num + val
+        if self.num_counts[num] == 0:
+            del self.num_counts[num]
+        self.n2[index] = num + val
         self.num_counts[num + val] += 1
     
     def count(self, tot: int) -> int:
