aee1321922d47e9f0de75fbc00bfc9a9ca7f3143
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-02-24 00:15:35 -0500

    Runtime: 243 ms (69.73%), Memory: 20.1 MB (82.84%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,72 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        m = len(grid)
-        n = len(grid[0])
-
-        class UnionFind:
-            def __init__(self, size):
-                self.roots = [i for i in range(size)]
-                self.ranks = [0] * size
-                self.islands = 0
-            
-
-            def find(self, x):
-                if self.roots[x] == x:
-                    return x
-                
-                self.roots[x] = self.find(self.roots[x])
-
-                return self.roots[x]
-            
-            def union(self, x,y):
-                rootX = self.find(x)
-                rootY = self.find(y)
-
-                if rootX != rootY:
-
-                    if self.ranks[rootX] < self.ranks[rootY]:
-                        self.roots[rootX] = rootY
-                    elif self.ranks[rootX] > self.ranks[rootY]:
-                        self.roots[rootY] = rootX
-                    else:
-                        self.roots[rootY] = rootX
-                        self.ranks[rootX] +=1
-                
-                    self.islands -=1 
-            
-            def valid(self, r,c):
-                return 0<=r<m and 0<=c<n
-            
-            def addLand(self, r,c):
-                self.islands +=1
-
-                dirs = [(0,1), (0,-1), (1,0), (-1,0)]
-
-                for dx, dy in dirs:
-                    new_r=dx + r
-                    new_c=dy + c
-                    if self.valid(new_r, new_c) and grid[new_r][new_c] == "1":
-                        self.union(r*n + c, new_r*n + new_c)
-            
-            def isConnected(self, x,y):
-                return self.find(x) == self.find(y)
-            
-            def getIslands(self):
-                return self.islands
-    
-        uf = UnionFind(m*n)
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
-                    uf.addLand(i,j)
+        rows = len(grid)
+        cols = len(grid[0])
+        def valid(r,c):
+            return 0<=r<rows and 0<=c<cols
         
-        return uf.getIslands()
+        dirs = [(0,1), (1,0), (0,-1), (-1,0)]
 
+        def dfs(r,c):
+            grid[r][c] = "0"
 
+            for dx, dy in dirs:
+                nr = dx + r
+                nc = dy + c
 
-
-
-
+                if valid(nr,nc) and grid[nr][nc] == "1":
+                    dfs(nr,nc)
         
+        ans = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    dfs(r,c)
+                    ans += 1
+        
+        return ans
