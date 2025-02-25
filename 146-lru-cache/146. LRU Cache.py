dffb8e50a198857410909259e9e7b8b308999f94
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-02-24 20:56:19 -0500

    Runtime: 147 ms (51.35%), Memory: 78.4 MB (55.42%) - Gitcode

--- 146-lru-cache/146. LRU Cache.py
+++ 146-lru-cache/146. LRU Cache.py
@@ -2,8 +2,8 @@ class Node:
     def __init__(self, key, val):
         self.val = val
         self.key = key
-        self.prev = None
         self.next = None
+        self.prev = None
 
 class LRUCache:
 
@@ -11,74 +11,53 @@ class LRUCache:
         self.cap = capacity
         self.head = Node(-1, -1)
         self.tail = Node(-1, -1)
-        self.map = {}
-        self.count = 0
-
+        self.mpp = {} #key,node
         self.head.next = self.tail
         self.tail.prev = self.head
         
 
     def get(self, key: int) -> int:
-        if key not in self.map:
+        if key not in self.mpp:
             return -1
-        
-        node = self.map[key]
-
-        val = node.val
-        
+        node = self.mpp[key]
         self.delete(node)
         self.addFront(node)
+        return node.val
 
-        return val
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.map:
-            nodeToDelete = self.map[key]
-            self.count -=1
-            self.delete(nodeToDelete)
-            del self.map[key] 
+        if key in self.mpp:
+            self.delete(self.mpp[key])
+            del self.mpp[key]
         
         node = Node(key, value)
-
-        self.map[key] = node
-
         self.addFront(node)
 
-        self.count +=1
+        self.mpp[key] = node
+        if len(self.mpp) > self.cap:
+            nodeToDelete = self.tail.prev
+            del self.mpp[nodeToDelete.key]
+            self.delete(nodeToDelete)
 
-        if self.count > self.cap:
-            LRUNODE = self.tail.prev
-
-            del self.map[LRUNODE.key]
-            self.delete(LRUNODE)
-            self.count -= 1
-
-
-
-    
 
     def addFront(self, node):
-
-        nextNode = self.head.next
-
+        headNext = self.head.next
         self.head.next = node
-        node.next = nextNode
-
-        nextNode.prev = node
         node.prev = self.head
+        node.next = headNext
+
+        headNext.prev = node
     
     def delete(self, node):
-
         prevNode = node.prev
         nextNode = node.next
-
         prevNode.next = nextNode
         nextNode.prev = prevNode
-        
 
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
