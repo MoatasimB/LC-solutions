f3d649803196a61cabc7d65af82e2d4973a37c7d
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-02-24 21:49:17 -0500

    Runtime: 3 ms (82.26%), Memory: 18.8 MB (49.20%) - Gitcode

--- 283-move-zeroes/283. Move Zeroes.py
+++ 283-move-zeroes/283. Move Zeroes.py
@@ -3,21 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
-   
-        # r=0
-
-        # for l in range(len(nums)):
-        #     if nums[l] == 0 and l!=len(nums) - 1:
-        #         while nums[r]==0 and r<len(nums) - 1:
-        #             r+=1 
-        #         nums[l], nums[r] = nums[r], nums[l]
-        
         l = 0
 
         for r in range(len(nums)):
             if nums[r] != 0:
                 nums[r], nums[l] = nums[l], nums[r]
-                l+=1
-        
+                l += 1
             
+        
