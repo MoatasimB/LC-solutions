2dc4a9948f8f8bee94b9d852bcc16b8847069ffb
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-02-24 21:56:46 -0500

    Runtime: 38 ms (43.99%), Memory: 34.8 MB (62.72%) - Gitcode

---
+++ 234-palindrome-linked-list/234. Palindrome Linked List.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        dummy = ListNode(-1, head)
+        slow = head
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        if fast:
+            slow = slow.next
+  
+        
+        prev = None
+        while slow:
+            nextNode = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nextNode
+        
+        while prev:
+            if prev.val != head.val:
+                return False
+            head = head.next
+            prev = prev.next
+        
+        return True
+
+        
