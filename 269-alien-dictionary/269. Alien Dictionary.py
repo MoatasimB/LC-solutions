3b2857b16cd590956d48fe6b612f6f792460ee6f
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-02-27 22:50:43 -0500

    Runtime: 0 ms (100.00%), Memory: 16.6 MB (100.00%) - Gitcode

--- 269-alien-dictionary/269. Alien Dictionary.py
+++ 269-alien-dictionary/269. Alien Dictionary.py
@@ -1,39 +1,38 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
+        
+        graph = defaultdict(list)
         inDeg = {c:0 for word in words for c in word}
-        neighbors = {c:[] for word in words for c in word}
+        for i in range(1, len(words)):
+            w1 = words[i-1]
+            w2 = words[i]
 
-        for i in range(len(words) - 1):
-            w1 = words[i]
-            w2 = words[i+1]
             minLen = min(len(w1), len(w2))
 
-            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+            if w1[:minLen] == w2[:minLen] and len(w1) > len(w2):
                 return ""
+            
             for j in range(minLen):
                 if w1[j] != w2[j]:
-                    if w2[j] not in neighbors[w1[j]]:
-                        inDeg[w2[j]] += 1
-                        neighbors[w1[j]].append(w2[j])
+                    graph[w1[j]].append(w2[j])
+                    inDeg[w2[j]] +=1
                     break
-
-            
-        q = deque()
+        print(graph)
         print(inDeg)
-        print(neighbors)
+        q = deque()
 
-        for k, v in inDeg.items():
-            if v == 0:
-                q.append(k)
-
+        for key, val in inDeg.items():
+            if val == 0:
+                q.append(key)
+        
         ans = []
         while q:
             node = q.popleft()
             ans.append(node)
-            for nei in neighbors[node]:
+
+            for nei in graph[node]:
                 inDeg[nei] -= 1
                 if inDeg[nei] == 0:
                     q.append(nei)
-                
-
+        
         return "".join(ans) if sum(inDeg.values()) == 0 else ""
