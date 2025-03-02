38cfe87d6f5ad5e6bf7ea094405f97cf32ba515c
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-01 23:03:48 -0500

    Runtime: 159 ms (38.08%), Memory: 39.5 MB (55.99%) - Gitcode

--- 295-find-median-from-data-stream/295. Find Median from Data Stream.py
+++ 295-find-median-from-data-stream/295. Find Median from Data Stream.py
@@ -1,38 +1,38 @@
 class MedianFinder:
 
     def __init__(self):
-        self.minH = [] #larger elements
-        self.maxH = [] #Smaller elements 
+        self.small = [] #max Heap            2 3  5
+        self.large = [] #min Heap           4 6 7    
+
 
     def addNum(self, num: int) -> None:
-        
-        heapq.heappush(self.maxH, -num)
+        heapq.heappush(self.large, num)
 
-        if len(self.maxH) > len(self.minH) + 1:
-            element = -heapq.heappop(self.maxH)
-            heapq.heappush(self.minH, element)
+        if len(self.large) > len(self.small) + 1:
+            x = -heapq.heappop(self.large)
+            heapq.heappush(self.small, x)
         
-        if self.minH and self.maxH and -self.maxH[0] > self.minH[0]:
-            element = -heapq.heappop(self.maxH)
-            heapq.heappush(self.minH, element)
+        if self.small and -self.small[0] > self.large[0]:
+            x = -heapq.heappop(self.large)
+            heapq.heappush(self.small, x)
+        
+        if len(self.small) > len(self.large):
+            x = -heapq.heappop(self.small)
+            heapq.heappush(self.large, x)
 
-        if len(self.minH) > len(self.maxH) + 1:
-            element = heapq.heappop(self.minH)
-            heapq.heappush(self.maxH, -element)
-        # print(self.maxH, self.minH)
+
+
     def findMedian(self) -> float:
-        if len(self.minH) == len(self.maxH):
-            return (self.minH[0] + (-self.maxH[0])) / 2
+        if len(self.small) == len(self.large):
+            return (-self.small[0] + self.large[0]) / 2
         
-        elif len(self.minH) > len(self.maxH):
-            return self.minH[0]
-        
-        else:
-            return -self.maxH[0]
-        
+        return self.large[0]
 
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
+
+
+#  s = -3          l = -2 -1 
