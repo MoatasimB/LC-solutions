524e836471ad9593f14bbd7b4fba7be58efdc5f6
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-02 16:07:22 -0500

    Runtime: 117 ms (69.94%), Memory: 74.2 MB (25.66%) - Gitcode

---
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/381. Insert Delete GetRandom O(1) - Duplicates allowed.py
@@ -0,0 +1,42 @@
+
+class RandomizedCollection:
+
+    def __init__(self):
+        self.indices = defaultdict(set)
+        self.lst = []
+        
+
+    def insert(self, val: int) -> bool:
+        self.indices[val].add(len(self.lst))
+        self.lst.append(val)
+        return len(self.indices[val]) == 1
+
+        
+
+    def remove(self, val: int) -> bool:
+        if not self.indices[val]:
+            return False
+        idx_to_remove = self.indices[val].pop()
+        if len(self.indices[val]) == 0:
+            del self.indices[val]
+        last_val = self.lst[-1]
+        last_idx = len(self.lst) - 1
+        self.indices[last_val].add(idx_to_remove)
+        self.indices[last_val].remove(last_idx)
+
+        self.lst[idx_to_remove], self.lst[last_idx] = self.lst[last_idx], self.lst[idx_to_remove]
+
+        self.lst.pop()
+
+        return True
+        
+
+    def getRandom(self) -> int:
+        return random.choice(self.lst)
+
+
+# Your RandomizedCollection object will be instantiated and called as such:
+# obj = RandomizedCollection()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
