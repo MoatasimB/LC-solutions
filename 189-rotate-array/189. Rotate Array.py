525ee0390bcdd0e49788fbdb0168f7612e1b0103
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-06 22:43:29 -0500

    Runtime: 7 ms (44.65%), Memory: 25.7 MB (46.48%) - Gitcode

--- 189-rotate-array/189. Rotate Array.py
+++ 189-rotate-array/189. Rotate Array.py
@@ -3,14 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def rev(i, j):
-
-            while i <= j:
+        def rev(i,j):
+            while i < j:
                 nums[i], nums[j] = nums[j], nums[i]
                 i += 1
                 j -= 1
         n = len(nums)
-        k = k % n
-        rev(0,n-k-1)
-        rev(n - k, n - 1)
-        rev(0, n - 1)
+        k = k%n
+        rev(n-k, n-1)
+        rev(0, n-k-1)
+        rev(0, n-1)
