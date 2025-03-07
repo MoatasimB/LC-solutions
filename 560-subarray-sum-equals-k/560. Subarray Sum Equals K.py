38a9afe859a2922cbfa0f4dc6ce402beeda9edaf
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-06 23:29:38 -0500

    Runtime: 43 ms (21.42%), Memory: 20.4 MB (70.10%) - Gitcode

--- 560-subarray-sum-equals-k/560. Subarray Sum Equals K.py
+++ 560-subarray-sum-equals-k/560. Subarray Sum Equals K.py
@@ -1,18 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         
-        counts = {0 : 1}
-        ans = 0
+        mpp = defaultdict(int)
+        mpp[0] = 1
+
         curr = 0
+        ans = 0
+
         for num in nums:
             curr += num
-
-            if (curr - k) in counts:
-                ans += counts[(curr - k)]
-            
-            if curr in counts:
-                counts[curr] += 1
-            else:
-                counts[curr] = 1
+            if (curr - k) in mpp:
+                ans += mpp[curr-k]
+            mpp[curr] += 1
         
         return ans
