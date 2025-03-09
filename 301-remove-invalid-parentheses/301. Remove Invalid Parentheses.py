57c29a28037a02893da470e80d9f6effc9f0afee
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-09 17:55:38 -0400

    Runtime: 2114 ms (11.03%), Memory: 20.3 MB (8.99%) - Gitcode

---
+++ 301-remove-invalid-parentheses/301. Remove Invalid Parentheses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        ans = []
+        def dfs(i, left, right, curr):
+
+            if i == len(s) and left == right:
+                ans.append("".join(curr[:]))
+                return
+            while i<len(s) and s[i] != "(" and s[i] != ")":
+                curr.append(s[i])
+                i += 1
+            
+            if i == len(s):
+                if left == right:
+                    ans.append("".join(curr[:]))
+                return
+
+            if s[i] == "(":
+                dfs(i+1, left + 1, right, curr + ["("])
+            else:
+                if left > right:
+                    dfs(i+1, left, right + 1, curr + [")"])
+            
+            #skip
+            dfs(i+1, left, right, curr)
+        
+        dfs(0,0,0, [])
+        longest = max(len(x) for x in ans)
+        ans = set(ans)
+        final = [x for x in ans if len(x) == longest]
+        return final
