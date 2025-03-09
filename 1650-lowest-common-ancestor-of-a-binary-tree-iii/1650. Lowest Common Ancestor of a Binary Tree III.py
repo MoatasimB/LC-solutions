1a293c4ecaf90de87e67d58848460dfdd7a7ec9d
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-08 21:55:27 -0500

    Runtime: 56 ms (41.14%), Memory: 20.9 MB (55.21%) - Gitcode

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650. Lowest Common Ancestor of a Binary Tree III.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650. Lowest Common Ancestor of a Binary Tree III.py
@@ -11,28 +11,28 @@ class Node:
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
         
-        def dfs(root):
-            if not root:
-                return [False, None]
-     
-            
-            left = dfs(root.left)
-            right = dfs(root.right)
-            
-            if left[1] != None or right[1]!= None:
-                return [True, left[1] if left[1] else right[1]]
-            if left[0] and right[0] and left[1] == None:
-                return [True, root]
-            if (left[0] or right[0]) and (root == p or root == q):
-                return [True, root]
-            if left[0] or right[0] or root == p or root == q:
-                return [True, None]
-  
-            
-            return [False, None]
+        def depth(node):
+            d = 0
+            n = node
+            while n.parent:
+                n = n.parent
+                d += 1
+            return d
         
-        curr = p 
-        while curr.parent != None:
-            curr = curr.parent
-        return dfs(curr)[1]
-            
+        pDepth = depth(p)
+        qDepth = depth(q)
+        print(pDepth, qDepth)
+
+        while pDepth < qDepth:
+            q = q.parent
+            qDepth -= 1
+        
+        while qDepth < pDepth:
+            p = p.parent
+            pDepth -= 1
+        
+        while q!=p:
+            q = q.parent
+            p = p.parent
+        
+        return p
