609bfb8d93e039e9816bd37550638fbc8808d2d7
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-13 17:39:34 -0400

    Runtime: 406 ms (5.34%), Memory: 18.5 MB (6.90%) - Gitcode

---
+++ 311-sparse-matrix-multiplication/311. Sparse Matrix Multiplication.py
@@ -0,0 +1,23 @@
+class Solution:
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+        
+
+        m = len(mat1)
+        k = len(mat1[0])
+        n = len(mat2[0])
+
+        def getVal(r,c):
+            ans = 0
+            for i in range(k):
+                ans += (mat1[r][i] * mat2[i][c])
+                print(mat1[r][i], mat2[i][c])
+            return ans
+        
+        final = [[0] * n for _ in range(m)]
+
+        for row in range(m):
+            for col in range(n):
+                val = getVal(row,col)
+                final[row][col] = val
+        
+        return final
