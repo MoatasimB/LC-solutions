afe7b36012236cb586c45bd97280ceb9dd019695
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-14 17:09:57 -0400

    Runtime: 389 ms (49.18%), Memory: 90.9 MB (52.24%) - Gitcode

---
+++ 1216-valid-palindrome-iii/1216. Valid Palindrome III.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValidPalindrome(self, s: str, k: int) -> bool:
+        n = len(s)
+        dp = {}
+        def dfs(i,j):
+            if (i,j) in dp:
+                return dp[(i,j)]
+            if i==j:
+                if s[i] == s[j]:
+                    return 0
+                else:
+                    return 1
+            if i > j:
+                return 0
+            if i == n:
+                return n - j - 1
+            if j == 0:
+                return n - i - 1
+            ans = float('inf')
+            if s[i] == s[j]:
+                ans = dfs(i+1, j-1)
+            else:
+                ans = 1 + min(dfs(i+1, j), dfs(i, j-1))
+            
+            dp[(i,j)] = ans
+            return ans
+
+        return dfs(0, n-1) <= k
+
+
