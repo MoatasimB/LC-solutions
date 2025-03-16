d668289d6421d000fdde16014988665e4b51dfb7
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-16 15:44:15 -0400

    Runtime: 16 ms (74.84%), Memory: 18.2 MB (81.37%) - Gitcode

---
+++ 1028-recover-a-tree-from-preorder-traversal/1028. Recover a Tree From Preorder Traversal.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        
+        levels = []
+        i = 0
+        n = len(traversal)
+
+        while i < n:
+            depth = 0
+            while i < n and traversal[i] == "-":
+                depth += 1
+                i += 1
+            
+            val = 0
+            while i < n and traversal[i].isdigit():
+                val = (val * 10) + int(traversal[i])
+                i += 1
+            
+            node = TreeNode(val)
+            if depth < len(levels):
+                levels[depth] = node
+            else:
+                levels.append(node)
+            
+            if depth > 0:
+                parent = levels[depth - 1]
+                if not parent.left:
+                    parent.left = node
+                else:
+                    parent.right = node
+            
+
+        
+        return levels[0]
