df9ee94df6293dfc0b71c193b3290e82cbd0a6e3
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-17 23:43:08 -0400

    Runtime: 7 ms (48.94%), Memory: 19.1 MB (73.15%) - Gitcode

--- 207-course-schedule/207. Course Schedule.py
+++ 207-course-schedule/207. Course Schedule.py
@@ -1,24 +1,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0] * numCourses
-        adj = [[] for _ in range(numCourses)]
-        for second, first in prerequisites:
-            indegree[second] +=1
-            adj[first].append(second)
-        
+        indeg = [0] * numCourses
+        adj = defaultdict(list)
 
+        for x, y in prerequisites:
+            indeg[x] += 1
+            adj[y].append(x)
+        
         q = deque()
-    
-        for i in range(len(indegree)):
-            if indegree[i] == 0:
+        
+        for i in range(len(indeg)):
+            if indeg[i] == 0:
                 q.append(i)
         
         while q:
             node = q.popleft()
-            for nei in adj[node]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
-                    q.append(nei)
-        print(indegree)
-        return sum(indegree) == 0
 
+            for nei in adj[node]:
+                indeg[nei] -= 1
+                if indeg[nei] == 0:
+                    q.append(nei)
+        
+        return sum(indeg) == 0
