a7fefaf37e6785c5e63c0d5d3a9a6870b737002b
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-19 22:53:07 -0400

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (67.00%) - Gitcode

---
+++ 140-word-break-ii/140. Word Break II.py
@@ -0,0 +1,25 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordDict = set(wordDict)
+        dp = {}
+        ans = []
+        def dfs(i, curr, prev):
+            if i == len(s):
+                if curr in wordDict:
+                    ans.append(prev + curr) 
+                return
+            
+            if curr in wordDict:
+                dfs(i, "", prev + curr + " ")
+            
+            dfs(i+1, curr + s[i], prev)
+            
+        
+        dfs(0, "", "")
+
+        return ans
+
+
+
+
+        
