452de642ae6d41a5c3c9daacea09c72420c346fd
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-19 23:42:42 -0400

    Runtime: 0 ms (100.00%), Memory: 17.6 MB (98.21%) - Gitcode

--- 91-decode-ways/91. Decode Ways.py
+++ 91-decode-ways/91. Decode Ways.py
@@ -1,6 +1,17 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        dp = [0] * (len(s) + 1)
+        dp[len(s)] = 1
+
+        for i in range(len(s) - 1, - 1, -1):
+            ans = 0
+            if s[i] != '0':
+                ans += dp[i + 1]
+            if i < len(s) - 1 and s[i] != '0' and  int(s[i:i+2]) <= 26:
+                ans += dp[i + 2]
+            dp[i] = ans
         
+        return dp[0]
         dp = {}
         def dfs(i):
 
