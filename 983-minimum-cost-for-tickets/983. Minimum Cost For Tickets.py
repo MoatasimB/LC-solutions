e508292db21b382e8332ef27c3a6987df5d5432d
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-03-23 22:04:42 -0400

    Runtime: 3 ms (78.34%), Memory: 17.8 MB (82.04%) - Gitcode

--- 983-minimum-cost-for-tickets/983. Minimum Cost For Tickets.py
+++ 983-minimum-cost-for-tickets/983. Minimum Cost For Tickets.py
@@ -1,7 +1,35 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        
+        days_to_buy = set(days)
 
+        dp = [0] * (days[-1] + 1)
+        # i = 0
+        # for day in range(1, days[-1] + 1):
+        #     if days[i] >=  day:
+        #         dp[day] = dp[day - 1]
+        #     else:
+        #         i += 1
+        #         thirty_day = costs[2] + (dp[day - 30] if day - 30 >= 0 else 0)
+        #         seven_day = costs[1] + (dp[day - 7] if day - 7 >= 0 else 0)
+        #         one_day = costs[0] + (dp[day - 1] if day - 1 >= 0 else 0)
+
+        #         dp[day] = min(thirty_day, seven_day, one_day)
+        # return dp[days[-1]]
+        
+        for day in range(days[-1], 0, -1):
+            if day not in days_to_buy:
+                dp[day] = dp[day + 1]
+            else:
+                thirty_day = costs[2] + (dp[day + 30] if day + 30 <= days[-1] else 0)
+                seven_day = costs[1] + (dp[day + 7] if day + 7 <= days[-1] else 0)
+                one_day = costs[0] + (dp[day +  1] if day + 1 <= days[-1] else 0)
+
+                ans = min(one_day, seven_day, thirty_day)
+                
+                dp[day] = ans
+        # print(dp)
+        return dp[1]
+        
         dp = {}
         days_to_buy = set(days)
         def dfs(day):
@@ -12,17 +40,6 @@ class Solution:
             if day not in days_to_buy:
                 return dfs(day + 1)
 
-            # while i < len(days) and days[i] < day:
-            #     i += 1
-            # if i == len(days):
-            #     return 0
-            # ans = None
-            # if day >= days[i]:
-            #     ans = dfs(i+1, day)
-            
-            # else:
-            # one_day = costs[0] + dfs(i+1, days[i]+ 1)
-            # seven_day = costs[1] + dfs(i+1, days[i]+ 7)
             thirty_day = costs[2] + dfs(day + 30)
             seven_day = costs[1] + dfs(day + 7)
             one_day = costs[0] + dfs(day + 1)
