ce0b82bf5864e2004e731ffde8d03f45e605d68f
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-04-07 17:39:03 -0400

    Runtime: 148 ms (64.37%), Memory: 70.9 MB (12.70%) - Gitcode

--- 716-max-stack/716. Max Stack.py
+++ 716-max-stack/716. Max Stack.py
@@ -1,48 +1,45 @@
 class MaxStack:
 
     def __init__(self):
-        self.stack = []
         self.heap = []
+        self.cnt = 0
+        self.stack = []
         self.removed = set()
-        self.idx = 0
-# [-2, -45, 66] stack
 
-# [[40,3], [-45,1], [-82,2]] heap
-
-# {0,3,2} set
     def push(self, x: int) -> None:
-        heapq.heappush(self.heap, [-x, -self.idx])
-        self.stack.append([x, self.idx])
-        self.idx += 1
-
+        self.stack.append([x, self.cnt])
+        heapq.heappush(self.heap, [-x, -self.cnt])
+        self.cnt += 1
+        
 
     def pop(self) -> int:
-        while self.stack and (self.stack[-1][1]) in self.removed:
-            self.removed.remove(self.stack[-1][1])
+        while self.stack and self.stack[-1][1] in self.removed:
             self.stack.pop()
-        removed_idx = self.stack[-1][1]
-        self.removed.add(removed_idx)
-        return self.stack.pop()[0]
+        
+        num, idx = self.stack.pop()
+        self.removed.add(idx)
+        return num
 
     def top(self) -> int:
-        while self.stack and (self.stack[-1][1]) in self.removed:
-            self.removed.remove(self.stack[-1][1])
+        while self.stack and self.stack[-1][1] in self.removed:
             self.stack.pop()
         return self.stack[-1][0]
 
     def peekMax(self) -> int:
         while self.heap and -self.heap[0][1] in self.removed:
-            self.removed.remove(-self.heap[0][1])
             heapq.heappop(self.heap)
+        
         return -self.heap[0][0]
-    
+        
+
     def popMax(self) -> int:
         while self.heap and -self.heap[0][1] in self.removed:
-            self.removed.remove(-self.heap[0][1])
             heapq.heappop(self.heap)
-        removed_idx = -self.heap[0][1]
-        self.removed.add(removed_idx)
-        return -heapq.heappop(self.heap)[0]
+        
+        num, idx = heapq.heappop(self.heap)
+        self.removed.add(-idx)
+
+        return -num
 
 
 # Your MaxStack object will be instantiated and called as such:
