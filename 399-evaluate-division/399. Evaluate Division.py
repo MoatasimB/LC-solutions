8ff2f85efab446ef5788c73473d00a2be869f499
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-04-07 22:43:42 -0400

    Runtime: 0 ms (100.00%), Memory: 18 MB (49.10%) - Gitcode

--- 399-evaluate-division/399. Evaluate Division.py
+++ 399-evaluate-division/399. Evaluate Division.py
@@ -4,51 +4,47 @@ class Solution:
         adj = defaultdict(list)
 
         for i in range(len(equations)):
-            adj[equations[i][0]].append([equations[i][1], values[i]])
-            adj[equations[i][1]].append([equations[i][0], 1/values[i]])
-        print(adj)
+            a, b = equations[i]
+            val = values[i]
+
+            adj[a].append([b, val])
+            adj[b].append([a, 1/val])
+
+        
+        def bfs(start, end):
+            print("__________")
+            if start not in adj:
+                return -1
+            seen = set()
+            q = deque()
+            q.append((start, 1))
+            seen.add(start)
+            while q:
+                node, curr = q.popleft()
+                print(node, curr)
+                if node == end:
+                    return curr
+
+                for nei, val in adj[node]:
+                    if nei not in seen:
+                        seen.add(nei)
+                        # curr = curr * val
+                        q.append((nei, curr * val))
+            return -1
+        
         ans = []
 
-        def dfs(currentNode,finalNode,curr,seen):
-
-            # q = deque([(currentNode, curr)])
-            
-            # while q:
-            #     node, currValue = q.popleft()
-
-            #     if node == finalNode:
-            #         return currValue
-            #     seen.add(node)
-            #     for nei,val in adj[node]:
-            #         if nei not in seen:
-            #             q.append((nei, currValue * val))
-            
-            # return -1
-
-
-
-
-
-            if currentNode == finalNode:
-                return curr
-            seen.add(currentNode)
-            ans = -1
-            for nei,val in adj[currentNode]:
-                if nei not in seen:
-                    ans = dfs(nei, finalNode, curr*val, seen)
-                    if ans != -1:
-                        break
-
-            
-            return ans
-
-
-
-
-        for start,end in queries:
-            if (start not in adj) or (end not in adj):
-                ans.append(-1.00000)
-                continue
-            ans.append(dfs(start, end, 1, set()))
+        for start, end in queries:
+            ans.append(bfs(start, end))
         
         return ans
+
+        # adj = {
+        #     X1 : [X2, 3],
+        #     X2 : [X3, 4], [X1, 1/3]
+        #     X3 : [X4, 5], [X2, 1/4]
+        #     X4: [X5, 6], [X3, 1/5]
+        #     X5: [X4, 1/6]
+        # }
+
+        # q = [x4,20] [x1,1/3] [x2, 4/3]
