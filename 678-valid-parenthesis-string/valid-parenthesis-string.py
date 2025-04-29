2ae123985d7782020da256030088d5cb5271aa13
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-04-28 23:00:39 -0400

    Time: 6 ms (12.42%) | Memory: 18.4 MB (9.58%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,26 +1,26 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         dp = {}
-        def dfs(i, left, right):
+        def dfs(i, left):
             if i == len(s):
-                return True if left == right else False
-            if (i, left, right) in dp:
-                return dp[(i,left,right)]
-            if right > left:
+                return True if left == 0 else False
+            if (i, left) in dp:
+                return dp[(i,left)]
+            if left < 0:
                 return False
             ans = False
             if s[i] == "(":
-                if dfs(i+1, left + 1, right):
+                if dfs(i+1, left + 1):
                     ans = True
             elif s[i] == ")":
-                if dfs(i+1, left, right + 1):
+                if dfs(i+1, left - 1):
                     ans = True
             #skip, left, right
             else:
-                if dfs(i + 1, left, right) or dfs(i + 1, left + 1, right) or dfs(i + 1, left, right + 1):
+                if dfs(i + 1, left) or dfs(i + 1, left + 1) or dfs(i + 1, left -1):
                     ans = True
-            dp[(i,left, right)] = ans
+            dp[(i,left)] = ans
             return ans
         
-        return dfs(0,0,0)
+        return dfs(0,0)
             
