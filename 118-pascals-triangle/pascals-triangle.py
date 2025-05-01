10778987bcd32cc26a7567450322cb60d23a67de
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-04-30 22:59:55 -0400

    Time: 3 ms (10.28%) | Memory: 17.8 MB (58.53%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,13 +1,19 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        curr = []
-        final = []
-        for i in range(numRows):
-            for j in range(i + 1):
-                curr.append(math.comb(i,j))
-            final.append(curr)
-            curr = []
+        ans = [[1]]
+
+        curr = [1]
+
+        for i in range(1, numRows):
+            prev = ans[-1]
+            for j in range(1, i):
+                print(prev)
+                curr.append(prev[j-1] + prev[j])
+            
+            curr.append(1)
+            ans.append(curr)
+            curr = [1]
         
-        return final
+        return ans
 
 
