0ff8a182e2f30af3a1af5badbd1378167bc45843
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-05-01 23:42:06 -0400

    Time: 35 ms (43.74%) | Memory: 20.5 MB (91.54%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -1,78 +1,91 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
 
-        graph = defaultdict(list)
-        for account in accounts:
-            first_email = account[1]
-            for i in range(2, len(account)):
-                graph[first_email].append(account[i])
-                graph[account[i]].append(first_email)
-
-                # if account[i] not in graph:
-                #     graph[account[i]] = set()
-                # owners[account[i]] = owner
-                # for j in range(i + 1, len(account)):
-                #     graph[account[i]].add(account[j])
-                #     graph[account[j]].add(account[i])
-
-                    # owners[account[j]] = owner
+        # graph = defaultdict(list)
+        # for account in accounts:
+        #     first_email = account[1]
+        #     for i in range(2, len(account)):
+        #         graph[first_email].append(account[i])
+        #         graph[account[i]].append(first_email)
 
         
-        def dfs(node):
-            seen.add(node)
-            lst.append(node)
-            for nei in graph[node]:
-                if nei not in seen:
-                    dfs(nei)
+        # def dfs(node):
+        #     seen.add(node)
+        #     lst.append(node)
+        #     for nei in graph[node]:
+        #         if nei not in seen:
+        #             dfs(nei)
         
-        seen = set()
+        # seen = set()
+        # final = []
+        # for account in accounts:
+        #     account_name = account[0]
+        #     node = account[1]
+        #     lst = [account_name]
+        #     if node not in seen:
+        #         dfs(node)
+        #         lst[1:] = sorted(lst[1:])
+        #         final.append(lst)
+
+        # return final
+  
+        
+
+
+
+
+        class UF:
+            
+            def __init__(self, size):
+                self.roots = [i for i in range(size)]
+                self.ranks = [0] * (size)
+            
+            def find(self, root):
+                if self.roots[root] == root:
+                    return root
+                
+                self.roots[root] = self.find(self.roots[root])
+
+                return self.roots[root]
+            
+            def union(self, x, y):
+                rootX = self.find(x)
+                rootY = self.find(y)
+
+                if rootX != rootY:
+                    if self.ranks[rootX] < self.ranks[rootY]:
+                        self.roots[rootX] = rootY
+                    elif self.ranks[rootX] > self.ranks[rootY]:
+                        self.roots[rootY] = rootX
+                    else:
+                        self.roots[rootX] = rootY
+                        self.ranks[rootX] += 1
+            
+        n = len(accounts)
+        uf = UF(n)
+
+        emails_to_idx = {}
+
+        for i in range(n):
+            for j in range(1, len(accounts[i])):
+                email = accounts[i][j]
+                print(email)
+
+                if email not in emails_to_idx:
+                    emails_to_idx[email] = i
+                else:
+                    uf.union(i, emails_to_idx[email])
+        
+        components = defaultdict(list)
+
+        for email in emails_to_idx:
+            components[uf.find(emails_to_idx[email])].append(email)
+        
         final = []
-        for account in accounts:
-            account_name = account[0]
-            node = account[1]
-            lst = [account_name]
-            if node not in seen:
-                dfs(node)
-                lst[1:] = sorted(lst[1:])
-                final.append(lst)
+
+        for key, val in components.items():
+            name = accounts[key][0]
+            sorted_emails = sorted(val)
+            final.append([name] + sorted_emails)
 
         return final
-        # ans = []
-        # for emails in final:
-        #     owner = owners[emails[0]]
-        #     sorted_emails = sorted(emails)
-        #     ans.append([owner] + sorted_emails)
-        
-        return ans
-
-
-
-
-
-        # class UF:
-            
-        #     def __init__(self, size):
-        #         self.roots = [i for i in range(size)]
-        #         self.ranks = [0] * len(size)
-            
-        #     def find(self, root):
-        #         if self.roots = root:
-        #             return root
-                
-        #         self.roots[root] = self.find(self.roots[root])
-
-        #         return self.roots[root]
-            
-        #     def union(self, x, y):
-        #         rootX = self.find(x)
-        #         rootY = self.find(y)
-
-        #         if rootX != rootY:
-        #             if self.ranks[rootX] < self.ranks[rootY]:
-        #                 self.roots[rootX] = rootY
-        #             elif self.ranks[rootX] > self.ranks[rootY]:
-        #                 self.roots[rootY] = rootX
-        #             else:
-        #                 self.roots[rootX] = rootY
-        #                 self.ranks[rootX] += 1
-            
