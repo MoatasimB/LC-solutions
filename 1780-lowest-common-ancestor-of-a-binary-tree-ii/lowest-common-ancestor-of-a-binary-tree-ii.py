11c7b54260ada0cd079d9ac2c30b802db844e61f
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-05-04 02:44:00 -0400

    Time: 162 ms (24.33%) | Memory: 22.3 MB (25.53%) - LeetSync

--- 1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -12,21 +12,26 @@ class Solution:
         def dfs(root):
             nonlocal ans
             if not root:
-                return False
+                return [False, None]
             
-            left = dfs(root.left)
-            right = dfs(root.right)
-            
+            left, n1 = dfs(root.left)
+            right, n2 = dfs(root.right)
+            if n1:
+                return [True, n1]
+            if n2:
+                return [True, n2]
             if left and right:
+                return [True, root]
                 ans = root
             elif (root == p or root == q) and (left or right):
+                return [True, root]
                 ans = root
             
             if root == p or root == q or left or right:
-                return True
+                return [True, None]
             
-            return False
+            return [False, None]
         
-        dfs(root)
+        return dfs(root)[1]
         return ans
 
