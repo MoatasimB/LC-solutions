475ac6fc942db970b1307bbf6374953f2ee6a504
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-05-04 01:53:43 -0400

    Time: 4 ms (35.56%) | Memory: 18 MB (94.00%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.py
@@ -0,0 +1,20 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        
+        trips.sort(key = lambda x : x[1])
+
+        arr = [0] * trips[-1][2]
+
+        for num, start, end in trips:
+            if num > capacity:
+                return False
+            arr[start] += num
+            if end < len(arr):
+                arr[end] -= num
+        
+        for i in range(1, len(arr)):
+            arr[i] = arr[i] + arr[i-1]
+            if arr[i] > capacity:
+                return False
+        
+        return True
