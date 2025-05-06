3462dd265e0392f52c665ae6afee7966bce3e33a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-05-06 00:47:14 -0400

    Time: 17 ms (86.74%) | Memory: 18.4 MB (76.50%) - LeetSync

---
+++ 536-construct-binary-tree-from-string/construct-binary-tree-from-string.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def str2tree(self, s: str) -> Optional[TreeNode]:
+        
+        i = 0
+        def dfs():
+            nonlocal i
+
+            if i >= len(s) or s[i] == ")":
+                return None
+
+            if s[i] == "(":
+                i += 1
+            
+            sign = 1
+            if s[i] == "-":
+                sign = -1
+                i += 1
+            curr = 0
+            while i < len(s) and s[i].isdigit():
+                curr = (curr * 10 ) + int(s[i])
+                i += 1
+            i -= 1
+            node = TreeNode(curr * sign)
+            i += 1
+            node.left = dfs()
+            node.right = dfs()
+            i+= 1
+            return node
+
+        
+        return dfs()
+
+# i = 0
+
+# 4
+
+#         i = 1
+#             i = 2
+#             2
+#                 i = 3
+#                 i = 4
+#                     3
+
+#                     lefr = None
+
