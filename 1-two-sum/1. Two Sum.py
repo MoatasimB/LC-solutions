1ba77cdd6015f7869a98eda9715b770ed4ff73e3
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-05-09 23:22:40 -0400

    Runtime: 0 ms (100.00%), Memory: 19.1 MB (11.21%) - Gitcode

--- 1-two-sum/1. Two Sum.py
+++ 1-two-sum/1. Two Sum.py
@@ -2,7 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mpp = {}
 
-        
+
+
         for i in range(len(nums)):
 
             if target - nums[i] in mpp:
