021cb0a36b5336dc467a430d33ca627d26589505
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-05-15 23:19:33 -0400

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (99.28%) - Gitcode

--- 968-binary-tree-cameras/968. Binary Tree Cameras.py
+++ 968-binary-tree-cameras/968. Binary Tree Cameras.py
@@ -10,29 +10,30 @@ class Solution:
         ans = 0
         def dfs(node):
             nonlocal ans
-            l = 0
-            r = 0
+            if not node:
+                return 0
             if not node.left and not node.right:
-                return -1
+                return -1 
             
-            if node.left:
-                l = dfs(node.left)
-            if node.right:
-                r = dfs(node.right)
 
-            if l == -1 or r == -1:
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            if left == -1 or right == -1:
                 ans += 1
                 return 1
-
-            if l == 1 or r == 1:
+            
+            elif left == 1 or right == 1:
                 return 0
             
-            if l == 0 and r == 0:
+            elif left == 0 and right == 0:
                 return -1
-            
         
         final = dfs(root)
+
         if final == -1:
             ans += 1
-
+        
         return ans
+
+        
