36838d6c75980e494f3628dfd2e0cf77c27fd929
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-06-07 23:29:51 -0400

    Runtime: 99 ms (52.81%), Memory: 17.8 MB (76.49%) - Gitcode

---
+++ 1723-find-minimum-time-to-finish-all-jobs/1723. Find Minimum Time to Finish All Jobs.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
+
+        def check(mid):
+            workers = [0] * k
+            
+            def dfs(i):
+                if i == len(jobs):
+                    return True
+                
+                for j in range(k):
+                    if workers[j] + jobs[i] <= mid:
+                        workers[j] += jobs[i]
+                        if dfs(i + 1):
+                            return True
+                        workers[j] -= jobs[i]
+                    if workers[j] == 0:
+                        break
+                return False
+            return dfs(0)
+
+
+        l = max(jobs)
+        r = sum(jobs)
+
+
+
+        ans = float('inf')
+        while l <= r : 
+            mid = (l + r) // 2
+            print(mid)
+            if check(mid):
+                ans = min(ans, mid)
+                r = mid - 1
+            else:
+                l = mid + 1
+            
+
+        return ans
