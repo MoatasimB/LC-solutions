11ed38c4f9e35c741596977d1a7766d9d89a6dce
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-06-09 22:33:19 -0400

    Runtime: 38 ms (82.78%), Memory: 17.6 MB (75.14%) - Gitcode

---
+++ 2929-distribute-candies-among-children-ii/2929. Distribute Candies Among Children II.py
@@ -0,0 +1,47 @@
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        #stars and bars to get total
+        # subtract when someone gets more than limit and this
+        # is symmetrical
+
+        total = ((n+2) * (n+1)) // 2
+        if n < limit:
+            return total
+        # a b c = n
+        # a + limit + 1 , b , c = n - limit - 1 
+        x = n - limit + 1 
+        extra_single = (x * ( x - 1) )// 2
+
+        # a + limit + 1 , b + limit + 1 , c = n - 2 * limit - 2
+
+        z = n - (2 * limit)
+        if z < 1:
+            return max(0,total - (3 * extra_single))
+
+        extra_double = (z * ( z - 1) )// 2
+
+
+        # a + limit + 1 , b + limit + 1 , c + limit + 1= n - 3 * limit - 3
+        w = n - (3 * limit) - 1
+
+        if w < 1:
+            return max(0,total - (3 * extra_single) + (3 * extra_double))
+        
+        
+        extra_triple = (w * (w - 1)) // 2
+        return max(0,total - (3 * extra_single) + (3 * extra_double) - extra_triple)
+
+        #if candies < people than some will have 0
+
+
+        '''
+        PIE 
+        |A| when a gets more 
+        |B| when b gets more
+        |C| when c gets more
+
+        - 
+
+        |A n B|
+
+        '''
