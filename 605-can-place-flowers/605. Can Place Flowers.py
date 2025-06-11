daf76130a1b50f63b816bd7fddfc38126eb94bf7
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-06-10 21:23:54 -0400

    Runtime: 7 ms (74.35%), Memory: 18.1 MB (82.16%) - Gitcode

--- 605-can-place-flowers/605. Can Place Flowers.py
+++ 605-can-place-flowers/605. Can Place Flowers.py
@@ -2,25 +2,19 @@ class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         if n == 0:
             return True
-        if len(flowerbed) == 1:
-            return n == 0 if flowerbed[0] == 1 else n == 1 
         for i in range(len(flowerbed)):
+
             if flowerbed[i] == 1:
                 continue
-            if i == 0 and i < len(flowerbed) - 1:
-                if flowerbed[i + 1] == 0:
-                    n -= 1
-                    flowerbed[i] = 1
-            elif i > 0 and i == len(flowerbed) - 1:
-                if flowerbed[i-1] == 0:
-                    n -= 1
-                    flowerbed[i] = 1
-            else:
-                if flowerbed[i - 1] == 0 and flowerbed[i+ 1] == 0:
-                    n -= 1
-                    flowerbed[i] = 1
-            
-            if n == 0:
-                return True
+            left = (i == 0) or (flowerbed[i-1] == 0)
+            right = (i == len(flowerbed) - 1) or (flowerbed[i+1] == 0)
+            if left and right:
+                n -= 1
+                flowerbed[i] = 1 
+
+                if n == 0:
+                    return True
         
         return False
+
+
