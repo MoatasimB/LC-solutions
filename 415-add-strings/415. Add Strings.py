e3a5fd36a445045ba1ad408e58baecc40b3beffb
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-06-23 21:27:07 -0400

    Runtime: 7 ms (46.05%), Memory: 18.5 MB (12.88%) - Gitcode

--- 415-add-strings/415. Add Strings.py
+++ 415-add-strings/415. Add Strings.py
@@ -1,50 +1,43 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         
-        i = len(num1) - 1
-        j = len(num2) - 1
+        n1 = len(num1)
+        n2 = len(num2)
+
+        i = n1 - 1
+        j = n2 - 1
         carry = 0
+
         ans = []
+
         while i >= 0 and j >= 0:
-            curr = int(num1[i]) + int(num2[j]) + carry
-            digit = curr % 10
-            ans.append(str(digit))
-            if curr >= 10:
-                carry = 1
-            else:
-                carry = 0
-            i -=1
-            j -= 1
-        while j >= 0:
-            curr = carry + int(num2[j])
-            digit = curr % 10
-            ans.append(str(digit))
-            if curr >= 10:
-                carry = 1
-            else:
-                carry = 0
-            j -=1
+
+            s = (int(num1[i]) + int(num2[j]) + carry) % 10
+            carry = (int(num1[i]) + int(num2[j]) + carry) // 10
+
+            ans.append(str(s))
+
+            i-=1
+            j-=1
+        
+
         while i >= 0:
-            curr = carry + int(num1[i])
-            digit = curr % 10
-            ans.append(str(digit))
-            if curr >= 10:
-                carry = 1
-            else:
-                carry = 0
-            i -=1
-        if carry:
-            ans.append(str(carry))
-
-        i = 0
-        j = len(ans) - 1
-        while i <= j:
-            ans[i], ans[j] = ans[j], ans[i]
-            i += 1
+            s = (int(num1[i]) + carry) % 10
+            carry = (int(num1[i]) + carry) // 10
+            ans.append(str(s))
+            i -= 1
+        
+        while j >= 0:
+            s = (int(num2[j]) + carry) % 10
+            carry = (int(num2[j]) + carry) // 10
+            ans.append(str(s))
             j -= 1
-        print(ans)
-        return "".join(ans)
-       
+        
+        if carry:
+            ans.append('1')
+        
 
+        return "".join(ans[::-1])
+            
 
 
