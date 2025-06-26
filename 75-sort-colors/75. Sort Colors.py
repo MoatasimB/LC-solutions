df64870811ceaa9ac745b1cb6007adb5f273809a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-06-25 22:02:16 -0400

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (33.70%) - Gitcode

--- 75-sort-colors/75. Sort Colors.py
+++ 75-sort-colors/75. Sort Colors.py
@@ -4,25 +4,21 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        left_border = -1
-        right_border = len(nums)
+        l = -1
+        r = len(nums)
+
         i = 0
 
-        while i < right_border:
+        while i < r:
+
             if nums[i] == 0:
-                nums[i], nums[left_border + 1] = nums[left_border + 1], nums[i]
-                left_border += 1
+                nums[i], nums[l+1] = nums[l+1], nums[i]
+                l +=1
                 i+=1
             elif nums[i] == 2:
-                nums[i], nums[right_border - 1] = nums[right_border - 1], nums[i]
-                right_border -= 1
+                nums[i], nums[r-1] = nums[r-1], nums[i]
+                r -= 1
             else:
-                i +=1
-            
+                i += 1
         
-# # 2 0 1
-#   i
-# # |1 0| 2
-#      i
 
-# # 0|1|2
