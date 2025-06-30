af68cb76bdd4cf46aac0a4b9f332290746256cdc
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-06-29 22:16:54 -0400

    Runtime: 95 ms (34.67%), Memory: 30.6 MB (55.37%) - Gitcode

--- 1944-number-of-visible-people-in-a-queue/1944. Number of Visible People in a Queue.py
+++ 1944-number-of-visible-people-in-a-queue/1944. Number of Visible People in a Queue.py
@@ -1,22 +1,18 @@
 class Solution:
     def canSeePersonsCount(self, heights: List[int]) -> List[int]:
-        
-
-        #mono_dec
-
-        stack = []
         ans = [0] * len(heights)
+
+        stack = [] #mono dec
 
         for i, h in enumerate(heights):
 
-            while stack and heights[stack[-1]] <= h:
+            while stack and heights[stack[-1]] < h:
 
-                idx = stack.pop()
-                ans[idx] += 1
+                ans[stack.pop()] += 1
             
             if stack:
                 ans[stack[-1]] += 1
-
+            
             stack.append(i)
         
         return ans
