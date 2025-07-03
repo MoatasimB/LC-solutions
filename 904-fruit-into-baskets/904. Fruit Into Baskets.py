76d0d450afa326a0883275173211dcc4373dab24
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-02 23:41:03 -0400

    Runtime: 214 ms (16.69%), Memory: 23.6 MB (42.37%) - Gitcode

--- 904-fruit-into-baskets/904. Fruit Into Baskets.py
+++ 904-fruit-into-baskets/904. Fruit Into Baskets.py
@@ -1,18 +1,19 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         
-        l = 0
-        holding = defaultdict(int)
         ans = 0
+        l = 0
+        mpp = defaultdict(int)
+
         for r in range(len(fruits)):
-            holding[fruits[r]] += 1
-            while len(holding) > 2:
-                holding[fruits[l]] -= 1
-                if holding[fruits[l]] == 0:
-                    del holding[fruits[l]]
-                l+=1
+            mpp[fruits[r]] += 1
+
+            while len(mpp) > 2:
+                mpp[fruits[l]] -= 1
+                if mpp[fruits[l]] == 0:
+                    del mpp[fruits[l]]
+                l += 1
             
             ans = max(ans, r - l + 1)
-            print(r, l)
-        
+
         return ans
