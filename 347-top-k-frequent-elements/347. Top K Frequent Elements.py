43376081a1bff13db1ee5df4f09df7cab1e4ac88
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-05 00:31:27 -0400

    Runtime: 3 ms (91.01%), Memory: 21.2 MB (61.60%) - Gitcode

--- 347-top-k-frequent-elements/347. Top K Frequent Elements.py
+++ 347-top-k-frequent-elements/347. Top K Frequent Elements.py
@@ -1,20 +1,48 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-
         mpp = defaultdict(int)
-
 
         for num in nums:
             mpp[num] += 1
-    
-        min_heap = []
+        
+        arr = list(mpp.keys())
+        
+        
+        def quickSelect(left, right, kth):
 
-        for num, val in mpp.items():
+            if left == right:
+                return
+            
+            index = random.randint(left, right)
 
-            heapq.heappush(min_heap, [val, num])
+            pivot_index = partition(left, right, index)
 
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
+            if pivot_index > kth:
+                quickSelect(left, pivot_index - 1, kth)
+            elif pivot_index < kth:
+                quickSelect(pivot_index + 1, right, kth)
+            else:
+                return
+        
+        def partition(left, right, p_idx):
 
-        return [num for _, num in min_heap]
+            #move piv to end
+
+            freq = mpp[arr[p_idx]]
+
+            arr[p_idx], arr[right] = arr[right], arr[p_idx]
+            store_index = left
+            for i in range(left, right):
+                if mpp[arr[i]] < freq:
+                    arr[i], arr[store_index] = arr[store_index], arr[i]
+                    store_index += 1
+            
+            arr[store_index], arr[right] = arr[right], arr[store_index]
+            return store_index
+        
+
+        quickSelect(0, len(arr) - 1, len(arr) - k)
+
+        return arr[len(arr)-k:]
+
