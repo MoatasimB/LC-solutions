626181cddae32ed561963ebef0a1c0753b67dd3b
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-06 16:09:29 -0400

    Runtime: 3 ms (73.21%), Memory: 18.1 MB (29.35%) - Gitcode

--- 746-min-cost-climbing-stairs/746. Min Cost Climbing Stairs.py
+++ 746-min-cost-climbing-stairs/746. Min Cost Climbing Stairs.py
@@ -1,18 +1,29 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+
+
+        memo = [float('inf')] * (n + 1)
+
+        memo[0] = 0
+        memo[1] = 0
+
+        for i in range(2, n + 1):
+            memo[i] = min(cost[i - 1] + memo[i-1], cost[i-2] + memo[i-2])
         
+        return memo[n]
+
 
         #C(n) = min cost to reach stair n
         #C(n) = min(C(n-1) + cost[n-1], C(n-2) + cost[n-2])
-        #C(0) = cost[0], C(1) = cost[1]
-        memo = {}
-        n = len(cost)
-        def dfs(i):
-            if i in memo:
-                return memo[i]
-            if i >= n:
-                return 0
-            memo[i] = min(cost[i] + dfs(i+1), cost[i] + dfs(i+2))
-            return memo[i]
+        #C(0) = 0, C(1) = 0
+        # memo = {}
+        # def dfs(i):
+        #     if i in memo:
+        #         return memo[i]
+        #     if i <= 1:
+        #         return 0
+        #     memo[i] = min(cost[i - 1] + dfs(i-1), cost[i - 2] + dfs(i-2))
+        #     return memo[i]
         
-        return min(dfs(0), dfs(1))
+        # return dfs(n)
