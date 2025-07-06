1991a5d68b50252bd4fab1692c4639f29fa2289a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-06 15:34:43 -0400

    Runtime: 81 ms (5.32%), Memory: 18.1 MB (99.99%) - Gitcode

--- 735-asteroid-collision/735. Asteroid Collision.py
+++ 735-asteroid-collision/735. Asteroid Collision.py
@@ -1,27 +1,38 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        
+        #Only way asteroids to collide is if we have one that is going right in our stack
+        #And the next one is going left
+        #Otherwise it is not possible for asteroids to collide
+        #Left is represented by negative and right by positive
 
-        # pos = ->
-        # neg = <-
+        #We can implement a stack and check the last element if it is going right
+        #If the curr element is going left and see what happens once they explode
 
         stack = []
 
+        def Rightmovement(asteroid):
+            if asteroid < 0:
+                return False
+            else:
+                return True 
+
         for i in range(len(asteroids)):
-            ast = asteroids[i]
-
-            while stack and ast < 0 and stack[-1] > 0:
-                if stack[-1] > abs(ast):
-                    ast = 0
-                elif stack[-1] == abs(ast):
+            
+            if stack and not Rightmovement(asteroids[i]) and Rightmovement(stack[-1]):
+                
+                while stack and Rightmovement(stack[-1]) and not Rightmovement(asteroids[i]) and abs(stack[-1]) < abs(asteroids[i]):
+                    x = stack.pop()
+             
+            
+                if stack and abs(asteroids[i]) == stack[-1]:
                     stack.pop()
-                    ast = 0
                     continue
-                else:
-                    stack.pop()
-
-
-            if ast != 0:
-                stack.append(ast)
+                
+                if not stack or (not Rightmovement(stack[-1]) and not Rightmovement(asteroids[i])):
+                    stack.append(asteroids[i])
+ 
+            else:
+                
+                stack.append(asteroids[i])
         
         return stack
