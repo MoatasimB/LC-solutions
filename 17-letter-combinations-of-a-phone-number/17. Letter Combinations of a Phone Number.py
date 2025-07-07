869c2ebaece6bb5346391025971a68e6b1dc2fcf
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-07 00:02:42 -0400

    Runtime: 0 ms (100.00%), Memory: 18 MB (22.69%) - Gitcode

--- 17-letter-combinations-of-a-phone-number/17. Letter Combinations of a Phone Number.py
+++ 17-letter-combinations-of-a-phone-number/17. Letter Combinations of a Phone Number.py
@@ -11,19 +11,23 @@ class Solution:
             "8":"tuv",
             "9":"wxyz",
         }
+
+
+        ans = []
         if not digits:
             return []
-        ans = []
         def dfs(i, curr):
-            if i == len(digits):
+
+            if len(curr) == len(digits):
                 ans.append("".join(curr[:]))
                 return
             
-            for ch in mpp[digits[i]]:
-                curr.append(ch)
+            for j in range(i, len(digits)):
+                for d in mpp[digits[j]]:
+                    curr.append(d)
+                    dfs(j+1, curr)
+                    curr.pop()
+        
 
-                dfs(i+1, curr)
-
-                curr.pop()
         dfs(0,[])
         return ans
