24081545db33919c2b6a3bbd63007d4697c4f679
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-06 23:48:31 -0400

    Runtime: 139 ms (49.58%), Memory: 59.4 MB (94.85%) - Gitcode

--- 77-combinations/77. Combinations.py
+++ 77-combinations/77. Combinations.py
@@ -5,21 +5,15 @@ class Solution:
         ans = []
 
         def dfs(i, curr):
-
             if len(curr) == k:
                 ans.append(curr[:])
                 return
             
-            for j in range(i,n+1):
+            for j in range(i, n + 1):
                 curr.append(j)
-                dfs(j+1, curr)
+                dfs(j + 1, curr)
                 curr.pop()
-            # curr.append(i)
-
-            # dfs(i+1, curr)
-
-            # curr.pop()
-            # dfs(i+1, curr)
+        
 
         dfs(1, [])
         return ans
