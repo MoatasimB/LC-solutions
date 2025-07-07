f5037a960bf42f8d8d53258f18610ae230fc13fe
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-06 23:53:58 -0400

    Runtime: 32 ms (4.77%), Memory: 16.3 MB (100.00%) - Gitcode

--- 216-combination-sum-iii/216. Combination Sum III.py
+++ 216-combination-sum-iii/216. Combination Sum III.py
@@ -1,22 +1,27 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        digits = [1,2,3,4,5,6,7,8,9]
         
-
-        ans = []
-
-        def dfs(i, curr, goal):
-
-            if len(curr) == k and goal == 0:
+        # curr = [13]
+        # start =2 
+        # sum = 4
+        
+        def backtrack(curr, start, s):
+            
+            if len(curr) == k and s==n:
                 ans.append(curr[:])
                 return
-            
-            for j in range(i, 10):
-                if goal - j >= 0:
-                    curr.append(j)
-                    dfs(j+1, curr, goal - j)
+                
+            for j in range(start, len(digits)):
+                if s + digits[j] <= n:
+                    curr.append(digits[j])
+                    backtrack(curr, j+1 ,s+digits[j])
                     curr.pop()
         
+        
+        ans = []
+        backtrack([],0,0)
 
-
-        dfs(1, [], n)
         return ans
+        
+            
