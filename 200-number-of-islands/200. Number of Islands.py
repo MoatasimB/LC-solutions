47a186b3e1068a2107fbbb8552a892d3ffae35a3
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-08 00:14:28 -0400

    Runtime: 309 ms (11.86%), Memory: 28.3 MB (9.96%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,27 +1,34 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        def valid(r,c):
-            return 0<=r<rows and 0<=c<cols
         
-        dirs = [(0,1), (1,0), (0,-1), (-1,0)]
-        visited  = [[False] * cols for _ in range(rows)]
-        def dfs(r,c):
-            visited[r][c] = True
+        m = len(grid)
+        n = len(grid[0])
+
+        def valid(r, c):
+            return 0<=r<m and 0<=c<n
+
+        dirs = [(0,1), (1,0), (-1,0), (0,-1)]
+        
+        def dfs(r, c):
+            
+            if (r,c) in seen:
+                return
+            
+            seen.add((r,c))
 
             for dx, dy in dirs:
                 nr = dx + r
                 nc = dy + c
-
-                if valid(nr,nc) and not visited[nr][nc] and grid[nr][nc] == "1":
+                if valid(nr, nc) and (nr, nc) not in seen and grid[nr][nc] == "1":
                     dfs(nr,nc)
         
+        seen = set()
         ans = 0
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1" and not visited[r][c]:
-                    dfs(r,c)
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == '1' and (r,c) not in seen:
                     ans += 1
+                    dfs(r,c)
         
         return ans
+        
