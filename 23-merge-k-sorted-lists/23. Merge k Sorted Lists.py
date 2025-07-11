474e5bb805674f57afe3619906077d7efd48d494
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-11 00:28:43 -0400

    Runtime: 17 ms (29.37%), Memory: 20.4 MB (36.68%) - Gitcode

--- 23-merge-k-sorted-lists/23. Merge k Sorted Lists.py
+++ 23-merge-k-sorted-lists/23. Merge k Sorted Lists.py
@@ -1,39 +1,40 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    
+class HeapNode:
+    def __init__(self, node):
+        self.node = node
+    def __lt__(self, other):
+        return self.node.val < other.node.val
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
-        def merge(l1, l2):
-            dummy = ListNode(-1)
-            curr = dummy
-            while l1 and l2:
-                # print(l1.val, l2.val)
-                if l1.val < l2.val:
-                    curr.next = l1
-                    l1 = l1.next
-                else:
-                    curr.next = l2
-                    l2 = l2.next
-                # print(curr.val)
-                curr = curr.next
-            curr.next = l1 if l1 else l2
-            curr = dummy
-            while curr:
-                curr = curr.next
-            return dummy.next
-        
-        def mergeS(left, right):
-            if left >=right:
-                return lists[right]
-            
-            mid = (left + right) // 2
 
-            l1 = mergeS(left, mid)
-            l2 = mergeS(mid+1, right)
-            return merge(l1, l2)
-        if not lists:
-            return None
-        return mergeS(0, len(lists)-1)
+        minH = []
+
+        for head in lists:
+            if head:
+                heapNode = HeapNode(head)
+                heapq.heappush(minH, heapNode)
+        
+
+        dummy = ListNode(-1)
+
+        curr = dummy
+
+        while minH:
+
+            smallestNode = heapq.heappop(minH)
+
+            if smallestNode.node.next:
+                heapNode = HeapNode(smallestNode.node.next)
+                heapq.heappush(minH, heapNode)
+
+            curr.next = smallestNode.node
+            curr = curr.next
+        
+        return dummy.next
