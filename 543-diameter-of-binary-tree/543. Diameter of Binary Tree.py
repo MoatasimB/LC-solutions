1505ad1c5fd148365e792d56e6ccf7bceb56ddc7
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-12 00:16:01 -0400

    Runtime: 7 ms (46.76%), Memory: 20.7 MB (95.83%) - Gitcode

--- 543-diameter-of-binary-tree/543. Diameter of Binary Tree.py
+++ 543-diameter-of-binary-tree/543. Diameter of Binary Tree.py
@@ -6,19 +6,21 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        
         diameter = 0
 
         def dfs(root):
-            nonlocal diameter
             if not root:
                 return 0
-            
+            nonlocal diameter
+
             left = dfs(root.left)
             right = dfs(root.right)
 
             diameter = max(diameter, left + right)
 
-            return max(left, right) + 1
+            return 1 + max(left, right)
         
         dfs(root)
+
         return diameter
