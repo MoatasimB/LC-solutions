32437b726a384b8b7eb1484a957f0cc5e74f8592
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-12 00:13:43 -0400

    Runtime: 6 ms (56.69%), Memory: 21.4 MB (30.02%) - Gitcode

--- 347-top-k-frequent-elements/347. Top K Frequent Elements.py
+++ 347-top-k-frequent-elements/347. Top K Frequent Elements.py
@@ -2,46 +2,44 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         mpp = defaultdict(int)
-
         for num in nums:
             mpp[num] += 1
-        
+
         arr = list(mpp.keys())
         n = len(arr)
         
-        def quickSelect(left, right):
-
+        def quickSort(left, right):
             if left >= right:
-                return
+                return 
             
             rand_pivot = random.randint(left, right)
 
             piv_idx = partition(left, right, rand_pivot)
 
-            if piv_idx > n - k:
-                quickSelect(left, piv_idx - 1)
-            elif piv_idx < n-k:
-                quickSelect(piv_idx + 1, right)
+            if piv_idx < n - k:
+                quickSort(piv_idx + 1, right)
+            elif piv_idx > n - k:
+                quickSort(left, piv_idx - 1)
             else:
                 return
         
+
         def partition(left, right, pivot_index):
 
-
             val = mpp[arr[pivot_index]]
+            arr[right], arr[pivot_index] = arr[pivot_index],arr[right]
 
-            arr[pivot_index], arr[right] = arr[right], arr[pivot_index]
-            store_index = left
+            store_idx = left
+
             for i in range(left, right):
                 if mpp[arr[i]] < val:
-                    arr[i], arr[store_index] = arr[store_index], arr[i]
-                    store_index += 1
+                    arr[i], arr[store_idx] = arr[store_idx], arr[i]
+                    store_idx += 1
             
-            arr[store_index], arr[right] = arr[right], arr[store_index]
-            return store_index
-        
+            arr[store_idx], arr[right] = arr[right], arr[store_idx]
 
-        quickSelect(0, n - 1)
+            return store_idx
+        
+        quickSort(0, n - 1)
 
         return arr[n-k:]
-
