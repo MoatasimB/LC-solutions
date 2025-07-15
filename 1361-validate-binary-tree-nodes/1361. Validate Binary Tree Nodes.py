abe636fb30400b4eb1340d6e5297a194d4a4f3ff
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-14 22:05:20 -0400

    Runtime: 47 ms (21.65%), Memory: 20.5 MB (69.48%) - Gitcode

--- 1361-validate-binary-tree-nodes/1361. Validate Binary Tree Nodes.py
+++ 1361-validate-binary-tree-nodes/1361. Validate Binary Tree Nodes.py
@@ -8,35 +8,57 @@ class Solution:
             if leftChild[i] != -1:
                 inDeg[leftChild[i]] += 1
                 g[i].append(leftChild[i])
-                g[leftChild[i]].append(i)
+                # g[leftChild[i]].append(i)
 
             if rightChild[i] != -1:
                 inDeg[rightChild[i]] += 1
                 g[i].append(rightChild[i])
-                g[rightChild[i]].append(i)
+                # g[rightChild[i]].append(i)
         
-        seen = set()
-        def dfs(node):
-            
-            if node in seen:
-                return
-            
-            seen.add(node)
+        q = deque()
+        for i in range(len(inDeg)):
+            if inDeg[i] == 0:
+                q.append(i)
+            if inDeg[i] > 1:
+                return False
+        if len(q) > 1:
+            return False
+        while q:
+            node = q.popleft()
 
             for nei in g[node]:
-                dfs(nei)
+                inDeg[nei] -= 1
+                if inDeg[nei] == 0:
+                    q.append(nei)
         
-        dfs(0)
+        return sum(inDeg) == 0
+
+
+
+
+        
+        # seen = set()
+        # def dfs(node):
+            
+        #     if node in seen:
+        #         return
+            
+        #     seen.add(node)
+
+        #     for nei in g[node]:
+        #         dfs(nei)
+        
+        # dfs(0)
 
             
 
-        total_deg = 0
-        for val in inDeg:
-            if val > 1:
-                return False
-            total_deg += val
+        # total_deg = 0
+        # for val in inDeg:
+        #     if val > 1:
+        #         return False
+        #     total_deg += val
         
-        return (total_deg) == n - 1 and len(seen) == n
+        # return (total_deg) == n - 1 and len(seen) == n
 
 
         
