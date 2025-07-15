b450b27f6ee751d9f79b17df95a46593c14c2e72
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-14 23:15:27 -0400

    Runtime: 0 ms (100.00%), Memory: 17.7 MB (50.62%) - Gitcode

--- 96-unique-binary-search-trees/96. Unique Binary Search Trees.py
+++ 96-unique-binary-search-trees/96. Unique Binary Search Trees.py
@@ -1,15 +1,24 @@
 class Solution:
     def numTrees(self, n: int) -> int:
         
-        F = [0] * (n + 1)
-        F[1] = 1
-        F[0] = 1
 
+    #     return int((1 / (n + 1)) * math.comb(2*n, n))
+
+    
+    #        i
+    #  1 2 3 4 5 6 7
+    #        j
+
+        g = [0] * (n + 1)
+        g[0] = 1
+        g[1] = 1
+        
+
+      
         for i in range(2, n + 1):
             for j in range(1, i+1):
-                F[i] += F[j - 1] * F[i - j]
+                left = j - 1
+                right = i - j
+                g[i] += g[left] * g[right]
         
-        return F[n]
-
-
-        
+        return g[n]
