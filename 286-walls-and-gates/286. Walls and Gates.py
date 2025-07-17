cb009695dcfeec0ca770d343123398eab8c381da
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-16 20:49:43 -0400

    Runtime: 71 ms (27.87%), Memory: 25.1 MB (20.43%) - Gitcode

--- 286-walls-and-gates/286. Walls and Gates.py
+++ 286-walls-and-gates/286. Walls and Gates.py
@@ -4,31 +4,25 @@ class Solution:
         Do not return anything, modify rooms in-place instead.
         """
         
-        m = len(rooms)
-        n = len(rooms[0])
-
-        dirs = [(0,1), (1,0), (0,-1), (-1,0)]
-
-        def valid(r,c):
-            return 0<=r<m and 0<=c<n and rooms[r][c] == 2147483647
-        
         q = deque()
         seen = set()
-        for r in range(m):
-            for c in range(n):
-                if rooms[r][c] == 0:
-                    q.append((r,c,0))
-                    seen.add((r,c))
+        dirs = [(0,1), (1,0), (0,-1), (-1,0)]
+        def valid(r, c):
+            return 0<= r < len(rooms) and 0<= c < len(rooms[0])
+        for i in range(len(rooms)):
+            for j in range(len(rooms[0])):
+                if rooms[i][j] == 0:
+                    q.append((i,j, 0))
+                    seen.add((i,j))
         
         while q:
-            r, c, dist = q.popleft()
+            row, col, dist = q.popleft()
 
             for dx, dy in dirs:
-                nr = r + dx
-                nc = c + dy
-                if valid(nr, nc) and (nr,nc) not in seen:
+                nr, nc = row + dx, col + dy
+
+                if valid(nr,nc) and (nr, nc) not in seen and rooms[nr][nc] != -1:
                     rooms[nr][nc] = dist + 1
-                    q.append((nr,nc,dist + 1))
-                    seen.add((nr,nc))
+                    q.append((nr, nc, dist + 1))
+                    seen.add((nr, nc))
         
-        return rooms
