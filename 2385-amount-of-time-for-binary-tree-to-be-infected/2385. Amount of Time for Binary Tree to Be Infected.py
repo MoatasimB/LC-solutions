5be3f9a43ad9b405feee8c9b12727f56902f9a81
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-07-24 21:57:00 -0400

    Runtime: 195 ms (60.11%), Memory: 60.2 MB (55.80%) - Gitcode

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385. Amount of Time for Binary Tree to Be Infected.py
@@ -0,0 +1,54 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        
+        parents = {} #node, parent
+        startNode = None
+        def dfs(node, parent):
+            nonlocal startNode
+            if not node:
+                return
+            
+            parents[node] = parent
+            dfs(node.left, node)
+            dfs(node.right, node)
+
+            if node.val == start:
+                startNode = node
+        
+
+        dfs(root, TreeNode(-1))
+
+        seen = set()
+
+        q = deque()
+        q.append([startNode, 0])
+        ans = float("-inf")
+        while q:
+            node, time = q.popleft()
+
+            if node in seen:
+                continue
+            
+            ans = max(ans, time)
+            seen.add(node)
+
+            if node.left:
+                q.append([node.left, time + 1])
+            if node.right:
+                q.append([node.right, time + 1])
+
+            if parents[node].val > 0:
+                q.append([parents[node], time + 1])
+        
+        return ans
+
+        
+
+
