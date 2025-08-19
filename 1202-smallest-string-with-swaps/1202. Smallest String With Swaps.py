f0276193481a05ff2f1e92fb1e6ccffad54b1c16
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-08-18 23:17:15 -0400

    Runtime: 123 ms (50.00%), Memory: 56.5 MB (22.20%) - Gitcode

---
+++ 1202-smallest-string-with-swaps/1202. Smallest String With Swaps.py
@@ -0,0 +1,37 @@
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        
+        indices = {s[i]: i for i in range(len(s))}
+
+        s = list(s)
+        graph = defaultdict(list)
+
+        for x, y in pairs:
+            graph[x].append(y)
+            graph[y].append(x)
+        
+        
+
+        def dfs(node):
+            curr.append(s[node])
+            curr_idx.append(node)
+            for nei in graph[node]:
+                if nei not in seen:
+                    seen.add(nei)
+                    dfs(nei)
+            
+        seen = set()
+        for x, y in pairs:
+            curr = []
+            curr_idx = []
+            if x not in seen:
+                seen.add(x)
+                dfs(x)
+            
+                if curr:
+                    curr.sort()
+                    curr_idx.sort()
+                    for i in range(len(curr)):
+                        s[curr_idx[i]] = curr[i]
+        
+        return "".join(s)
