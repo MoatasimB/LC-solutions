54e20397fbb6a6260f29c2b7b1ee005ea4c79e37
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-08-22 20:47:50 -0400

    Runtime: 3 ms (8.23%), Memory: 18 MB (10.35%) - Gitcode

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28. Find the Index of the First Occurrence in a String.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28. Find the Index of the First Occurrence in a String.py
@@ -1,41 +1,44 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         
-        n = len(haystack)
-        m = len(needle)
         p = 31
         MOD = 10**9 + 7
+        m = len(haystack)
+        n = len(needle)
 
-        max_factor = 1
-
-        for _ in range(m):
-            max_factor = (max_factor * p) % MOD
         def hash(word):
 
-            ans = 0
             mult = 1
-            for i in range(m - 1, -1, -1):
-                ans += ((ord(word[i]) - ord('a')) * mult) % MOD
+            h = 0
+            for i in range(n - 1, -1, -1):
+                h  += ((ord(word[i]) - ord('a')) * mult) % MOD
                 mult = (mult * p) % MOD
             
-            return ans % MOD
+            return h % MOD
         
+        max_mult = 1
 
+        for _ in range(n):
+            max_mult =( max_mult * p) % MOD
+        
         needle_hash = hash(needle)
 
-        for i in range(n - m + 1):
+        for i in range(m - n + 1):
             if i == 0:
                 hay_hash = hash(haystack)
             else:
-                hay_hash = (((hay_hash * p) % MOD) - (((ord(haystack[i - 1]) - ord('a')) * max_factor)% MOD) + (ord(haystack[i + m - 1]) - ord('a'))) % MOD
-            
-
+                hay_hash = (((hay_hash * p) % MOD) + ((ord(haystack[i + n - 1]) - ord('a')) % MOD) - (((ord(haystack[i - 1]) - ord('a')) * max_mult) % MOD)) % MOD
+        
             if hay_hash == needle_hash:
-                for j in range(m):
+                for j in range(n):
                     if haystack[i + j] != needle[j]:
                         break
-                if j == m - 1:
+                
+                if j == n - 1:
                     return i
         
         return -1
-                
+
+
+
+
