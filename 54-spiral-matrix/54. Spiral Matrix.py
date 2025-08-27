de7c0311245ae6652b015cba7ac1609f4e458e07
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-08-27 15:42:19 +0500

    Runtime: 0 ms (100.00%), Memory: 17.7 MB (88.35%) - Gitcode

--- 54-spiral-matrix/54. Spiral Matrix.py
+++ 54-spiral-matrix/54. Spiral Matrix.py
@@ -15,22 +15,21 @@ class Solution:
 
             for i in range(left, right):
                 ans.append(matrix[top][i])
-            top += 1
-            
+            top +=1
+
             for i in range(top, bottom):
                 ans.append(matrix[i][right - 1])
-            right -= 1
-            
-            if not (top < bottom and left < right):
+            right -=1
+
+            if left >= right or top >= bottom:
                 break
             
-            for i in range(right - 1, left - 1, -1 ):
+            for i in range(right - 1, left - 1, -1):
                 ans.append(matrix[bottom - 1][i])
-            bottom -= 1
-            
-            for i in range(bottom - 1, top - 1, -1 ):
-                ans.append(matrix[i][left])
-            left += 1
-        
-        return ans
+            bottom -=1
 
+            for i in range(bottom - 1, top - 1, -1):
+                ans.append(matrix[i][left])
+            left +=1
+
+        return ans
