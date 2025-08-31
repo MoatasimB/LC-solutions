ad2de19c2668c10ce901dee89149ed33d894bb43
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-08-31 08:11:05 +0500

    Runtime: 0 ms (100.00%), Memory: 21.2 MB (51.86%) - Gitcode

--- 378-kth-smallest-element-in-a-sorted-matrix/378. Kth Smallest Element in a Sorted Matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378. Kth Smallest Element in a Sorted Matrix.py
@@ -1,17 +1,45 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-    
-        pq = []
-
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-
-                heapq.heappush(pq, -matrix[i][j])
-
-                if len(pq) > k:
-                    heapq.heappop(pq)
         
-        return -pq[0]
+        n = len(matrix)
+        def find_counts(mid, smaller, larger):
+            count = 0
 
+            r = n - 1
+            c = 0
 
-    
+            while r >= 0 and c <= n - 1:
+
+                if matrix[r][c] > mid:
+                    
+                    larger = min(larger, matrix[r][c])
+                    r -= 1
+                else:
+                    smaller = max(smaller, matrix[r][c])
+                    count += r + 1
+                    c += 1
+            
+            return count, smaller, larger
+        
+
+        l = matrix[0][0]
+        r = matrix[n-1][n-1]
+        
+
+        while l < r:
+            mid = (l + r) // 2
+            smaller = matrix[0][0]
+            larger = matrix[n-1][n-1]
+
+            count, smaller, larger = find_counts(mid, smaller, larger)
+
+            if count == k:
+                return smaller
+            elif count < k:
+                l = larger
+            else:
+                r = smaller
+        
+        
+        
+        return l
