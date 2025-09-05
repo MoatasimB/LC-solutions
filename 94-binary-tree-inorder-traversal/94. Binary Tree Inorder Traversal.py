8966ede78ad560bddb2394831e8bab1074a2a699
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-09-05 10:44:25 +0500

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (54.20%) - Gitcode

---
+++ 94-binary-tree-inorder-traversal/94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        
+        ans = []
+        
+        stack = []
+        curr = root
+        while stack or curr:
+            
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            ans.append(curr.val)
+            curr = curr.right
+        
+        return ans
+                
+        
+        def dfs(root):
+            if not root:
+                return
+            
+            dfs(root.left)
+            ans.append(root.val)
+            dfs(root.right)
+        
+        dfs(root)
+        return ans
+    
