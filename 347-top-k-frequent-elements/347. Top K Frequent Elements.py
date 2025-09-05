9f6fb50a5188c189208781368c6a0a3ad707a6a2
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-09-06 04:43:01 +0500

    Runtime: 3 ms (90.54%), Memory: 21 MB (90.39%) - Gitcode

--- 347-top-k-frequent-elements/347. Top K Frequent Elements.py
+++ 347-top-k-frequent-elements/347. Top K Frequent Elements.py
@@ -1,45 +1,22 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        mpp = defaultdict(int)
+        freq = defaultdict(int)
+
         for num in nums:
-            mpp[num] += 1
+            freq[num] += 1
 
-        arr = list(mpp.keys())
-        n = len(arr)
+        counts = [[val, key] for key, val in freq.items()]
+
+        heap = []
+
+        for i in range(len(counts)):
+            heapq.heappush(heap, (counts[i][0], counts[i][1]))
+            if len(heap) > k:
+                heapq.heappop(heap)
         
-        def quickSort(left, right):
-            if left >= right:
-                return 
-            
-            rand_pivot = random.randint(left, right)
-
-            piv_idx = partition(left, right, rand_pivot)
-
-            if piv_idx < n - k:
-                quickSort(piv_idx + 1, right)
-            elif piv_idx > n - k:
-                quickSort(left, piv_idx - 1)
-            else:
-                return
+        ans = []
+        for c, v in heap:
+            ans.append(v)
         
-
-        def partition(left, right, pivot_index):
-
-            val = mpp[arr[pivot_index]]
-            arr[right], arr[pivot_index] = arr[pivot_index],arr[right]
-
-            store_idx = left
-
-            for i in range(left, right):
-                if mpp[arr[i]] < val:
-                    arr[i], arr[store_idx] = arr[store_idx], arr[i]
-                    store_idx += 1
-            
-            arr[store_idx], arr[right] = arr[right], arr[store_idx]
-
-            return store_idx
-        
-        quickSort(0, n - 1)
-
-        return arr[n-k:]
+        return ans
