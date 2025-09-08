c608432bb3043b646bb3a49ef60d71211f7259ad
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-09-08 23:46:56 +0500

    Runtime: 185 ms (22.34%), Memory: 34.5 MB (39.94%) - Gitcode

---
+++ 2537-count-the-number-of-good-subarrays/2537. Count the Number of Good Subarrays.py
@@ -0,0 +1,45 @@
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        
+
+        counts = defaultdict(int)
+        l = 0
+        ans = 0
+        pairs = 0
+        for r in range(len(nums)):
+
+            counts[nums[r]] += 1
+
+            if counts[nums[r]] > 1:
+                if counts[nums[r]] - 1 > 1 :
+                    pairs -= math.comb(counts[nums[r]] - 1, 2)
+                    pairs += math.comb(counts[nums[r]], 2)
+                else:
+                    pairs += 1
+            
+            while pairs >= k:
+                ans += len(nums) - 1 - r + 1
+                if counts[nums[l]] > 1:
+                    pairs -= math.comb(counts[nums[l]], 2)
+                    counts[nums[l]] -= 1
+                    if counts[nums[l]] > 1:
+                        pairs += math.comb(counts[nums[l]], 2)
+                else:
+                    counts[nums[l]] -= 1
+                l += 1
+            
+        
+        return ans
+
+        # [3,1,4,3,2,2,4]
+
+
+        # 3 = 1
+        # 1 = 0
+        # 4 = 2
+        # 2 = 2
+
+        # pairs = 2
+
+        # 4
+        # while pairs >= k
