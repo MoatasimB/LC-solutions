232dc34a65fa4da1b61eb34693a194bafd7ec69c
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-09-18 05:33:11 +0500

    Runtime: 56 ms (5.42%), Memory: 27.2 MB (5.30%) - Gitcode

--- 238-product-of-array-except-self/238. Product of Array Except Self.py
+++ 238-product-of-array-except-self/238. Product of Array Except Self.py
@@ -5,19 +5,23 @@ class Solution:
         front = [1] * n
         back = [1] * n
 
-        for i in range(1, len(nums)):
-            front[i] = front[i-1] * nums[i-1]
+        prev = 1
+        for i in range(len(nums)):
+            front[i] = prev * nums[i]
+            prev *= nums[i]
 
-        back[n-1] = 1
-        for i in range(len(nums) - 2, -1, -1):
-            back[i] = back[i+1] * nums[i+1]
+        prev = 1
+        for i in range(len(nums) - 1, -1, -1):
+            back[i] = prev * nums[i]
+            prev *= nums[i]
         
         ans = [1] * n
 
-        for i in range(len(nums)):
-            ans[i] = front[i] * back[i]
-
-        # ans[0] = back[1]
-        # ans[n-1] = front[n-2]
+        for i in range(1, len(nums) - 1):
+            ans[i] = front[i-1] * back[i+1]
+        print(front)
+        print(back)
+        ans[0] = back[1]
+        ans[n-1] = front[n-2]
         return ans
 
