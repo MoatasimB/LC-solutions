deae0224d81b2e88d1f67dd454c2ff6df08c563b
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-09-18 05:32:58 +0500

    Runtime: 29 ms (36.34%), Memory: 23 MB (98.32%) - Gitcode

--- 238-product-of-array-except-self/238. Product of Array Except Self.py
+++ 238-product-of-array-except-self/238. Product of Array Except Self.py
@@ -1,23 +1,32 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
-        n = len(nums)
-        front = [1] * n
-        back = [1] * n
 
-        for i in range(1, len(nums)):
-            front[i] = front[i-1] * nums[i-1]
+        # The length of the input array
+        length = len(nums)
 
-        back[n-1] = 1
-        for i in range(len(nums) - 2, -1, -1):
-            back[i] = back[i+1] * nums[i+1]
-        
-        ans = [1] * n
+        # The answer array to be returned
+        answer = [0] * length
 
-        for i in range(len(nums)):
-            ans[i] = front[i] * back[i]
+        # answer[i] contains the product of all the elements to the left
+        # Note: for the element at index '0', there are no elements to the left,
+        # so the answer[0] would be 1
+        answer[0] = 1
+        for i in range(1, length):
 
-        # ans[0] = back[1]
-        # ans[n-1] = front[n-2]
-        return ans
+            # answer[i - 1] already contains the product of elements to the left of 'i - 1'
+            # Simply multiplying it with nums[i - 1] would give the product of all
+            # elements to the left of index 'i'
+            answer[i] = nums[i - 1] * answer[i - 1]
 
+        # R contains the product of all the elements to the right
+        # Note: for the element at index 'length - 1', there are no elements to the right,
+        # so the R would be 1
+        R = 1
+        for i in reversed(range(length)):
+
+            # For the index 'i', R would contain the
+            # product of all elements to the right. We update R accordingly
+            answer[i] = answer[i] * R
+            R *= nums[i]
+
+        return answer
