d2649cee53237edf08091790640b18cee21db407
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-09-23 11:08:05 +0500

    Runtime: 190 ms (30.59%), Memory: 22.5 MB (44.22%) - Gitcode

--- 329-longest-increasing-path-in-a-matrix/329. Longest Increasing Path in a Matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329. Longest Increasing Path in a Matrix.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        
+                
         dirs = [(0,1), (1,0), (0,-1), (-1,0)]
         m = len(matrix)
         n = len(matrix[0])
@@ -8,43 +8,23 @@ class Solution:
         def valid(r,c):
             return 0<=r<m and 0<=c<n
         
-        lgt_p = defaultdict(int) #(r,c) : longest inc from this node
+        lip = {}
         
-        def bfs(r, c):
-            ogr = r
-            ogc = c
-            q = deque()
-            q.append((r,c, 1))
-            seen = set()
-            seen.add((r,c))
-            ans = 0
-            while q:
-                r,c, p_len = q.popleft()
-                ans = max(ans, p_len)
-                for dx, dy in dirs:
-                    nr, nc = r + dx, c + dy
-                    if valid(nr,nc) and matrix[nr][nc] > matrix[r][c] and (nr,nc) not in seen:
-                        seen.add((nr,nc))
-                        if (nr,nc) not in lgt_p:
-                            bfs(nr,nc)
- 
-                        ans = max(ans, p_len + lgt_p[(nr,nc)])
-                            
+        def dfs(r,c):
+            if (r,c) in lip:
+                return lip[(r,c)]
+            ans = 1
+            for dx, dy in dirs:
+                nr, nc = dx + r, dy + c
+
+                if valid(nr,nc) and matrix[nr][nc] > matrix[r][c]:
+                    ans = max(ans, 1 + dfs(nr,nc))
             
-            lgt_p[(ogr,ogc)] = ans
+            lip[(r,c)] = ans
+            return ans
         
         for r in range(m):
             for c in range(n):
-                bfs(r,c)
-                
+                dfs(r,c)
         
-        return max(lgt_p.values())
-                    
-                
-        [[7,6,1,1],
-         [2,7,6,0],
-         [1,3,5,1],
-         [6,6,3,2]]
-                            
-            
-            
+        return max(lip.values())
