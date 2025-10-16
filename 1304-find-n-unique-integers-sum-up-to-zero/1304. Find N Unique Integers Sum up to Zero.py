867998422efcf0db099ebb24dc615aa6064cedc7
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-10-16 09:03:48 +0500

    Runtime: 0 ms (100.00%), Memory: 18 MB (35.32%) - Gitcode

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304. Find N Unique Integers Sum up to Zero.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        
+        ans = []
+        count = 1
+        if n % 2 != 0:
+            ans.append(0)
+            n -= 1
+        while n:
+            print(n)
+            ans.append(count)
+            n-=1
+            if n:
+                ans.append(-count)
+                n-=1
+            count +=1
+        
+        return ans
