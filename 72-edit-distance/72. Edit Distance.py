52431dad5dc0727ba9bbbde70b7ef72274174411
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-10-20 08:06:02 +0500

    Runtime: 63 ms (61.95%), Memory: 21.2 MB (32.63%) - Gitcode

--- 72-edit-distance/72. Edit Distance.py
+++ 72-edit-distance/72. Edit Distance.py
@@ -7,9 +7,22 @@ class Solution:
 
         dp = [[0] * (m+1) for _ in range(n+1)]
 
-        # for i in range(1, n):
+        for j in range(1, m+1):
+            dp[0][j] = j
+        for i in range(1, n+1):
+            dp[i][0] = i
         
-
+        
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                ans = 0
+                if word1[i-1] == word2[j-1]:
+                    ans = dp[i-1][j-1]
+                else:
+                    ans =  1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+                dp[i][j] = ans
+        return dp[n][m]
+        
         memo = {}
         def dfs(i,j):
             if (i,j) in memo:
