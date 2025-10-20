a5a329b84083aa0e6685130a3a94d11db7f25738
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-10-20 07:58:54 +0500

    Runtime: 39 ms (84.24%), Memory: 21.6 MB (9.56%) - Gitcode

--- 72-edit-distance/72. Edit Distance.py
+++ 72-edit-distance/72. Edit Distance.py
@@ -1,27 +1,33 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        
+        
         n = len(word1)
         m = len(word2)
+
+        dp = [[0] * (m+1) for _ in range(n+1)]
+
+        # for i in range(1, n):
+        
+
         memo = {}
         def dfs(i,j):
             if (i,j) in memo:
                 return memo[(i,j)]
-            if i == -1 and j == -1:
+            if i == 0 and j == 0:
                 return 0
-            if i == -1:
-                return j + 1
-            if j == -1:
-                return i + 1
+            if i == 0:
+                return j
+            if j == 0:
+                return i
             ans = 0
-            if word1[i] == word2[j]:
+            if word1[i-1] == word2[j-1]:
                 ans = dfs(i-1, j-1) 
             else:
                 ans =  1 + min(dfs(i-1, j), dfs(i, j-1), dfs(i-1,j-1))
             memo[(i,j)] = ans
             return ans
         
-        return dfs(n-1, m-1)
+        return dfs(n, m)
 
         
-        # intention
-        # execution
