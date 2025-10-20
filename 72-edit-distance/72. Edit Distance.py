4989827d441e665b9c9b9cf89424d41deb612d81
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-10-20 07:55:02 +0500

    Runtime: 39 ms (84.24%), Memory: 19.4 MB (86.22%) - Gitcode

--- 72-edit-distance/72. Edit Distance.py
+++ 72-edit-distance/72. Edit Distance.py
@@ -1,56 +1,27 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1)
+        m = len(word2)
+        memo = {}
+        def dfs(i,j):
+            if (i,j) in memo:
+                return memo[(i,j)]
+            if i == -1 and j == -1:
+                return 0
+            if i == -1:
+                return j + 1
+            if j == -1:
+                return i + 1
+            ans = 0
+            if word1[i] == word2[j]:
+                ans = dfs(i-1, j-1) 
+            else:
+                ans =  1 + min(dfs(i-1, j), dfs(i, j-1), dfs(i-1,j-1))
+            memo[(i,j)] = ans
+            return ans
         
-        # def dfs(i,j):
-        #     if (i,j) in dp:
-        #         return dp[(i, j)]
-        #     if i <= -1 and j <= -1:
-        #         return 0
-        #     if i <= -1:
-        #         return j + 1
-        #     if j <= -1:
-        #         return i + 1
+        return dfs(n-1, m-1)
 
-        #     if word1[i] == word2[j]:
-        #         dp[(i,j)] = dfs(i - 1, j - 1)
-        #         return dp[(i, j)]
-        #     else:
-        #         dp[(i,j)] = 1 + min(dfs(i - 1, j), dfs(i, j - 1), dfs(i - 1, j - 1))
-        #         return dp[(i, j)]
         
-        # dp = {}
-        # return dfs(len(word1) - 1, len(word2) - 1)
-
-
-        # dp = [[0]*(len(word2) + 1) for _ in range(len(word1) + 1)]
-
-        # for i in range(len(word2) + 1):
-        #     dp[0][i] = i
-        
-        # for i in range(len(word1) + 1):
-        #     dp[i][0] = i
-        
-        
-        # for i in range(1, len(word1) + 1):
-        #     for j in range(1, len(word2) + 1):
-
-        #         if word1[i-1] == word2[j-1]:
-        #             dp[i][j] = dp[i-1][j-1]
-        #         else:
-        #             dp[i][j] = 1 + min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
-        
-        # return dp[len(word1)][len(word2)]
-
-        prev = [i for i in range(len(word2)+1)]
-
-        for i in range(1, len(word1) + 1):
-            curr = [0] * (len(word2) + 1)
-            curr[0] = i
-            for j in range(1, len(word2) + 1):
-                if word1[i-1] == word2[j-1]:
-                    curr[j] = prev[j-1]
-                else:
-                    curr[j] = 1 + min(prev[j-1], prev[j], curr[j-1])
-            prev = curr
-        
-        return prev[len(word2)]
+        # intention
+        # execution
