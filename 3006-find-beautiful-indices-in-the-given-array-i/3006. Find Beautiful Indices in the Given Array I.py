2e6d9b1fdcf2c33377d11b38d545c08d2742be17
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-10-21 18:05:37 -0400

    Runtime: 3158 ms (5.02%), Memory: 22.4 MB (23.61%) - Gitcode

--- 3006-find-beautiful-indices-in-the-given-array-i/3006. Find Beautiful Indices in the Given Array I.py
+++ 3006-find-beautiful-indices-in-the-given-array-i/3006. Find Beautiful Indices in the Given Array I.py
@@ -1,49 +1,38 @@
 class Solution:
     def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
         
-        def find_matches(m, p): #main string, phrase
+
+        def find_matches(word, phrase):
             ans = []
-            if len(p) > len(m):
-                return ans
-            curr = 0
-            for i in range(len(m) - len(p) + 1):
-                if p[curr] == m[i]:
-                    start = i
 
-                    while curr < len(p) and start < len(m) and p[curr] == m[start]:
-                        curr += 1
-                        start += 1
-                    
-                    if curr == len(p):
-                        ans.append(i)
-                    
-                    curr = 0
-
+            curr = 0 #phrase ptr
+            i = 0 #word ptr
+            while i < len(word):
+                j = i
+                while j < len(word) and curr < len(phrase) and word[j] == phrase[curr]:
+                    j += 1
+                    curr += 1
+                
+                if curr == len(phrase):
+                    ans.append(i)
+                
+                curr = 0
+            
+                i += 1
+            
             return ans
         
-        a_matches = find_matches(s, a)
+
+
+        a_matches = find_matches(s,a)
         b_matches = find_matches(s, b)
 
-        # a_matches.sort()
-        # b_matches.sort()
+        final = []
 
-        # [16, 33]
-        # [4, 18]
-
-        fin = []
-        print(a_matches)
-        print(b_matches)
         for i in range(len(a_matches)):
             for j in range(len(b_matches)):
                 if abs(a_matches[i] - b_matches[j]) <= k:
-                    fin.append(a_matches[i])
+                    final.append(a_matches[i])
                     break
         
-        return sorted(fin)
-
-                
-
-
-
-
-    
+        return final
