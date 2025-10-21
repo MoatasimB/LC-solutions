5e4f4a858f83a5d1fdb0b66b7f4f27ec43c72a8b
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-10-21 17:41:54 -0400

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (35.30%) - Gitcode

--- 231-power-of-two/231. Power of Two.py
+++ 231-power-of-two/231. Power of Two.py
@@ -2,13 +2,15 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n == 0:
             return False
-        def rec(n):
+        def dfs(n):
             if n == 1:
                 return True
-            
             if n % 2:
                 return False
             
-            return rec(n // 2)
+            if dfs(n/2):
+                return True
+            
+            return False
         
-        return rec(n)
+        return dfs(n)
