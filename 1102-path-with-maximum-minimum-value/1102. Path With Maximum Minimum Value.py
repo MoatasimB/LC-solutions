e6886789b38e6f65cedf715c23bee667e152ae1b
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-10-21 23:49:04 -0400

    Runtime: 1063 ms (25.47%), Memory: 20.8 MB (16.62%) - Gitcode

--- 1102-path-with-maximum-minimum-value/1102. Path With Maximum Minimum Value.py
+++ 1102-path-with-maximum-minimum-value/1102. Path With Maximum Minimum Value.py
@@ -6,36 +6,18 @@ class Solution:
             return 0<=r<m and 0<=c<n
         dirs = [(0,1), (1,0), (0,-1), (-1,0)]
         
-        def check(mid):
-            if grid[0][0] < mid:
-                return False
-            seen = set()
-            seen.add((0,0))
-            def dfs(r, c):
-                if r == m - 1 and c == n - 1:
-                    return True
-                for dx, dy in dirs:
-                    nr, nc = r + dx, c + dy
-                    if valid(nr,nc) and (nr,nc) not in seen and grid[nr][nc] >= mid:
-                        seen.add((nr,nc))
-                        if dfs(nr,nc):
-                            return True
-                return False
-            return dfs(0,0)
+        ans = float("inf")
+        heap = [[-grid[0][0], 0, 0]] #cell val, r,c
+        seen = set()
+        while heap:
+            cell_val, r, c = heapq.heappop(heap)
+            ans = min(ans, -cell_val)
+            if r == m - 1 and c == n - 1:
+                return ans
+            
+            seen.add((r,c))
 
-        
-        
-        
-        l = min([min(row) for row in grid])
-        r = max([max(row) for row in grid])
-        ans = l
-        while l <= r:
-            mid = (l + r) // 2
-
-            if check(mid):
-                ans = mid
-                l = mid + 1
-            else:
-                r = mid - 1
-        
-        return ans
+            for dx, dy in dirs:
+                nr, nc = r + dx, c + dy
+                if valid(nr,nc) and (nr,nc) not in seen:
+                    heapq.heappush(heap, [-grid[nr][nc], nr, nc])
