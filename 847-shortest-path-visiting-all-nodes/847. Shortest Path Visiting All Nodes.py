a7e8f76b42767304f7d7e8c29bfb0ab2f1253100
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-10-24 19:12:33 -0400

    Runtime: 120 ms (38.74%), Memory: 22.5 MB (30.79%) - Gitcode

--- 847-shortest-path-visiting-all-nodes/847. Shortest Path Visiting All Nodes.py
+++ 847-shortest-path-visiting-all-nodes/847. Shortest Path Visiting All Nodes.py
@@ -1,25 +1,29 @@
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
-        
         n = len(graph)
         if n == 1:
             return 0
-        end = (1 << n) - 1
+        ending = (1 << n) - 1
         q = deque()
         seen = set()
-        for i in range(n):
-            q.append((i, 1 << i, 0))
-            seen.add((i,1 << i))
-        
+
+
+        for node in range(n):
+            q.append((node, 1 << node, 0))
+            seen.add((node, 1 << node))
 
         while q:
-            node, mask, step = q.popleft()
+            node, mask, steps = q.popleft()
 
-            if mask == end:
-                return step
-            
+            if mask == ending:
+                return steps
+
             for nei in graph[node]:
-                if (nei, mask | 1 << nei) not in seen:
-                    q.append((nei,mask | 1 << nei, step + 1 ))
-                    seen.add((nei, mask | 1 << nei))
+                if (nei, mask | (1 << nei)) not in seen:
+
+                    q.append((nei,mask | (1 << nei), steps + 1))
+                    seen.add((nei, mask | (1 << nei)))
+        
+
+
         
