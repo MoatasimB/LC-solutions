dba36fff5f4f226ba499bbdeb5043a1af2a322fc
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-10-26 23:03:29 -0400

    Runtime: 31 ms (5.23%), Memory: 18.4 MB (20.36%) - Gitcode

--- 692-top-k-frequent-words/692. Top K Frequent Words.py
+++ 692-top-k-frequent-words/692. Top K Frequent Words.py
@@ -1,29 +1,64 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        self.k = k
         class Node:
-            def __init__(self, count, word):
-                self.count = count
-                self.word = word
+            def __init__(self):
+                self.children = {}
+                self.end = False
+        
+        class Trie:
+            def __init__(self):
+                self.root = Node()
             
-            def __lt__(self, other):
-                if self.count != other.count:
-                    return self.count < other.count
-                return self.word > other.word
+            def add_word(self, word):
+                curr = self.root
+
+                for c in word:
+                    if c not in curr.children:
+                        curr.children[c] = Node()
+                    curr = curr.children[c]
+                curr.end = True
+            
+
+            def get_word(self,curr,word):
+                nonlocal k
+                if k == 0:
+                    return []
+                res = []
+                if curr.end:
+                    k -= 1
+                    res.append(word)
+
+                for i in range(26):
+                    c = chr(ord('a') + i)
+
+                    if c in curr.children:
+                        res += self.get_word(curr.children[c], word + c)
+
+                return res
+
+        n = len(words)
+        buckets = [[] for _ in range(n + 1)]
 
         freq = defaultdict(int)
+
         for word in words:
             freq[word] += 1
         
-        lst = []
-
         for word, count in freq.items():
-            heapq.heappush(lst, Node(count, word))
-            if len(lst) > k:
-                heapq.heappop(lst)
+            if len(buckets[count]) == 0:
+                buckets[count].append(Trie())
+            buckets[count][0].add_word(word)
+
+        ans = []
+
+        for i in range(n, -1, -1):
+            if k == 0:
+                return ans
+            if len(buckets[i]) == 1:
+                t = buckets[i][0]
+                ans.extend(t.get_word(t.root, ""))
         
-        # lst.sort(key= lambda x: (-x[0], x[1]))
+        return ans
 
-        # new_lst.sort(key = lambda x: (x[0], x[1]), reverse = True)
 
-        lst.sort(reverse = True)
-        return [p.word for p in lst]
