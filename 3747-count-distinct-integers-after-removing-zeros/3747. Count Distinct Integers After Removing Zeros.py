c8022e6771f87a4d26424a738ffc0ff378d90748
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-16 00:43:24 -0500

    Runtime: 0 ms (100.00%), Memory: 17.7 MB (100.00%) - Gitcode

---
+++ 3747-count-distinct-integers-after-removing-zeros/3747. Count Distinct Integers After Removing Zeros.py
@@ -0,0 +1,48 @@
+class Solution:
+    def countDistinct(self, n: int) -> int:
+        #number of elements with no 0s
+
+        str_n = str(n)
+        x = len(str_n)
+
+        # all numbers 1 digit less is just 9 + 9^2 + 9^3
+        ans = 0
+        for i in range(1, x):
+            ans += 9**i
+        
+        #Now we want numbers from the same number of digits up to the number n ie if n=4594 we want all 4 digits with only non 0s less than n
+
+        #we can break this apart to:
+            #first digit we can do 1,2,3 (3 choices)
+            #remaining digits is 9 choices each
+
+            #next digit we repeat the process we can do 1 2 3 4 (4 choices)
+            #remaining digits is 9 choicies each
+
+            #what this simulates is getting all the numbers 1000s,2000s,3000s, 4100s, 4200s, 4300s, 4400s, 4510s, 4520s, ..., 4580s, 4591,4592,4593
+            #then final check is number itself so
+
+        for i, s in enumerate(str_n):
+            digit = int(s)
+            if digit == 0:
+                break
+
+            limit = digit - 1
+
+            ans += limit * 9**(x-i-1) ##remaining places are 9 choices each
+        if "0" not in str_n:
+            ans += 1
+        return ans
+
+
+        # 4594
+
+        # 3* 9^3
+        # 4* 9^2
+        # 8 * 9
+        # 3
+
+        # 4029
+
+        
+
