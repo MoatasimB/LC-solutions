d10e913c885e0b63be55110bd19cc61ad4bdd5a4
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-18 18:54:09 -0500

    Runtime: 0 ms (100.00%), Memory: 18.9 MB (74.62%) - Gitcode

--- 34-find-first-and-last-position-of-element-in-sorted-array/34. Find First and Last Position of Element in Sorted Array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34. Find First and Last Position of Element in Sorted Array.py
@@ -1,39 +1,60 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
+        if not nums:
+            return [-1,-1]
         l = 0
-        r = len(nums) -1 
-        leftmost = -1
-
-        while l<=r:
-            mid = (l+r) // 2
-
-            if nums[mid] >= target:
-                if nums[mid] == target:
-                    leftmost = mid
-                r = mid - 1
-            else:
-                l = mid + 1
-        
-        
-        l = 0
-        r = len(nums) -1 
-        rightmost = -1
-
-        while l<=r:
-            mid = (l+r) // 2
-
-            if nums[mid] <= target:
-                if nums[mid] == target:
-                    rightmost = mid
+        r = len(nums) - 1
+        flag = False
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                flag = True
+                break
+            elif nums[mid] < target:
                 l = mid + 1
             else:
                 r = mid - 1
-        # rightmost = leftmost
-        # if leftmost != -1:
-        #     for i in range(leftmost, len(nums)):
-        #         if nums[i] != target:
-        #             break
-        #         rightmost = i
         
-        return [leftmost, rightmost]
+        if not flag:
+            return [-1,-1]
+        def firstO(target):
+            l = 0
+            r = len(nums) - 1
+            ans = -1
+            while l <= r:
+                mid = (l + r) // 2
+
+                if nums[mid] < target:
+                    ans = mid
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return ans
+        
+        def lastO(target):
+            l = 0
+            r = len(nums) - 1
+            ans = 0
+            while l <= r:
+                mid = (l + r) // 2
+
+                if nums[mid] <= target:
+                    ans = mid
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return ans
+        
+        f = firstO(target)
+        l = lastO(target)
+        # if f == 0:
+        #     return [f, l]
+        if f + 1 < len(nums) and nums[f + 1] == target:
+            return [f + 1,l]
+        
+        return [f, l]
+        # if f == -1 or l == -1:
+        #     return [-1,-1]
+        # if f + 1 < len(nums) and nums[f + 1] == target:
+        #     return [f + 1,l]
+        # return [-1,-1]
