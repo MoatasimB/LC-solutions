0bb0ab5eb700ed759cd3ff26af47ea1dc81be77c
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-19 15:52:05 -0500

    Runtime: 531 ms (8.27%), Memory: 124.4 MB (7.09%) - Gitcode

--- 221-maximal-square/221. Maximal Square.py
+++ 221-maximal-square/221. Maximal Square.py
@@ -1,21 +1,45 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        ans = 0
-        def dfs(i,j):
-            if i < 0 or j <0:
+        m = len(matrix)
+        n = len(matrix[0])
+        mat = [[0] * n for _ in range(m)]
+        mat[0][0] = int(matrix[0][0])
+        memo = {}
+        def dfs(r,c):
+            if (r,c) in memo:
+                return memo[(r,c)]
+            if r == 0 and c == 0:
+                return int(matrix[r][c])
+            
+            if r < 0 or c < 0:
                 return 0
-            if (i,j) in dp:
-                return dp[(i,j)]
-            dp[(i,j)] = 0
-            curr = min(dfs(i-1,j),dfs(i, j-1), dfs(i-1,j-1))
-
-            if matrix[i][j] == "1":
-                dp[(i,j)] = curr + 1
-            return dp[(i,j)]
             
-            
-      
+            curr = matrix[r][c]
+            top = dfs(r - 1, c)
+            left = dfs(r, c - 1)
+            diagonal = dfs(r-1, c-1)
+            if int(curr) == 1:
+                mat[r][c] = min(top, left, diagonal) + int(curr)
+            memo[(r, c)] = mat[r][c]
+            return mat[r][c]
+        
+        dfs(m-1, n-1)
+        print(mat)
+        return max(max(row) for row in mat) ** 2
 
-        dp = {}
-        dfs(len(matrix)-1, len(matrix[0])-1)
-        return max(dp.values())**2 if dp.values() else 0
+
+        [
+            ["1","0","1","1","0","1"],
+            ["1","1","1","1","1","1"],
+            ["0","1","1","0","1","1"],
+            ["1","1","1","0","1","0"],
+            ["0","1","1","1","1","1"],
+            ["1","1","0","1","1","1"]]
+
+        [
+            [1, 0, 1, 1, 0, 1], 
+            [1, 1, 1, 2, 1, 1], 
+            [0, 1, 2, 1, 2, 2], 
+            [1, 1, 2, 1, 2, 2], 
+            [0, 1, 2, 2, 2, 3], 
+            [1, 1, 1, 2, 3, 3]]
