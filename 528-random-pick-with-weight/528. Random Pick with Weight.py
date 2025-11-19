c1905c40ef38a17070b6f11dfe239dd7e38c2d9a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-18 19:21:55 -0500

    Runtime: 87 ms (36.63%), Memory: 23.5 MB (16.72%) - Gitcode

--- 528-random-pick-with-weight/528. Random Pick with Weight.py
+++ 528-random-pick-with-weight/528. Random Pick with Weight.py
@@ -1,34 +1,43 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.w = w
+        self.w = [w[0]]
+        for i in range(1, len(w)):
+            self.w.append(self.w[-1] + w[i])
+        
 
-        self.pre = [self.w[0]]
-        for num in self.w[1:]:
-            self.pre.append(self.pre[-1] + num)
-        
+
     def pickIndex(self) -> int:
-        rand = random.randint(1, self.pre[-1])
-        
         l = 0
-        r = len(self.pre) - 1
+        r = len(self.w) - 1
+
+        randNum = random.randint(1, self.w[-1])
+        if randNum <= self.w[0]:
+            return 0
         ans = 0
         while l <= r:
             mid = (l + r) // 2
-
-            if self.pre[mid] == rand:
-                return mid
-            elif self.pre[mid] < rand:
+            if self.w[mid] > randNum :
+                r = mid - 1
+            elif self.w[mid] < randNum:
+                ans = mid
                 l = mid + 1
             else:
-                ans = mid
-                r = mid - 1
-
-        return ans
-        # [1, 4]
-
+                return mid
+        
+        return ans + 1
 
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
+
+
+#  3 14 1 7
+
+#  3/25 14/25 1/25 7/25
+
+#  0 1. 2. 3
+#  3 17 18 25
+
+
