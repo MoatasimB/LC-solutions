2004bbcacb4356e30457809af1fcbd8da97a7bb7
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-18 19:02:06 -0500

    Runtime: 0 ms (100.00%), Memory: 18.9 MB (93.44%) - Gitcode

--- 34-find-first-and-last-position-of-element-in-sorted-array/34. Find First and Last Position of Element in Sorted Array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34. Find First and Last Position of Element in Sorted Array.py
@@ -1,60 +1,50 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
         if not nums:
             return [-1,-1]
-        l = 0
-        r = len(nums) - 1
-        flag = False
-        while l <= r:
-            mid = (l + r) // 2
-            if nums[mid] == target:
-                flag = True
-                break
-            elif nums[mid] < target:
-                l = mid + 1
-            else:
-                r = mid - 1
         
-        if not flag:
-            return [-1,-1]
-        def firstO(target):
+        def binSR(val): #rightmost position of target
+            
             l = 0
             r = len(nums) - 1
             ans = -1
             while l <= r:
                 mid = (l + r) // 2
-
-                if nums[mid] < target:
+                
+                if nums[mid] <= val:
                     ans = mid
                     l = mid + 1
                 else:
                     r = mid - 1
+            
             return ans
         
-        def lastO(target):
+        def binSL(val): #leftmost position of target
+            
             l = 0
             r = len(nums) - 1
-            ans = 0
+            ans = -1
             while l <= r:
                 mid = (l + r) // 2
-
-                if nums[mid] <= target:
-                    ans = mid
-                    l = mid + 1
+                
+                if nums[mid] < val:
+                    l = mid + 1                    
                 else:
                     r = mid - 1
+                    ans = mid
+            
             return ans
         
-        f = firstO(target)
-        l = lastO(target)
-        # if f == 0:
-        #     return [f, l]
-        if f + 1 < len(nums) and nums[f + 1] == target:
-            return [f + 1,l]
         
-        # return [f, l]
-        # if f == -1 or l == -1:
-        #     return [-1,-1]
-        # if f + 1 < len(nums) and nums[f + 1] == target:
-        #     return [f + 1,l]
-        # return [-1,-1]
+        final = [binSL(target), binSR(target)]
+        
+        print(final)
+        
+        if nums[final[0]] != target or nums[final[1]] != target:
+            return [-1,-1]
+          
+        
+        return final
+                    
+                    
