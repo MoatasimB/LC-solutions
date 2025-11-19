01c3d007bb05489f1b22261f13b5e33302ebbada
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-19 15:56:37 -0500

    Runtime: 148 ms (42.23%), Memory: 33 MB (58.05%) - Gitcode

--- 221-maximal-square/221. Maximal Square.py
+++ 221-maximal-square/221. Maximal Square.py
@@ -4,42 +4,47 @@ class Solution:
         n = len(matrix[0])
         mat = [[0] * n for _ in range(m)]
         mat[0][0] = int(matrix[0][0])
-        memo = {}
-        def dfs(r,c):
-            if (r,c) in memo:
-                return memo[(r,c)]
-            if r == 0 and c == 0:
-                return int(matrix[r][c])
-            
-            if r < 0 or c < 0:
-                return 0
-            
-            curr = matrix[r][c]
-            top = dfs(r - 1, c)
-            left = dfs(r, c - 1)
-            diagonal = dfs(r-1, c-1)
-            if int(curr) == 1:
-                mat[r][c] = min(top, left, diagonal) + int(curr)
-            memo[(r, c)] = mat[r][c]
-            return mat[r][c]
         
-        dfs(m-1, n-1)
-        print(mat)
+        for r in range(m):
+            for c in range(n):
+
+                top = 0
+                left = 0
+                diagonal = 0
+                if r > 0:
+                    top = mat[r - 1][c]
+                if c > 0:
+                    left = mat[r][c - 1]
+                if r > 0 and c > 0:
+                    diagonal = mat[r-1][c-1]
+                
+                curr = int(matrix[r][c])
+                if curr == 1:
+                    mat[r][c] = min(top, left, diagonal) + 1
+        
+
+
+        # memo = {}
+        # def dfs(r,c):
+        #     if (r,c) in memo:
+        #         return memo[(r,c)]
+        #     if r == 0 and c == 0:
+        #         return int(matrix[r][c])
+            
+        #     if r < 0 or c < 0:
+        #         return 0
+            
+        #     curr = matrix[r][c]
+        #     top = dfs(r - 1, c)
+        #     left = dfs(r, c - 1)
+        #     diagonal = dfs(r-1, c-1)
+        #     if int(curr) == 1:
+        #         mat[r][c] = min(top, left, diagonal) + int(curr)
+        #     memo[(r, c)] = mat[r][c]
+        #     return mat[r][c]
+        
+        # dfs(m-1, n-1)
         return max(max(row) for row in mat) ** 2
 
 
-        [
-            ["1","0","1","1","0","1"],
-            ["1","1","1","1","1","1"],
-            ["0","1","1","0","1","1"],
-            ["1","1","1","0","1","0"],
-            ["0","1","1","1","1","1"],
-            ["1","1","0","1","1","1"]]
-
-        [
-            [1, 0, 1, 1, 0, 1], 
-            [1, 1, 1, 2, 1, 1], 
-            [0, 1, 2, 1, 2, 2], 
-            [1, 1, 2, 1, 2, 2], 
-            [0, 1, 2, 2, 2, 3], 
-            [1, 1, 1, 2, 3, 3]]
+    
