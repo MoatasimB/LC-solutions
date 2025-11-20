bfa778df81eee766105d0e98bde31ce9fec9a464
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-19 23:56:36 -0500

    Runtime: 747 ms (20.96%), Memory: 24.3 MB (82.43%) - Gitcode

--- 912-sort-an-array/912. Sort an Array.py
+++ 912-sort-an-array/912. Sort an Array.py
@@ -1,44 +1,43 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         
+        def mergeSort(arr, left, right):
 
-        def mergeSort(l, r):
-            if l >= r:
+            if left>=right:
                 return
-
-            mid = (l + r) // 2
-            
-            mergeSort(l, mid)
-            mergeSort(mid + 1, r)
-
-            merge(l, mid, r)
+            mid = (left + right) // 2
+            mergeSort(arr, left, mid)
+            mergeSort(arr, mid + 1, right)
+            merge(arr, left, mid, right)
         
-        def merge(left, mid, right):
+        def merge(arr, left, mid, right):
+
             temp = []
+            start1 = left
+            start2 = mid + 1
 
-            i = left
-            j = mid + 1
-
-            while i <= mid and j <= right:
-                if nums[i] < nums[j]:
-                    temp.append(nums[i])
-                    i += 1
+            while start1 <= mid and start2 <= right:
+                if arr[start1] < arr[start2]:
+                    temp.append(arr[start1])
+                    start1 +=1
                 else:
-                    temp.append(nums[j])
-                    j += 1
-            
-            while i <= mid:
-                temp.append(nums[i])
-                i += 1
-            while j <= right:
-                temp.append(nums[j])
-                j += 1
-            
-            
+                    temp.append(arr[start2])
+                    start2 +=1
+
+
+            while start1 <= mid:
+                temp.append(arr[start1])
+                start1+=1
+
+            while start2 <= right:
+                temp.append(arr[start2])
+                start2+=1
+
+
             for i in range(left, right + 1):
-                nums[i] = temp[i - left]
-        
-        mergeSort(0, len(nums) - 1)
+                arr[i] = temp[i-left] 
+
+        mergeSort(nums, 0, len(nums) - 1)
 
         return nums
-                
+
