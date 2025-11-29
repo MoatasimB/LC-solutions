e58e5534a9537a27aaa46500741c736142f5e039
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-28 22:23:09 -0500

    Runtime: 5 ms (13.11%), Memory: 17.8 MB (56.48%) - Gitcode

--- 343-integer-break/343. Integer Break.py
+++ 343-integer-break/343. Integer Break.py
@@ -1,32 +1,19 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        #goal try to maximize the product
-        #min product is break it into n components of size 1
-        #if they are equal size of s and k components product is s^k
-        #if we go through the factors from 1...n (not 1 or n) and break it into
-        #k size components and then we can just take the maximum of all
-
-
-        # 1 1 1 1 1 1 1 1 1 1 1 1  
-   
         memo = {}
-        def dfs(n, k):
-            if (n, k) in memo:
-                return memo[(n, k)]
+        def dfs(num):
+            if num in memo:
+                return memo[num]
+            if num == 1:
+                return 1
             
-            if k == 1:
-                return n
-            ans = 0
-            for i in range(1, n):
-                ans = max(i * dfs(n - i, k - 1), ans)
+            ans = 0 if num == n else num
+
+            for i in range(1, num):
+                ans = max(ans, dfs(i) * dfs(num - i))
             
-            memo[(n,k)] = ans
+            memo[num] = ans
+        
             return ans
-        final = 0
-        if n == 2:
-            return 1
-        for k in range(2, n):
-            final = max(final, dfs(n, k))
-
-        return final
-
+        
+        return dfs(n)
