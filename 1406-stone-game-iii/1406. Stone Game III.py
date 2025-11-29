19b17b893b1043c24f6531c0796c1ce473c6fc0f
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-28 22:55:13 -0500

    Runtime: 1567 ms (33.52%), Memory: 167.4 MB (37.52%) - Gitcode

--- 1406-stone-game-iii/1406. Stone Game III.py
+++ 1406-stone-game-iii/1406. Stone Game III.py
@@ -1,29 +1,26 @@
 class Solution:
     def stoneGameIII(self, stoneValue: List[int]) -> str:
         memo = {}
-        def dfs(i, player):
+        def dfs(i):
             if i >= len(stoneValue):
                 return 0
 
-            if (i, player) in memo:
-                return memo[(i, player)]
+            if i in memo:
+                return memo[i]
             
             #each player can take up to 1-3 stones
-            score = float("-inf") if player == 0 else float("inf")
+            score = float("-inf")
             curr = 0
             for j in range(i, min(i + 3, len(stoneValue))):
-                if player == 0:
-                    curr += stoneValue[j]
-                    score = max(score, curr + dfs(j + 1, 1))
-                else:
-                    curr -= stoneValue[j]
-                    score = min(score, curr + dfs(j + 1, 0))
+                curr += stoneValue[j]
+                score = max(score, curr - dfs(j + 1))
+                
             
-            memo[(i, player)] = score
+            memo[i] = score
             return score
         
         #Alice score;
-        aliceScore = dfs(0,0)
+        aliceScore = dfs(0)
         if aliceScore > 0:
             return "Alice"
         elif aliceScore < 0:
