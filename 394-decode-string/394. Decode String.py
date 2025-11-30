d5254c28fbcf9fd95b3caea5d4d8e27a91203275
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-29 23:35:40 -0500

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (54.15%) - Gitcode

--- 394-decode-string/394. Decode String.py
+++ 394-decode-string/394. Decode String.py
@@ -1,31 +1,31 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
+        
+        str_stack = []
+        num_stack = []
+
+
+        num = 0
+        curr = ""
 
         for i in range(len(s)):
-            if s[i] == "]":
-                curr_str = []
-                while stack and stack[-1] != '[':
-                    curr_str.append(stack.pop())
-                stack.pop()
-                x = 0
-                y = len(curr_str) - 1
-                while x < y:
-                    curr_str[x], curr_str[y] = curr_str[y], curr_str[x]
-                    x +=1
-                    y -=1
-                curr_str = "".join(curr_str)
-                mul = 0
-                count = 0
-                while stack and stack[-1] in '0123456789':
-                    num = int(stack.pop())
-                    count += (10**mul)*num
-                    mul +=1
-                print(count)
-                curr_str = curr_str * count
-                stack.append(curr_str)
+            if s[i].isdigit():
+                num = (num * 10) + int(s[i])
+            elif s[i] == "[":
+                str_stack.append(curr)
+                num_stack.append(num)
+                curr = ""
+                num = 0
+            elif s[i] == "]":
+                subStr = str_stack.pop()
+                mult = num_stack.pop()
+                curr = mult * curr
+                subStr += curr
+                curr = subStr
+                num = 0
             else:
-                stack.append(s[i])
-        
-        return "".join(stack)
-            
+                curr += s[i]
+
+        return curr
+
+
