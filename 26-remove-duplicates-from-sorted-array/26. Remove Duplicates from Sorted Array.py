2da2aee65344fde6de671a59079269ea7f850977
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-30 22:51:28 -0500

    Runtime: 0 ms (100.00%), Memory: 19 MB (29.30%) - Gitcode

--- 26-remove-duplicates-from-sorted-array/26. Remove Duplicates from Sorted Array.py
+++ 26-remove-duplicates-from-sorted-array/26. Remove Duplicates from Sorted Array.py
@@ -1,12 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         
-
         curr = 1
 
         for i in range(1, len(nums)):
             if nums[i] != nums[curr - 1]:
-                nums[curr] = nums[i]
+                nums[curr], nums[i] = nums[i], nums[curr]
                 curr += 1
         
         return curr
