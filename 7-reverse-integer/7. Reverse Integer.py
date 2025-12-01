e8273e375e4a9fd9f3e0feb9813ef449610b7367
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-30 21:43:55 -0500

    Runtime: 40 ms (48.48%), Memory: 18 MB (16.51%) - Gitcode

--- 7-reverse-integer/7. Reverse Integer.py
+++ 7-reverse-integer/7. Reverse Integer.py
@@ -1,7 +1,7 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        MIN = -2147483648  # -2^31,
-        MAX = 2147483647  #  2^31 - 1
+        MIN = -(2**31) # -2^31,
+        MAX = (2**31) - 1  #  2^31 - 1
 
         res = 0
         while x:
