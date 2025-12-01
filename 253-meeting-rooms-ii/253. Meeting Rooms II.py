e9d0af042db012b461ef00aadbd7dc3ab0be460c
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-30 21:49:01 -0500

    Runtime: 0 ms (100.00%), Memory: 19.9 MB (74.96%) - Gitcode

--- 253-meeting-rooms-ii/253. Meeting Rooms II.py
+++ 253-meeting-rooms-ii/253. Meeting Rooms II.py
@@ -1,22 +1,13 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
+        intervals.sort(key = lambda x: x[0])
+        minHeap = [] #endTime
 
-        intervals.sort(key = lambda x : x[0])
-
-
-        rooms = []
-
-
-        rooms.append(intervals[0][1])
-
-        i = 1
-        while i < len(intervals):
-
-            if intervals[i][0] >= rooms[0]:
-                heapq.heappop(rooms)
-            
-            heapq.heappush(rooms, intervals[i][1])
-            i += 1
-        return len(rooms)
-
+        for i in range(len(intervals)):
+            start, end = intervals[i]
+            if minHeap and minHeap[0] <= start:
+                heapq.heappop(minHeap)
+            heapq.heappush(minHeap, end)
+        
+        return len(minHeap)
