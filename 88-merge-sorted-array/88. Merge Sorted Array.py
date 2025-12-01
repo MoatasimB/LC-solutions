3709d3d0a5e037345aacbd06f0b140c7703816ff
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-30 23:04:28 -0500

    Runtime: 0 ms (100.00%), Memory: 18.1 MB (25.38%) - Gitcode

--- 88-merge-sorted-array/88. Merge Sorted Array.py
+++ 88-merge-sorted-array/88. Merge Sorted Array.py
@@ -3,28 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        x = nums1[:]
+        
+        i = m - 1
+        j = n - 1
 
-        i = 0
-        j = 0
-        curr = 0
-        while i < m and j < n:
-            if x[i] < nums2[j]:
-                nums1[curr] = x[i]
-                i += 1
-                curr +=1
+        k = m + n - 1
+
+        while i >= 0 and j >= 0:
+
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                nums1[curr] = nums2[j]
-                j +=1
-                curr +=1
-        while i < m:
-            nums1[curr] = x[i]
-            curr += 1
-            i+=1
-
-        while j < n:
-            print(curr,j)
-            nums1[curr] = nums2[j]
-            curr += 1
-            j+=1
-
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        
+        while i >= 0:
+            nums1[k] = nums1[i]
+            i -= 1
+            k -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+        
