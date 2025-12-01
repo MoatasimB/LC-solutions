45c6c8a708e843e0333bbee17bd7b258a465e6c2
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-30 21:43:26 -0500

    Runtime: 41 ms (41.87%), Memory: 17.7 MB (82.68%) - Gitcode

--- 7-reverse-integer/7. Reverse Integer.py
+++ 7-reverse-integer/7. Reverse Integer.py
@@ -1,18 +1,41 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        
-        sign = -1 if x < 0 else 1
+        MIN = -2147483648  # -2^31,
+        MAX = 2147483647  #  2^31 - 1
 
-        ans = 0
-        x = abs(x)
+        res = 0
         while x:
-            digit = x % 10
+            digit = int(math.fmod(x, 10))
+            x = int(x / 10)
 
-            ans = ans * 10 + digit
-
-            if ans > (2**31) - 1:
+            if res > MAX // 10 or (res == MAX // 10 and digit > MAX % 10):
                 return 0
-            
-            x = x // 10
+            if res < MIN // 10 or (res == MIN // 10 and digit < MIN % 10):
+                return 0
+            res = (res * 10) + digit
+
+        return res
         
-        return sign * ans
+        # MAX_INT = 2**31 - 1
+        # MIN_INT = -(2**31)
+        
+        # maxThresHold = MAX_INT // 10
+        # maxThresHoldDigit = MAX_INT % 10
+
+        # minThres = int(MIN_INT / 10)
+        # minThresDigit = int(math.fmod(MIN_INT, 10))
+        # res = 0
+
+        # while x:
+        #     digit = int(math.fmod(x, 10))
+        #     x = int(x / 10)
+
+        #     if res * 10 > MAX_INT / 10 or (res * 10 == MAX_INT // 10 and digit > MAX_INT % 10):
+        #         return 0
+        #     if res * 10 < minThres or (res * 10 == minThres and digit < minThresDigit):
+        #         return 0
+
+
+        #     res = (res * 10) + digit
+        
+        # return res
