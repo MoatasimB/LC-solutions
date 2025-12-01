a482b9324cd83a9b1c56f04dc3c3f171ca42063e
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-30 23:16:50 -0500

    Runtime: 4 ms (67.35%), Memory: 21.3 MB (61.06%) - Gitcode

--- 347-top-k-frequent-elements/347. Top K Frequent Elements.py
+++ 347-top-k-frequent-elements/347. Top K Frequent Elements.py
@@ -1,17 +1,49 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = defaultdict(int)
-        for num in nums:
-            count[num] += 1
-        freq_element = [[freq, num] for num, freq in count.items()]
-
-        minHeap = []
-
-        for i in range(len(freq_element)):
-            heapq.heappush(minHeap, [freq_element[i][0], freq_element[i][1]])
-
-            if len(minHeap) > k:
-                heapq.heappop(minHeap)
         
+        counts = defaultdict(int)
+        
+        for num in nums:
+            counts[num] += 1
+        
+        unique = list(counts.keys())
 
-        return [ el for count, el in minHeap]
+        n = len(unique)
+        def quickselect(left, right):
+            if left >= right:
+                return
+            
+            p = random.randint(left, right)
+            
+            pivot = quicksort(left, right, p)
+            
+            if pivot == n - k:
+                return 
+            elif pivot < n - k:
+                quickselect(pivot + 1, right)
+            else:
+                quickselect(left, pivot - 1)
+        
+        
+        def quicksort(left, right, p):
+
+            freq = counts[unique[p]]
+
+            unique[p], unique[right] = unique[right], unique[p]
+
+            curr = left
+
+            for i in range(left, right):
+                if counts[unique[i]] < freq:
+                    unique[i], unique[curr] = unique[curr], unique[i]
+                    curr += 1
+
+            unique[curr], unique[right] = unique[right], unique[curr]
+
+            return curr
+
+
+        quickselect(0, n - 1)
+
+        return unique[n - k: ]
+            
