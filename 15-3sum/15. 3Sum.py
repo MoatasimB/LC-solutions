8504fa5f7917a4085b56327ac22947a160e046da
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-30 22:35:18 -0500

    Runtime: 789 ms (24.16%), Memory: 20.7 MB (70.84%) - Gitcode

--- 15-3sum/15. 3Sum.py
+++ 15-3sum/15. 3Sum.py
@@ -1,34 +1,41 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
         nums.sort()
         ans = []
-        def twoSum(l, r, num):            
+        
+        def twoSum(curr, l):
+            r = len(nums) - 1
+            
             while l < r:
-                if nums[l] + nums[r] + num == 0:
-                    ans.append([nums[l], nums[r], num])
-
-                    while l < r and nums[l] == nums[l + 1]:
-                        l += 1
+                
+                if nums[l] + nums[r] + curr == 0:
+                    ans.append([curr, nums[l], nums[r]])
                     l += 1
-                elif nums[l] + nums[r] + num > 0:
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                elif nums[l] + nums[r] + curr > 0:
                     r -= 1
                 else:
                     l += 1
-            
 
+                
+        
         i = 0
-        while i < (len(nums) - 1):
-            num = nums[i]
-
-            if num > 0:
-                break
+        
+        while i < len(nums) - 1:
             
-            twoSum(i + 1, len(nums) - 1, num)
-
-            while i < len(nums) - 1 and nums[i] == nums[i + 1]:
+            
+            if i < len(nums) - 1:
+                twoSum(nums[i], i + 1)
+            
+            while i < len(nums) -1 and nums[i] == nums[i + 1]:
                 i += 1
+        
             i += 1
         
         return ans
-            
-
+    
+    [0,0,0,0]
+    
+    
