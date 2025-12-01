39634fa3fab845cc51ef720ce7c522a494de8a49
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-30 23:59:34 -0500

    Runtime: 32 ms (14.92%), Memory: 19.2 MB (93.05%) - Gitcode

--- 42-trapping-rain-water/42. Trapping Rain Water.py
+++ 42-trapping-rain-water/42. Trapping Rain Water.py
@@ -1,54 +1,24 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-
-
+        
+        minLeft = [0]
         n = len(height)
-
-
-        leftMax = 0
-        rightMax = 0
-
-        l = 0
-        r = n - 1
-        ans = 0
-        while l <= r:
-
-            leftMax = max(leftMax, height[l])
-
-            rightMax = max(rightMax, height[r])
-
-            if leftMax < rightMax:
-                ans += max(0, leftMax - height[l])
-                l += 1
-            else:
-                ans += max(0, rightMax - height[r])
-                r -= 1
+        minRight = [0] * n
+        [0,0,0,0,0,0,0]
         
-        return ans
-
-
-
-
-
-
-        left = [0] * n
-        right = [0] * n
-
-        left[0] = height[0]
         for i in range(1, n):
-            left[i] = max(height[i], left[i-1])
+            h = height[i - 1]
+            minLeft.append(max(minLeft[-1], h))
         
-        right[-1] = height[-1]
-
         for i in range(n - 2, -1, -1):
-            right[i] = max(height[i], right[i + 1])
+            h = height[i + 1]
+            minRight[i] = (max(minRight[i+1], h))
         
-
+        # minRight.reverse()
         ans = 0
 
         for i in range(n):
-            threshold = min(left[i], right[i])
-
-            ans += max(0, threshold - height[i])
+            ans += max(0,min(minLeft[i], minRight[i]) - height[i])
         
         return ans
+        
