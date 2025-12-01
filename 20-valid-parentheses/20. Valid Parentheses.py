960ba6dc9038b3ecf085d00aab531d4e99dad1b0
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-30 23:40:13 -0500

    Runtime: 0 ms (100.00%), Memory: 18 MB (15.41%) - Gitcode

--- 20-valid-parentheses/20. Valid Parentheses.py
+++ 20-valid-parentheses/20. Valid Parentheses.py
@@ -1,26 +1,28 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        answer= []
-        for symbol in s:
-            if symbol == "(" or symbol == "{" or symbol == "[":
-                answer.append(symbol)
-            elif symbol == ")":
-                if answer and answer[-1] == "(":
-                    answer.pop()
-                else:
-                    answer.append(symbol)
-            elif symbol == "}":
-                if answer and answer[-1] == "{":
-                    answer.pop()
-                else:
-                    answer.append(symbol)
-            elif symbol == "]":
-                if answer and answer[-1] == "[":
-                    answer.pop()
-                else:
-                    answer.append(symbol)
+        
+        stack = []
 
-        if len(answer) == 0:
-            return True
-        else:
-            return False
+        mpp = {}
+
+        for ch in s:
+
+            if ch == ")":
+                if stack and stack[-1] == "(":
+                    stack.pop()
+                else:
+                    return False
+            elif ch == "]":
+                if stack and stack[-1] == "[":
+                    stack.pop()
+                else:
+                    return False
+            elif ch == "}":
+                if stack and stack[-1] == "{":
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(ch)
+        
+        return len(stack) == 0
