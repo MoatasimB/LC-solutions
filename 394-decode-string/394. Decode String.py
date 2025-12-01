a16f5842277047a71292ad411faaf4e8bb518ee6
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-30 22:21:21 -0500

    Runtime: 0 ms (100.00%), Memory: 18 MB (11.81%) - Gitcode

--- 394-decode-string/394. Decode String.py
+++ 394-decode-string/394. Decode String.py
@@ -1,6 +1,34 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         
+        i = 0
+
+        def dfs():
+            nonlocal i
+            num = 0
+            curr = ""
+            while i < len(s):
+
+                if s[i].isdigit():
+                    num = num * 10 + int(s[i])
+                elif s[i] == "[":
+                    i += 1
+                    tmp = dfs()
+                    curr += num * tmp
+                    num = 0
+                elif s[i] == "]":
+                    return curr
+                else:
+                    curr += s[i]
+                i += 1
+            return curr
+        
+        return dfs()
+        
+        
+        
+        
+        
         num_stack = []
         str_stack = []
 
