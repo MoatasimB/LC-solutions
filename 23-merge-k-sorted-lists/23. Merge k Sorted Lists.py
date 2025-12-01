54b054ac10697acb4c0dad6930a26775f47d1282
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-11-30 23:47:39 -0500

    Runtime: 8 ms (75.54%), Memory: 20.1 MB (71.63%) - Gitcode

--- 23-merge-k-sorted-lists/23. Merge k Sorted Lists.py
+++ 23-merge-k-sorted-lists/23. Merge k Sorted Lists.py
@@ -1,36 +1,39 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-    def __lt__(self, other):
-        return self.val < other.val
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
-        class HeapNode:
-            def __init__(self, node):
-                self.node = node
-            def __lt__(self, other):
-                return self.node.val < other.node.val
+        def merge(head1: Optional[ListNode], head2: Optional[ListNode]) -> Optional[ListNode]:
 
-        min_heap = []
-        dummy = ListNode(-1)
-        curr = dummy
+            dummy = ListNode(-1)
+            curr = dummy
 
-        for head in lists:
-            if head:
-                heapq.heappush(min_heap, [head.val, HeapNode(head)])
+            while head1 and head2:
+                if head1.val < head2.val:
+                    curr.next = head1
+                    head1 = head1.next
+                else:
+                    curr.next = head2
+                    head2 = head2.next
+                curr = curr.next
+            
+            if head1:
+                curr.next = head1
+            if head2:
+                curr.next = head2
+            return dummy.next
+
+        def mergesort(l: int, r: int) -> Optional[ListNode]:
+            if l == r:
+                return lists[l]
+            mid = (l + r) // 2
+            left = mergesort(l, mid)
+            right = mergesort(mid + 1, r)
+            
+            return merge(left, right)
         
-        while min_heap:
-            val, heap_node = heapq.heappop(min_heap)
-
-            curr.next = heap_node.node
-            nextNode = heap_node.node.next
-            if nextNode:
-                heapq.heappush(min_heap, [nextNode.val, HeapNode(nextNode)])
-            curr = curr.next 
-        
-        return dummy.next
+        return mergesort(0, len(lists) - 1)
