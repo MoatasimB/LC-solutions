155704702f1274862f4842371f13aa30c5cfebe6
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-15 01:32:12 -0500

    Runtime: 87 ms (86.57%), Memory: 18.6 MB (67.61%) - Gitcode

--- 427-construct-quad-tree/427. Construct Quad Tree.py
+++ 427-construct-quad-tree/427. Construct Quad Tree.py
@@ -12,49 +12,38 @@ class Node:
 
 class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':
+        n = len(grid)
 
+        def dfs(l, r, t, b):
+            isLeaf = True
+            node = Node(val = 0, topLeft = None, topRight = None, bottomLeft=None, bottomRight=None)
+            vals = set()
+            for i in range(t, b + 1):
+                for j in range(l, r + 1):
+                    vals.add(grid[i][j])
+                    if len(vals) > 1:
+                        isLeaf = False
+                        break
+                if not isLeaf:
+                    break
+            if isLeaf:
+                nodeVal = None
+                for val in vals:
+                    nodeVal = val
+                node.val = nodeVal
+                node.isLeaf = True
+            else:
+                midRow = (t + b) // 2
+                midCol = (l + r) // 2
+                node.topLeft = dfs(l, midCol, t, midRow)
+                node.topRight = dfs(midCol + 1, r, t, midRow)
+                node.bottomLeft = dfs(l, midCol, midRow + 1, b)
+                node.bottomRight = dfs(midCol + 1, r, midRow + 1, b)
+                node.isLeaf = False
 
-        def dfs(sR, sC, eR, eC):
-            n = eR - sR + 1
-            m = eC - sC + 1
-
-            if n * m == 1:
-                return Node(grid[sR][sC], True)
-            curr_vals = set()
-            only_val = None
-            for r in range(sR, eR + 1):
-                for c in range(sC, eC + 1):
-                    if len(curr_vals) == 0:
-                        curr_vals.add(grid[r][c])
-                        only_val = grid[r][c]
-                        continue
-                    elif grid[r][c] not in curr_vals:
-
-                        #make a new tree
-                        top = sR
-                        vertMid = (eR + sR) // 2
-
-                        bottom = eR
-
-                        left = sC
-                        right = eC
-                        horMid = (eC + sC) // 2
-
-                        print(f"From Row: {sR} to Row: {eR}\nFrom Col: {sC} to Col: {eC}")
-                        print("topleft",sR, sC, eR // 2, eC // 2)
-                        print("topright", sR, (sC + eC // 2) + 1, eR // 2, eC)
-                        print("bottomleft", (sR + eR // 2) + 1, sC, eR, eC // 2)
-                        print("bottomright", (sR + eR // 2) + 1, (sC + eC // 2) + 1, eR, eC)
-                        topleft = dfs(top, left, vertMid, horMid)
-                        topright = dfs(top, horMid + 1, vertMid, right)
-                        bottomleft =dfs(vertMid + 1, left, bottom, horMid)
-                        bottomright = dfs(vertMid + 1, horMid + 1, bottom, right)
-                        return Node(1, False, topleft, topright, bottomleft, bottomright)
-            
-            return Node(only_val, True)
+            return node
         
+        return dfs(0, n - 1, 0, n - 1)
 
-        return dfs(0,0, len(grid) - 1, len(grid[0]) - 1)
 
 
-        
