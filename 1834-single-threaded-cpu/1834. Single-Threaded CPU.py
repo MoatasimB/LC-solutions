840b47dd12a5ef7ad69d59084c0eafda2351d7c5
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-15 22:06:42 -0500

    Runtime: 644 ms (5.01%), Memory: 58.2 MB (49.87%) - Gitcode

---
+++ 1834-single-threaded-cpu/1834. Single-Threaded CPU.py
@@ -0,0 +1,26 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        
+        tasks = [[tasks[i][0], tasks[i][1], i] for i in range(len(tasks))]
+        # print(tasks)
+        tasks.sort()
+
+
+        toDo = []
+        time = tasks[0][0]
+        ans = []
+        while tasks or toDo:
+
+            while tasks and tasks[0][0] <= time:
+                enqTime, procTime, idx = heapq.heappop(tasks)
+                heapq.heappush(toDo, [procTime, idx])
+            
+            if toDo:
+                procTime, idx = heapq.heappop(toDo)
+                ans.append(idx)
+                time += procTime
+
+            if tasks and not toDo:
+                time = max(time, tasks[0][0])
+        
+        return ans
