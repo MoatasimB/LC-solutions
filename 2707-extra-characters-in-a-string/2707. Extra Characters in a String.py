a1c9ccf5dc3463de24f4d23c906b4eb4bf0959c4
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-16 16:04:34 -0500

    Runtime: 664 ms (5.00%), Memory: 22.7 MB (7.12%) - Gitcode

--- 2707-extra-characters-in-a-string/2707. Extra Characters in a String.py
+++ 2707-extra-characters-in-a-string/2707. Extra Characters in a String.py
@@ -1,7 +1,31 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+    
+    def add(self, word):
+        curr = self.root
+
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = Node()
+            curr = curr.children[ch]
+        
+        curr.end = True
+            
+
+
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        
+        trie = Trie()
         dictionary = set(dictionary)
+        for word in dictionary:
+            trie.add(word)
+
         memo = {}
         def dfs(i, curr):
             if (i, curr) in memo:
@@ -11,10 +35,24 @@ class Solution:
             
 
             ans = len(s)
+
+            node = trie.root
+
             for j in range(i, len(s)):
-                if s[i: j + 1] in dictionary:
-                    ans = min(ans, dfs(j + 1, curr))
-                ans = min(ans, dfs(j+1, curr + j - i +1 ))
+                ch = s[j]
+                ans = min(ans, dfs(j + 1, curr + j - i + 1))
+                if ch in node.children:
+                    node = node.children[ch]
+                    if node.end:
+                        ans = min(ans, dfs(j + 1, curr))
+                else:
+                    break
+
+
+
+                # if s[i: j + 1] in dictionary:
+                #     ans = min(ans, dfs(j + 1, curr))
+                # ans = min(ans, dfs(j+1, curr + j - i +1 ))
             memo[(i, curr)] = ans
             return ans
         
