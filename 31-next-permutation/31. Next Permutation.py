0f5c19614015d989b19971d59a7ddedd82dbb5e9
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-20 01:13:02 -0500

    Runtime: 0 ms (100.00%), Memory: 17.2 MB (99.90%) - Gitcode

--- 31-next-permutation/31. Next Permutation.py
+++ 31-next-permutation/31. Next Permutation.py
@@ -3,62 +3,40 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # go from back to front till numbers are not increasing
-        # first number that is not increasing we want to switch
-        #find the smallest number to the right that is greater than our number
-        # reverse second half
-
-        if len(nums) == 1:
-            return
-
-        i = len(nums) - 2
-        while i > 0:
-            if nums[i] < nums[i + 1]:
-                break
-            i -= 1
         
-        if i == 0 and nums[0] == max(nums):
-            nums.reverse()
-            return
+        #[.   x  n, n - 1, n - 2, n - 3]
+        def swap(i, j):
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+
+        i = 0
+        j = len(nums) - 2
+
+        while j >= 0:
+            nextNum = nums[j + 1]
+            currNum = nums[j]
+
+            if nextNum <= currNum:
+                j -= 1
+                continue
+            else:
+                #currNum is smaller
+                #find the first num from the end that is greater than it
+                x = len(nums) - 1
+
+                while x >= 0 and nums[x] <= currNum:
+                    x -= 1
+                
+                nums[x], nums[j] = nums[j], nums[x]
+                swap(j + 1, len(nums) - 1)
+                return
         
-        smallestR = float('inf')
+        i = 0
         j = len(nums) - 1
-        idxSwitch = len(nums) - 1
-        while j > i:
-            if nums[j] > nums[i]:
-                if nums[j] < smallestR:
-                    smallestR = nums[j]
-                    idxSwitch = j
-            j -= 1
+
+        swap(i, j)
         
-        nums[i], nums[idxSwitch] = nums[idxSwitch], nums[i]
 
-        k = i + 1
-        m = len(nums) - 1
-
-        while k <= m:
-            nums[k], nums[m] = nums[m], nums[k]
-            k += 1
-            m -= 1
-
-
-
-        
-        # 1 2 3 4
-
-        # 1 2 4 3    
-
-        # 1 3 2 4
-
-        # 1 3 4 2
-
-        # 1 4 2 3
-
-        # 1 4 3 2
-
-        # 2 1 3 4
-
-        # 2 1 4 3
-
-        # 2 
-
+  
