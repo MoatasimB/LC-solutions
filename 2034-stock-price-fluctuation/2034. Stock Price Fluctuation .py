b9ec1ad6d4335e51e7af6e88167c10c293346136
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-19 22:52:55 -0500

    Runtime: 200 ms (48.68%), Memory: 68.8 MB (5.35%) - Gitcode

--- 2034-stock-price-fluctuation/2034. Stock Price Fluctuation .py
+++ 2034-stock-price-fluctuation/2034. Stock Price Fluctuation .py
@@ -1,31 +1,30 @@
 class StockPrice:
 
     def __init__(self):
-        self.mpp = {}
-        self.maxHeap = []
-        self.minHeap = []
-        self.currTime = 0
+        self.history = {}
+        self.last = 0
+        self.max = []
+        self.min = []
+
     def update(self, timestamp: int, price: int) -> None:
-        self.mpp[timestamp] = price
-        heapq.heappush(self.minHeap, [price, timestamp])
-        heapq.heappush(self.maxHeap, [-price, timestamp])
-        self.currTime = max(self.currTime, timestamp)
+        self.history[timestamp] = price
+        heapq.heappush(self.max, [-price, timestamp])
+        heapq.heappush(self.min, [price, timestamp])
+
+        self.last = max(self.last, timestamp)
+
     def current(self) -> int:
-        return self.mpp[self.currTime]
+        return self.history[self.last]
+        
 
     def maximum(self) -> int:
-
-        while self.maxHeap and -self.maxHeap[0][0] != self.mpp[self.maxHeap[0][1]]:
-            heapq.heappop(self.maxHeap)
-        return -self.maxHeap[0][0]
-
+        while self.max and -self.max[0][0] != self.history[self.max[0][1]]:
+            heapq.heappop(self.max)
+        return -self.max[0][0]
     def minimum(self) -> int:
-        #[price, timestamp]
-        #check if mpp[timestamp] = price (if it does not this was invalidated)
-        while self.minHeap and self.minHeap[0][0] != self.mpp[self.minHeap[0][1]]:
-            heapq.heappop(self.minHeap)
-        return self.minHeap[0][0]
-
+        while self.min and self.min[0][0] != self.history[self.min[0][1]]:
+            heapq.heappop(self.min)
+        return self.min[0][0]
 
 # Your StockPrice object will be instantiated and called as such:
 # obj = StockPrice()
