5aae4acb366650c279d68b5cda153a204647a55e
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-19 22:41:26 -0500

    Runtime: 243 ms (10.91%), Memory: 23.6 MB (63.23%) - Gitcode

---
+++ 911-online-election/911. Online Election.py
@@ -0,0 +1,48 @@
+class TopVotedCandidate:
+
+    def __init__(self, persons: List[int], times: List[int]):
+        self.leading = [] #[time, leading]
+        leader = persons[0]
+        freqs = defaultdict(int)
+        freqs[leader] += 1
+        maxF = 1
+        self.leading.append([times[0], leader])
+        for i in range(1, len(persons)):
+            time = times[i]
+            person = persons[i]
+
+            freqs[person] += 1
+
+            if freqs[person] >= maxF:
+                maxF = freqs[person]
+                leader = person
+            
+            self.leading.append([time, leader])
+    
+
+                
+        print(self.leading)
+    def q(self, t: int) -> int:
+        #time <= t
+        l = 0
+        r = len(self.leading) - 1
+        ans = None
+        while l <= r:
+            mid = (l + r) // 2
+
+            if self.leading[mid][0] == t:
+                return self.leading[mid][1]
+            elif self.leading[mid][0] < t:
+                ans = self.leading[mid][1]
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        return ans
+
+        
+
+
+# Your TopVotedCandidate object will be instantiated and called as such:
+# obj = TopVotedCandidate(persons, times)
+# param_1 = obj.q(t)
