445e1f369619554f90f4a1e454287cc0e6babd57
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-21 22:05:28 -0500

    Runtime: 0 ms (100.00%), Memory: 18.4 MB (98.78%) - Gitcode

--- 34-find-first-and-last-position-of-element-in-sorted-array/34. Find First and Last Position of Element in Sorted Array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34. Find First and Last Position of Element in Sorted Array.py
@@ -1,58 +1,38 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return [-1,-1]
+        if not nums or target < nums[0] or target > nums[len(nums) - 1]:
+            return [-1, -1]
         
-        def firstO(target):
-            l = 0
-            r = len(nums) - 1
-            ans = -1
-            while l <= r:
-                mid = (l + r) // 2
+        l = 0
+        r = len(nums) - 1
+        ans = 0
+        while l <= r:
+            mid = (l + r) // 2
 
-                if nums[mid] == target:
-                    if mid == l or nums[mid - 1] < target:
-                        return mid
-                    else:
-                        r = mid - 1
-                elif nums[mid] < target:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            
-            return -1
+            if nums[mid] >= target:
+                ans = mid
+                r = mid - 1
+            else:
+                l = mid + 1
         
-        def lastO(target):
-            l = 0
-            r = len(nums) - 1
-            ans = 0
-            while l <= r:
-                mid = (l + r) // 2
+        if nums[ans] != target:
+            return [-1, -1]
+        
+        left = ans
 
-                if nums[mid] == target:
-                    if mid == r or nums[mid + 1] > target:
-                        return mid
-                    else:
-                        l = mid + 1
-                elif nums[mid] <= target:
-                    ans = mid
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            return -1
+        l = 0
+        r = len(nums) - 1
+        ans = 0
+        while l <= r:
+            mid = (l + r) // 2
+
+            if nums[mid] <= target:
+                ans = mid
+                l = mid + 1
+            else:
+                r = mid - 1
         
-        f = firstO(target)
-        l = lastO(target)
-        if f == -1 or l == -1 : return [-1,-1]
-        return [f,l]
-        # if f == 0:
-        #     return [f, l]
-        if f + 1 < len(nums) and nums[f + 1] == target:
-            return [f + 1,l]
-        
-        # return [f, l]
-        # if f == -1 or l == -1:
-        #     return [-1,-1]
-        # if f + 1 < len(nums) and nums[f + 1] == target:
-        #     return [f + 1,l]
-        # return [-1,-1]
+        if nums[ans] != target:
+            return [-1, -1]
+
+        return [left, ans]
