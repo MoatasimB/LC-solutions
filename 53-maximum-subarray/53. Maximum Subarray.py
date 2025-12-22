41772985a5933f28745031e03442040842d4ee50
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-21 22:24:17 -0500

    Runtime: 58 ms (64.30%), Memory: 30.2 MB (97.26%) - Gitcode

--- 53-maximum-subarray/53. Maximum Subarray.py
+++ 53-maximum-subarray/53. Maximum Subarray.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
+        curr = 0
         ans = float("-inf")
 
-        curr = 0
-        
         for num in nums:
             if curr < 0:
                 curr = 0
@@ -12,4 +11,4 @@ class Solution:
             curr += num
             ans = max(ans, curr)
         
-        return max(ans, max(nums))
+        return ans
