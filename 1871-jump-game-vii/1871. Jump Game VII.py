760a910259aa25df40099d2e007e9b0b0f54dcfb
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-22 20:05:51 -0500

    Runtime: 383 ms (6.85%), Memory: 35.7 MB (5.18%) - Gitcode

--- 1871-jump-game-vii/1871. Jump Game VII.py
+++ 1871-jump-game-vii/1871. Jump Game VII.py
@@ -1,61 +1,26 @@
 class Solution:
     def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
         
-
         n = len(s)
+
         if s[n - 1] == "1":
             return False
-        
         q = deque()
         seen = set()
 
-        q.append(0)
+        q.append([s[0], 0])
         seen.add(0)
         farthest = 0
-        while q:
-            idx = q.popleft()
+        while q:    
+            val, idx = q.popleft()
 
-            if idx >= n - 1:
+            if idx == n - 1:
                 return True
             
-            for j in range(max(farthest,idx + minJump), min(n, idx + maxJump + 1)):
-                if s[j] == "0" and j not in seen:
+            for j in range(max(farthest,idx + minJump), min(idx + maxJump + 1, n)):
+                if j not in seen and s[j] == "0":
                     seen.add(j)
-                    q.append(j)
-            farthest = max(farthest, idx + maxJump)
+                    q.append([s[j], j])
+            farthest = max(farthest, idx + maxJump + 1)
+        
         return False
-
-
-
-        memo = {}
-        def dfs(i):
-            if i in memo:
-                return memo[i]
-            if i >= n - 1:
-                return True
-            
-            for j in range(i + minJump, min(n, i + maxJump + 1)):
-                if s[j] == "0" and dfs(j):
-                    memo[i] = True
-                    return True
-            memo[i] = False
-            return False
-        
-        return dfs(0)
-            
-        # if s[n - 1] == "1":
-        #     return False
-        
-        # goal = n - 1
-
-        # for i in range(n - 2, -1, -1):
-        #     if s[i] == "1":
-        #         continue
-            
-        #     if i + minJump <= goal <= i + maxJump:
-        #         goal = i
-        
-        # return goal == 0
-
-
-        # 00111010
