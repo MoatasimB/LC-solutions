4f8b3c9dbcf07bda78a58b4f289ffedb94776bbb
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-24 18:24:10 -0500

    Runtime: 655 ms (37.77%), Memory: 18.9 MB (83.19%) - Gitcode

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312. Minimum Insertion Steps to Make a String Palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312. Minimum Insertion Steps to Make a String Palindrome.py
@@ -3,21 +3,35 @@ class Solution:
         
         n = len(s)
         s2 = s[::-1]
-        memo = {}
-        def dfs(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
-            if i == -1 or j == -1:
-                return 0
 
-            ans = 0
-            if s[i] == s2[j]:
-                ans = 1 + dfs(i - 1, j - 1)
-            else:
-                ans = max(dfs(i - 1, j), dfs(i, j - 1))
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
 
-            memo[(i, j)] = ans
-            return ans
-        
-        LCS = dfs(n - 1, n - 1)
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if s[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        LCS = dp[n][n]
+
         return n - LCS
+
+        # memo = {}
+        # def dfs(i, j):
+        #     if (i, j) in memo:
+        #         return memo[(i, j)]
+        #     if i == -1 or j == -1:
+        #         return 0
+
+        #     ans = 0
+        #     if s[i] == s2[j]:
+        #         ans = 1 + dfs(i - 1, j - 1)
+        #     else:
+        #         ans = max(dfs(i - 1, j), dfs(i, j - 1))
+
+        #     memo[(i, j)] = ans
+        #     return ans
+        
+        # LCS = dfs(n - 1, n - 1)
+        # return n - LCS
