2e86756cb57db667c3d10c0929bf988a113e6b82
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-24 18:26:05 -0500

    Runtime: 539 ms (67.99%), Memory: 17.5 MB (98.68%) - Gitcode

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312. Minimum Insertion Steps to Make a String Palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312. Minimum Insertion Steps to Make a String Palindrome.py
@@ -4,16 +4,18 @@ class Solution:
         n = len(s)
         s2 = s[::-1]
 
-        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        prev = [0] * (n + 1)
 
         for i in range(1, n + 1):
+            curr = [0] * (n + 1)
             for j in range(1, n + 1):
                 if s[i - 1] == s2[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                    curr[j] = 1 + prev[j - 1]
                 else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                    curr[j] = max(prev[j], curr[j - 1])
+            prev = curr
 
-        LCS = dp[n][n]
+        LCS = prev[n]
 
         return n - LCS
 
