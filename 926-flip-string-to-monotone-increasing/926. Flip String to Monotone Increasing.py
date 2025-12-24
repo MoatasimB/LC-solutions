7510846cad7a105563462d3e71066454bc5b424c
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-24 17:52:34 -0500

    Runtime: 158 ms (33.93%), Memory: 17.8 MB (93.75%) - Gitcode

--- 926-flip-string-to-monotone-increasing/926. Flip String to Monotone Increasing.py
+++ 926-flip-string-to-monotone-increasing/926. Flip String to Monotone Increasing.py
@@ -4,33 +4,33 @@ class Solution:
         #everything including position i and before is == s[i]
         #everything after s[i] is 1
         n = len(s)
-        suffix = [0] * n
-        suffix[n -1] = int(s[n - 1])
-        for i in range(n - 2, -1, -1):
-            suffix[i] = suffix[i + 1] + int(s[i])
+        # suffix = [0] * n
+        # suffix[n -1] = int(s[n - 1])
+        # for i in range(n - 2, -1, -1):
+        #     suffix[i] = suffix[i + 1] + int(s[i])
         
-        # [22210]
-        ans = float("inf")
-        zero_count_front = 0
-        one_count_front = 0
-        for i in range(len(s)):
-            zero_count_back = (n - i - 1 - suffix[i + 1]) if i + 1 < n else 0
-            one_count_back = suffix[i + 1] if i + 1 < n else 0
+        # # [22210]
+        # ans = float("inf")
+        # zero_count_front = 0
+        # one_count_front = 0
+        # for i in range(len(s)):
+        #     zero_count_back = (n - i - 1 - suffix[i + 1]) if i + 1 < n else 0
+        #     one_count_back = suffix[i + 1] if i + 1 < n else 0
             
-            if s[i] == "0":
-                before = one_count_front
-                after = zero_count_back
+        #     if s[i] == "0":
+        #         before = one_count_front
+        #         after = zero_count_back
 
-                ans = min(ans, before + after)
-                zero_count_front += 1
-            else:
-                before = one_count_front
-                after = zero_count_back
-                ans = min(ans, before + after)
-                one_count_front += 1
+        #         ans = min(ans, before + after)
+        #         zero_count_front += 1
+        #     else:
+        #         before = one_count_front
+        #         after = zero_count_back
+        #         ans = min(ans, before + after)
+        #         one_count_front += 1
         
 
-        return ans
+        # return ans
         # 22210 
         # 00110
 
@@ -38,3 +38,17 @@ class Solution:
         # ones = 0
         
 
+        zerosR = 0
+        ans = float("inf")
+        for i in range(n):
+            if s[i] == "0":
+                zerosR += 1
+        onesL = 0
+        for i in range(n):
+            if s[i] == "0":
+                zerosR -= 1
+                ans = min(ans, zerosR + onesL)
+            else:
+                ans = min(ans, zerosR + onesL)
+                onesL += 1
+        return ans
