45f88d926060f81f2fb850c93d61e9572ecb21ec
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-25 13:59:57 -0500

    Runtime: 1529 ms (35.63%), Memory: 40.3 MB (97.12%) - Gitcode

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290. Minimum Obstacle Removal to Reach Corner.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minimumObstacles(self, grid: List[List[int]]) -> int:
+        
+        m = len(grid)
+        n = len(grid[0])
+        
+        dirs = [(0, 1), (1,0), (0,-1), (-1,0)]
+
+        def valid(r, c):
+            return 0<= r < m and 0<= c <n
+        
+        dists = [[float("inf")] * n for _ in range(m)]
+
+        dists[0][0] = 0
+
+        minHeap = [[0, 0, 0]] #cost, r, c
+
+        while minHeap:
+            cost, r, c = heapq.heappop(minHeap)
+
+            if dists[r][c] < cost:
+                continue
+            if r == m - 1 and c == n - 1:
+                return cost
+            for dx, dy in dirs:
+                nr = r + dx
+                nc = c + dy
+
+                if valid(nr, nc):
+                    if grid[nr][nc] == 1:
+                        newCost = 1 + cost
+                        if dists[nr][nc] > newCost:
+                            dists[nr][nc] = newCost
+                            heapq.heappush(minHeap, [newCost, nr, nc])
+                    else:
+                        if dists[nr][nc] > cost:
+                            dists[nr][nc] = cost
+                            heapq.heappush(minHeap, [cost, nr, nc])
+        
