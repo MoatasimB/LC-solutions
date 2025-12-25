49373e4c3ad401ee1bb49f6d266af6114866b49a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-24 22:39:52 -0500

    Runtime: 27 ms (27.39%), Memory: 19.1 MB (95.80%) - Gitcode

--- 42-trapping-rain-water/42. Trapping Rain Water.py
+++ 42-trapping-rain-water/42. Trapping Rain Water.py
@@ -1,54 +1,24 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-
-
         n = len(height)
-
-
-        leftMax = 0
-        rightMax = 0
+        leftSide = 0
+        rightSide = 0
 
         l = 0
         r = n - 1
         ans = 0
-        while l <= r:
+        while l < r:
+            leftSide = max(leftSide, height[l])
+            rightSide = max(rightSide, height[r])
 
-            leftMax = max(leftMax, height[l])
-
-            rightMax = max(rightMax, height[r])
-
-            if leftMax < rightMax:
-                ans += max(0, leftMax - height[l])
+            if leftSide < rightSide:
+                ans += max(0, leftSide - height[l])
                 l += 1
             else:
-                ans += max(0, rightMax - height[r])
+                ans += max(0, rightSide - height[r])
                 r -= 1
+            
         
-        return ans
-
-
-
-
-
-
-        left = [0] * n
-        right = [0] * n
-
-        left[0] = height[0]
-        for i in range(1, n):
-            left[i] = max(height[i], left[i-1])
         
-        right[-1] = height[-1]
-
-        for i in range(n - 2, -1, -1):
-            right[i] = max(height[i], right[i + 1])
-        
-
-        ans = 0
-
-        for i in range(n):
-            threshold = min(left[i], right[i])
-
-            ans += max(0, threshold - height[i])
         
         return ans
