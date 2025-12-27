5b9e019b1da8ddba1cbe8554d702d5d2bf7787e6
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-26 23:32:50 -0500

    Runtime: 2 ms (57.41%), Memory: 17.6 MB (96.65%) - Gitcode

--- 4-median-of-two-sorted-arrays/4. Median of Two Sorted Arrays.py
+++ 4-median-of-two-sorted-arrays/4. Median of Two Sorted Arrays.py
@@ -5,38 +5,32 @@ class Solution:
 
         if n1 > n2:
             return self.findMedianSortedArrays(nums2, nums1)
+
         
-
         total = n1 + n2
-
         half = total // 2
 
         l = 0
         r = n1
 
-        # [1 2 3 4 5]
-        # [9 10 11 13]
-
         while l <= r:
             midA = (l + r) // 2
             midB = half - midA
 
-            preA = nums1[midA - 1] if midA - 1 >= 0 else float("-inf")
-            aftA = nums1[midA] if midA < n1 else float("inf")
+            leftA = nums1[midA - 1] if midA - 1 >= 0 else float("-inf")
+            rightA = nums1[midA] if midA < n1 else float("inf")
+            
+            leftB = nums2[midB - 1] if midB - 1 >= 0 else float("-inf")
+            rightB = nums2[midB] if midB < n2 else float("inf")
 
-            preB = nums2[midB - 1] if midB - 1 >= 0 else float("-inf")
-            aftB = nums2[midB] if midB < n2 else float("inf")
 
-
-            if preA <= aftB and preB <= aftA:
-                #two halves are sorted
+            if leftA <= rightB and leftB <= rightA:
                 if total % 2 == 0:
-                    return (max(preA, preB) + min(aftA, aftB)) / 2
+                    return (max(leftA, leftB) + min(rightA, rightB)) / 2
                 else:
-                    return min(aftA, aftB)
-            elif preA > aftB:
+                    return min(rightA, rightB)
+            elif leftA > rightB:
                 r = midA - 1
             else:
                 l = midA + 1
-
-
+            
