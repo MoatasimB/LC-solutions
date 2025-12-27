5f0feb73da2885bd0cc863c210dc45bde4bcc2f8
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-26 23:01:18 -0500

    Runtime: 45 ms (32.28%), Memory: 28.3 MB (80.31%) - Gitcode

--- 2366-minimum-replacements-to-sort-the-array/2366. Minimum Replacements to Sort the Array.py
+++ 2366-minimum-replacements-to-sort-the-array/2366. Minimum Replacements to Sort the Array.py
@@ -1,26 +1,21 @@
 class Solution:
     def minimumReplacement(self, nums: List[int]) -> int:
         
-
         n = len(nums)
         ans = 0
         for i in range(n - 2, -1, -1):
-            nextNum = nums[i + 1]
             curr = nums[i]
+            nextN = nums[i + 1]
 
-            if nextNum >= curr:
-                continue
-            
-            if curr % nextNum == 0:
-                numElements = curr // nextNum
-                ans += numElements - 1
+            if curr > nextN:
 
-                nums[i] = curr // numElements
-            
-            else:
-                numElements = math.ceil(curr / nextNum)
-                ans += numElements - 1
-                nums[i] = curr // numElements
+                if curr % nextN == 0:
+                    numElements = curr // nextN
+                    nums[i] = curr // numElements
+                    ans += numElements - 1
+                else:
+                    numElements = math.ceil(curr / nextN)
+                    nums[i] = curr // numElements
+                    ans += numElements - 1
         
         return ans
-
