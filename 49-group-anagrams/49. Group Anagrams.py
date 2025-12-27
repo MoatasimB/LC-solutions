ffa56a43647193443773d19e8d7cd6d58343fec2
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-26 22:41:49 -0500

    Runtime: 7 ms (97.90%), Memory: 20.5 MB (83.63%) - Gitcode

--- 49-group-anagrams/49. Group Anagrams.py
+++ 49-group-anagrams/49. Group Anagrams.py
@@ -1,12 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mpp = defaultdict(list)
+        
+        self.mpp = defaultdict(list)
 
-        for i in range(len(strs)):
-            key = [0] * 26
-            for ch in strs[i]:
-                key[ord(ch) - ord('a')] +=1
-            
-            mpp[tuple(key)].append(strs[i])
-        ans = [val for val in mpp.values()]
-        return ans
+
+        for word in strs:
+            key = sorted(word)
+            self.mpp[tuple(key)].append(word)
+        
+
+        return list(self.mpp.values())
