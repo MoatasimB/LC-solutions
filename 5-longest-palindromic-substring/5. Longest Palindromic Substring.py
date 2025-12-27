76577fa8aa0f6bfd6e8d1e8150c61f157e3348e5
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-26 23:08:58 -0500

    Runtime: 249 ms (88.22%), Memory: 17.3 MB (98.19%) - Gitcode

--- 5-longest-palindromic-substring/5. Longest Palindromic Substring.py
+++ 5-longest-palindromic-substring/5. Longest Palindromic Substring.py
@@ -1,33 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-        i = 0
-        start = 0
-        finish = 0
-        curr = 0
-        while i < len(s):
 
+        n = len(s)
+        currMax = 0
+        currStartIdx = -1
+        for i in range(n):
             l = i
             r = i
 
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r-l)+1 > curr:
-                    curr = (r-l) + 1
-                    start = l
-                    finish = r
-                l -=1
-                r +=1
-            
+            while l  >= 0 and r < n and s[l] == s[r]:
+                if (r - l + 1) > currMax:
+                    currMax = (r - l + 1)
+                    currStartIdx = l
+                l -= 1
+                r += 1
+
             l = i
             r = i + 1
 
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r-l)+1 > curr:
-                    curr = (r-l) + 1
-                    start = l
-                    finish = r
-                l -=1
-                r +=1
-            i+=1
+            while l  >= 0 and r < n and s[l] == s[r]:
+                if (r - l + 1) > currMax:
+                    currMax = (r - l + 1)
+                    currStartIdx = l
+                l -= 1
+                r += 1
         
-        return s[start:finish+1]
+
+        return s[currStartIdx : currStartIdx + currMax]
