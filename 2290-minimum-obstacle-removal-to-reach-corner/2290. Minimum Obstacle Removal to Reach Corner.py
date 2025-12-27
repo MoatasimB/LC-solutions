de92009a25ef923c5698936dcf75e9208aaf4923
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-26 22:25:45 -0500

    Runtime: 1542 ms (36.69%), Memory: 44.6 MB (50.44%) - Gitcode

--- 2290-minimum-obstacle-removal-to-reach-corner/2290. Minimum Obstacle Removal to Reach Corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290. Minimum Obstacle Removal to Reach Corner.py
@@ -3,33 +3,31 @@ class Solution:
         
         m = len(grid)
         n = len(grid[0])
-        
-        dirs = [(0, 1), (1,0), (0,-1), (-1,0)]
 
         def valid(r, c):
-            return 0<= r < m and 0<= c <n
+            return 0<= r < m and 0 <= c < n
         
-        dists = [[float("inf")] * n for _ in range(m)]
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-        dists[0][0] = 0
+        seen = set()
+        q = deque()
 
-        minHeap = [[0, 0, 0]] #cost, r, c
+        seen.add((0,0))
+        q.append([0, 0, 0])
 
-        while minHeap:
-            cost, r, c = heapq.heappop(minHeap)
+        while q:
+            r, c, cost = q.popleft()
 
-            if dists[r][c] < cost:
-                continue
-            if r == m - 1 and c == n - 1:
+            if (r, c) == (m - 1, n - 1):
                 return cost
+
             for dx, dy in dirs:
                 nr = r + dx
                 nc = c + dy
+                if valid(nr, nc) and (nr, nc) not in seen:
+                    seen.add((nr, nc))
+                    if grid[nr][nc] == 1:
+                        q.append([nr, nc, cost + 1])
+                    else:
+                        q.appendleft([nr, nc, cost])
 
-                if valid(nr, nc):
-                    newCost = 1 + cost if grid[nr][nc] == 1 else cost
-                    if dists[nr][nc] > newCost:
-                        dists[nr][nc] = newCost
-                        heapq.heappush(minHeap, [newCost, nr, nc])
-                   
-        
