3b47d4a97812450cce38755d11b28fb813545f5d
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 00:45:02 -0500

    Runtime: 5389 ms (18.99%), Memory: 17.6 MB (89.10%) - Gitcode

--- 79-word-search/79. Word Search.py
+++ 79-word-search/79. Word Search.py
@@ -1,33 +1,35 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
         m = len(board)
         n = len(board[0])
-        
-        def valid(r,c):
-            return 0<=r<m and 0<=c<n
-        
-        dirs = [(0,1), (0,-1), (1,0), (-1,0)]
 
-        def dfs(r, c, i, seen):
+        def valid(r, c):
+            return 0 <= r < m and 0 <= c < n
+        
+        dirs = [(0, 1), (1,0), (0, -1), (-1,0)]
+
+
+        def dfs(i, r, c, seen):
             if i == len(word):
                 return True
+            
             for dx, dy in dirs:
-                nr = dx + r
-                nc = dy + c
-                if valid(nr,nc) and (nr,nc) not in seen and board[nr][nc] == word[i]:
-                    seen.add((nr,nc))
-                    if dfs(nr,nc,i+1, seen):
+                nr = r + dx
+                nc = c + dy
+
+                if valid(nr, nc) and (nr, nc) not in seen and board[nr][nc] == word[i]:
+                    seen.add((nr, nc))
+                    if dfs(i + 1, nr, nc, seen):
                         return True
-                    
                     seen.remove((nr,nc))
+            
+            return False
         
 
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == word[0]:
-                    if dfs(i,j,1, set([(i,j)])):
+        for r in range(m):
+            for c in range(n):
+                if board[r][c] == word[0]:
+                    if dfs(1, r, c, set([(r, c)])):
                         return True
         
         return False
-
