fdb0d811fbd0c275118f3672d96887ce854344f3
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 02:57:52 -0500

    Runtime: 4 ms (40.41%), Memory: 18.9 MB (5.66%) - Gitcode

--- 139-word-break/139. Word Break.py
+++ 139-word-break/139. Word Break.py
@@ -1,19 +1,46 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+    
+    def add(self, word):
+        curr = self.root
+
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = Node()
+            
+            curr = curr.children[ch]
+
+        curr.end = True
+
+
+
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
 
         wordDict = set(wordDict)
-        n = len(s)
-        dp = [False] * (n + 1)
-        dp[n] = True
-        
-        for i in range(n - 1, -1, -1):
-            for j in range(i, n):
-                if s[i:j + 1] in wordDict:
-                    if dp[j + 1]:
-                        dp[i] = True
+        trie = Trie()
 
-        return dp[0]
+        for word in wordDict:
+            trie.add(word)
+        n = len(s)
+        # dp = [False] * (n + 1)
+        # dp[n] = True
+        
+        # for i in range(n - 1, -1, -1):
+        #     for j in range(i, n):
+        #         if s[i:j + 1] in wordDict:
+        #             if dp[j + 1]:
+        #                 dp[i] = True
+
+        # return dp[0]
 
 
 
@@ -24,12 +51,21 @@ class Solution:
             if i == len(s):
                 return True
             
-
+            curr = trie.root
             for j in range(i, len(s)):
-                if s[i:j + 1] in wordDict:
-                    if dfs(j + 1):
-                        memo[i] = True
-                        return True
+                ch = s[j]
+                if ch in curr.children:
+                    curr = curr.children[ch]
+                    if curr.end:
+                        if dfs(j + 1):
+                            memo[i] = True
+                            return True
+                else:
+                    break
+                # if s[i:j + 1] in wordDict:
+                #     if dfs(j + 1):
+                #         memo[i] = True
+                #         return True
             
             memo[i] = False
             return False
