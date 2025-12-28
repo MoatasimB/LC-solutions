5faf72db70f6b109002ea089580d53911c00fa7f
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 00:18:08 -0500

    Runtime: 0 ms (100.00%), Memory: 17.2 MB (98.56%) - Gitcode

--- 9-palindrome-number/9. Palindrome Number.py
+++ 9-palindrome-number/9. Palindrome Number.py
@@ -1,13 +1,14 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0 or x % 10 == 0 and x != 0:
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
         
-        num = 0
+        curr = 0
 
-        while x > num:
-            digit = x % 10
-            num = num * 10 + digit
+        while curr < x:
+            
+            curr = curr * 10 + (x % 10)
             x = x // 10
         
-        return x == num or x == num // 10
+
+        return curr == x or curr // 10 == x
