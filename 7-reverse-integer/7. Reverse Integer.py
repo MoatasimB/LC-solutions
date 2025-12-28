acfa167f5b8fb8cd44bcb0c6a9ab2a685257b607
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 00:31:55 -0500

    Runtime: 40 ms (46.68%), Memory: 17.2 MB (98.45%) - Gitcode

--- 7-reverse-integer/7. Reverse Integer.py
+++ 7-reverse-integer/7. Reverse Integer.py
@@ -1,21 +1,21 @@
 class Solution:
     def reverse(self, x: int) -> int:
         
-        MAX_INT = 2**31 - 1
-        MIN_INT = -(2**31)
-        
-        res = 0
+        INT_MAX = 2**31 - 1
+        INT_MIN = -(2**31)
+
+        curr = 0
 
         while x:
             digit = int(math.fmod(x, 10))
+
+            if curr > INT_MAX // 10 or (curr == INT_MAX // 10 and digit > INT_MAX % 10):
+                return 0
+            if curr < INT_MIN // 10 or (curr == INT_MIN // 10 and digit < INT_MIN % 10):
+                return 0
+            
+            curr = curr * 10 + digit
             x = int(x / 10)
-
-            if res > MAX_INT // 10 or (res == MAX_INT // 10 and digit > MAX_INT % 10):
-                return 0
-            if res < MIN_INT // 10 or (res == MIN_INT // 10 and digit < MIN_INT % 10):
-                return 0
-
-
-            res = (res * 10) + digit
         
-        return res
+
+        return curr
