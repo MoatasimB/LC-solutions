2ee4caf9e0e17e28221ee8cb1fec547d83783dde
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 02:59:45 -0500

    Runtime: 4 ms (40.41%), Memory: 18.9 MB (5.66%) - Gitcode

--- 139-word-break/139. Word Break.py
+++ 139-word-break/139. Word Break.py
@@ -31,25 +31,25 @@ class Solution:
         for word in wordDict:
             trie.add(word)
         n = len(s)
-        dp = [False] * (n + 1)
-        dp[n] = True
+        # dp = [False] * (n + 1)
+        # dp[n] = True
         
-        for i in range(n - 1, -1, -1):
-            curr = trie.root
-            for j in range(i, n):
-                ch = s[j]
-                if ch in curr.children:
-                    curr = curr.children[ch]
-                    if curr.end and dp[j + 1]:
-                        dp[i] = True
-                else:
-                    break
+        # for i in range(n - 1, -1, -1):
+        #     curr = trie.root
+        #     for j in range(i, n):
+        #         ch = s[j]
+        #         if ch in curr.children:
+        #             curr = curr.children[ch]
+        #             if curr.end and dp[j + 1]:
+        #                 dp[i] = True
+        #         else:
+        #             break
 
-                # if s[i:j + 1] in wordDict:
-                #     if dp[j + 1]:
-                #         dp[i] = True
+        #         # if s[i:j + 1] in wordDict:
+        #         #     if dp[j + 1]:
+        #         #         dp[i] = True
 
-        return dp[0]
+        # return dp[0]
 
 
 
@@ -65,10 +65,9 @@ class Solution:
                 ch = s[j]
                 if ch in curr.children:
                     curr = curr.children[ch]
-                    if curr.end:
-                        if dfs(j + 1):
-                            memo[i] = True
-                            return True
+                    if curr.end and dfs(j + 1):
+                        memo[i] = True
+                        return True
                 else:
                     break
                 # if s[i:j + 1] in wordDict:
