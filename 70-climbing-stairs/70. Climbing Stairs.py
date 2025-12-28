fa8a04202d696daa9c9cecf52c3599e058bfae3e
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-27 23:56:03 -0500

    Runtime: 0 ms (100.00%), Memory: 17.4 MB (90.57%) - Gitcode

--- 70-climbing-stairs/70. Climbing Stairs.py
+++ 70-climbing-stairs/70. Climbing Stairs.py
@@ -1,16 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        memo = {}
-        def dfs(n):
-            if n in memo:
-                return memo[n]
-            if n == 0:
-                return 1
-            if n == 1:
-                return 1
-            
-            memo[n] = dfs(n - 2) + dfs(n - 1)
-            return memo[n]
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
         
-        return dfs(n)
+        return dp[n]
