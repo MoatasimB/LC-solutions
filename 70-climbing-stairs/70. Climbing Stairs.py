0c5e6ce8bf0625df66ae00c1f5f8e6948bb2c835
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-27 23:56:55 -0500

    Runtime: 0 ms (100.00%), Memory: 17.2 MB (97.30%) - Gitcode

--- 70-climbing-stairs/70. Climbing Stairs.py
+++ 70-climbing-stairs/70. Climbing Stairs.py
@@ -1,11 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        dp = [0] * (n + 1)
-        dp[0] = 1
-        dp[1] = 1
+        twoBack = 1
+        oneBack = 1
 
+        curr = 0
         for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        
-        return dp[n]
+            curr = oneBack + twoBack
+
+            twoBack = oneBack
+            oneBack = curr
+            
+        return oneBack
