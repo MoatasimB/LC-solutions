ceba30f31ce7d0198f15010ee94d5af9aaac1632
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 00:21:44 -0500

    Runtime: 3 ms (80.86%), Memory: 17.2 MB (95.59%) - Gitcode

--- 13-roman-to-integer/13. Roman to Integer.py
+++ 13-roman-to-integer/13. Roman to Integer.py
@@ -2,19 +2,20 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         
         mpp = {
-            "I":1,
-            "V":5,
-            "X":10,
-            "L"  :50,
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
             "C" : 100,
             "D" : 500,
-            "M" : 1000
+            "M" : 1000,
         }
 
         ans = 0
 
         for i in range(len(s)):
-            if i + 1 < len(s) and mpp[s[i + 1]] > mpp[s[i]]:
+
+            if i + 1 < len(s) and mpp[s[i]] < mpp[s[i + 1]]:
                 ans -= mpp[s[i]]
             else:
                 ans += mpp[s[i]]
