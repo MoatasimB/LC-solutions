19f6888d82eb07641c7fb8a3d58abcc2c0f25f63
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-27 23:58:57 -0500

    Runtime: 15 ms (80.56%), Memory: 18.5 MB (85.97%) - Gitcode

--- 55-jump-game/55. Jump Game.py
+++ 55-jump-game/55. Jump Game.py
@@ -1,21 +1,11 @@
 class Solution:
-    def __init__(self):
-        self.memo = []
-        self.nums = []
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        goal = n - 1
 
-    def canJumpFromPosition(self, position):
-        if self.memo[position] != -1:
-            return self.memo[position] == 1
-        furthestJump = min(position + self.nums[position], len(self.nums) - 1)
-        for nextPosition in range(position + 1, furthestJump + 1):
-            if self.canJumpFromPosition(nextPosition):
-                self.memo[position] = 1
-                return True
-        self.memo[position] = 0
-        return False
+        for i in range(n - 2, -1, -1):
+            if nums[i] + i >= goal:
+                goal = i
+        
 
-    def canJump(self, nums):
-        self.nums = nums
-        self.memo = [-1] * len(nums)  # -1 for unknown, 0 for bad, 1 for good
-        self.memo[-1] = 1  # The last position is always "good"
-        return self.canJumpFromPosition(0)
+        return goal == 0
