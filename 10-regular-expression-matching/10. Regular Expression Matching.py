995c8440cda2b63d38bbc2aad0bcb7eac5d37429
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 21:38:34 -0500

    Runtime: 7 ms (74.70%), Memory: 17.5 MB (89.80%) - Gitcode

--- 10-regular-expression-matching/10. Regular Expression Matching.py
+++ 10-regular-expression-matching/10. Regular Expression Matching.py
@@ -3,27 +3,29 @@ class Solution:
         
         memo = {}
         def dfs(i, j):
-            if (i,j) in memo:
-                return memo[(i,j)]
-            if (i == len(s) and j == len(p)):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if i == len(s) and j == len(p):
                 return True
-            
             if j == len(p):
                 return False
             
+            isMatch = (i < len(s) and (s[i] == p[j] or p[j] == "."))
+
+            memo[(i, j)] = False
+            if j + 1 < len(p) and p[j + 1] == "*":
+                # memo[(i, j)] = (isMatch and dfs(i + 1, j)) or dfs(i, j + 2)
+                if isMatch:
+                    if dfs(i + 1, j):
+                        memo[(i, j)] = True
+                        return True
+                if dfs(i, j + 2):
+                    memo[(i, j)] = True
+                    return True
+            if isMatch and dfs(i + 1, j + 1):
+                memo[(i, j)] = True
+                return True
             
-            match =i < len(s) and  (s[i] == p[j] or p[j] == ".")
-            memo[(i,j)] = False
-            
-            if j + 1 < len(p) and p[j+1] == "*":
-                memo[(i,j)] = (match and dfs(i + 1, j ) or (dfs(i, j+2)))
-                return memo[(i,j)]
-            
-            
-            if match:
-                memo[(i,j)] = dfs(i+1, j+1)
-                return memo[(i,j)]
-            return memo[(i,j)]
+            return memo[(i, j)]
         
-        return dfs(0,0)
-            
+        return dfs(0, 0)
