ed1a7f4632d26e1616d70bbfd351df5bf61afd20
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 21:16:08 -0500

    Runtime: 0 ms (100.00%), Memory: 17.1 MB (98.42%) - Gitcode

--- 62-unique-paths/62. Unique Paths.py
+++ 62-unique-paths/62. Unique Paths.py
@@ -1,55 +1,37 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        prev = [1] * n
-
-        for i in range(1,m):
-            curr = [0] * n
-            for j in range(n):
-         
-                up = prev[j]
-                left = 0
-                if j > 0:
-                    left = curr[j-1]
-        
-                curr[j] = up + left
-            prev = curr
-        
-        return prev[n-1]
-        
-        
-        
-        dp = [[0]*n for _ in range(m)]
-
+        dp = [[0] * n for _ in range(m)]
         dp[0][0] = 1
 
-        for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0:
+        for r in range(m):
+            for c in range(n):
+                if r == 0 and c == 0:
                     continue
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                left = 0
+                up = 0
+                if r - 1 >= 0:
+                    up = dp[r-1][c]
+                if c - 1 >= 0:
+                    left = dp[r][c - 1]
+                dp[r][c] = left + up
         
-        return dp[m-1][n-1]
-
-
-
-
-        return math.comb(m+n-2, m-1)
+        return dp[m - 1][n - 1]
         
-        
-        
-        dp = [[-1]*n for _ in range(m)]
-
-        def dfs(i,j):
-            if i == 0 and j == 0:
+        memo = {}
+        def dfs(r, c):
+            if (r, c) in memo:
+                return memo[(r, c)]
+            if r == 0 and c == 0:
                 return 1
-            if i<0 or j<0:
+            
+            if r < 0 or c < 0:
                 return 0
-            if dp[i][j] != -1:
-                return dp[i][j]
-            up = dfs(i-1,j)
-            left = dfs(i,j-1)
-            dp[i][j] = up + left
-            return dp[i][j]
+            
+            left = dfs(r, c - 1)
+            up = dfs(r - 1, c)
+
+            memo[(r, c)] = left + up
+            return left + up
         
-        return dfs(m-1,n-1)
+        return dfs(m - 1, n - 1)
