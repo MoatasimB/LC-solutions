b1c743b0671255f9be6837de63e419400306ba69
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 20:15:22 -0500

    Runtime: 3 ms (71.36%), Memory: 17.9 MB (89.95%) - Gitcode

--- 73-set-matrix-zeroes/73. Set Matrix Zeroes.py
+++ 73-set-matrix-zeroes/73. Set Matrix Zeroes.py
@@ -3,33 +3,32 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-
+        
         m = len(matrix)
         n = len(matrix[0])
 
-        col0 = 1
+        row1 = False
 
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j] == 0:
-                    if j == 0:
-                        col0 = 0
-                        matrix[i][0] = 0
-                    else:
-                        matrix[0][j] = 0
-                        matrix[i][0] = 0
-        x = matrix[0][0]
-        for i in range(1,m):
-            for j in range(1,n):
-                if matrix[i][0]==0 or matrix[0][j]==0:
-                    matrix[i][j] = 0
-
-        if matrix[0][0] == 0:
-            for i in range(n):
-                matrix[0][i] = 0
-        if col0 == 0:
-            for i in range(m):
-                matrix[i][0] = 0
+        for r in range(m):
+            for c in range(n):
+                if r == 0 and matrix[r][c] == 0:
+                    row1 = True
+                else:
+                    if matrix[r][c] == 0:
+                        matrix[0][c] = 0
+                        matrix[r][0] = 0
+        
+        for r in range(1, m):
+            for c in range(1, n):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
         
 
+        if matrix[0][0] == 0:
+            for r in range(m):
+                matrix[r][0] = 0
+        
+        if row1:
+            for c in range(n):
+                matrix[0][c] = 0
         
