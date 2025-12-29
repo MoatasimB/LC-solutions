3d9c86a4d36abec8c7d16154a6100d60779d5ffb
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 20:07:55 -0500

    Runtime: 0 ms (100.00%), Memory: 17.3 MB (95.78%) - Gitcode

--- 78-subsets/78. Subsets.py
+++ 78-subsets/78. Subsets.py
@@ -2,22 +2,18 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
         ans = []
+
         def dfs(i, curr):
-            print(curr)
-            ans.append(curr[:])
             if i == len(nums):
+                ans.append(curr[:])
                 return
+            
+            curr.append(nums[i])
+            dfs(i + 1, curr)
+            curr.pop()
 
-            for j in range(i, len(nums)):
-                curr.append(nums[j])
-                dfs(j + 1, curr)
-                curr.pop()
-            # curr.append(nums[i])
-            # dfs(i+1, curr)
-            # curr.pop()
-
-            # dfs(i+1, curr)
+            dfs(i + 1, curr)
         
-        dfs(0,[])
-        return ans
 
+        dfs(0, [])
+        return ans
