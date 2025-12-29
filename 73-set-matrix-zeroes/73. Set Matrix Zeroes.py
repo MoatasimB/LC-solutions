ed99ea2d61f0b3402d7da9542ce6b10425ab555b
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 20:21:16 -0500

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (98.33%) - Gitcode

--- 73-set-matrix-zeroes/73. Set Matrix Zeroes.py
+++ 73-set-matrix-zeroes/73. Set Matrix Zeroes.py
@@ -6,29 +6,18 @@ class Solution:
         
         m = len(matrix)
         n = len(matrix[0])
+        rows = set()
+        cols = set()
+        
+        for r in range(m):
+            for c in range(n):
+                if matrix[r][c] == 0:
+                    rows.add(r)
+                    cols.add(c)
+        
 
-        row1 = False
 
         for r in range(m):
             for c in range(n):
-                if r == 0 and matrix[r][c] == 0:
-                    row1 = True
-                else:
-                    if matrix[r][c] == 0:
-                        matrix[0][c] = 0
-                        matrix[r][0] = 0
-        
-        for r in range(1, m):
-            for c in range(1, n):
-                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                if r in rows or c in cols:
                     matrix[r][c] = 0
-        
-
-        if matrix[0][0] == 0:
-            for r in range(m):
-                matrix[r][0] = 0
-        
-        if row1:
-            for c in range(n):
-                matrix[0][c] = 0
-        
