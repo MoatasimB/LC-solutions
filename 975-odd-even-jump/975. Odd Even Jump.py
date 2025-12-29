2d02bc9aaf3e2f6f7c027d0c86c2abd726d1d7e1
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-29 00:28:25 -0500

    Runtime: 94 ms (79.95%), Memory: 21.7 MB (74.94%) - Gitcode

--- 975-odd-even-jump/975. Odd Even Jump.py
+++ 975-odd-even-jump/975. Odd Even Jump.py
@@ -1,33 +1,36 @@
 class Solution:
     def oddEvenJumps(self, arr: List[int]) -> int:
         n = len(arr)
-        odd_next = [0] * n
-        even_next = [0] * n
+        oddNext = [0] * n
+        evenNext = [0] * n
 
         stack = []
-        for i, val in sorted(enumerate(arr), key = lambda x : x[1]):
+        for i, num in sorted(enumerate(arr), key = lambda x:x[1]):
             while stack and stack[-1] < i:
-                odd_next[stack.pop()] = i
-            stack.append(i)
-        
-        stack = []
-        for i, val in sorted(enumerate(arr), key = lambda x : x[1], reverse = True):
-            while stack and stack[-1] < i:
-                even_next[stack.pop()] = i
-            stack.append(i)
-        
-        odd_next_jump = [0] * n
-        even_next_jump = [0] * n
-        odd_next_jump[n-1] = 1
-        even_next_jump[n-1] = 1
-
-        for i in range(n-2, -1, -1):
-            nextJumpOdd = odd_next[i]
-            if nextJumpOdd and even_next_jump[nextJumpOdd]:
-                odd_next_jump[i] = 1
+                oddNext[stack.pop()] = i
             
-            nextJumpEven = even_next[i]
-            if nextJumpEven and odd_next_jump[nextJumpEven]:
-                even_next_jump[i] = 1
+            stack.append(i)
         
-        return sum(odd_next_jump)
+        stack = []
+        for i, num in sorted(enumerate(arr), key = lambda x:x[1], reverse = True):
+            while stack and stack[-1] < i:
+                evenNext[stack.pop()] = i
+            
+            stack.append(i)
+
+        oddJumps = [0] * n
+        evenJumps = [0] * n
+
+        oddJumps[n - 1] = 1
+        evenJumps[n - 1] = 1
+
+        for i in range(n - 2, -1, -1):
+            nextOddJump = oddNext[i]
+            if oddNext and evenJumps[nextOddJump]:
+                oddJumps[i] = 1
+            
+            nextEvenJump = evenNext[i]
+            if evenNext and oddJumps[nextEvenJump]:
+                evenJumps[i] = 1
+        
+        return sum(oddJumps)
