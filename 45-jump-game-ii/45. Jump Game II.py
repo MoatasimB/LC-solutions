f3058bf1ae5b8b7718223c9340946427e3c7ee74
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 19:47:16 -0500

    Runtime: 971 ms (22.60%), Memory: 18.8 MB (30.56%) - Gitcode

--- 45-jump-game-ii/45. Jump Game II.py
+++ 45-jump-game-ii/45. Jump Game II.py
@@ -1,14 +1,25 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        q = deque()
+
+        q.append([nums[0], 0, 0])
+        seen = set()
+        seen.add(0)
+
+        farthest = 0
+        while q:
+            val, idx, steps = q.popleft()
+
+            if idx == n - 1:
+                return steps
+            
+            farthest = max(farthest, idx + 1)
+            for j in range(farthest, min(idx + val + 1, n)):
+                if j not in seen:
+                    seen.add(j)
+                    q.append([nums[j], j, steps + 1])
         
-        currEnd = 0
-        currFarthest = 0
-        ans = 0
-        for i in range(len(nums) - 1):
-            currFarthest = max(currFarthest, i + nums[i])
 
-            if i == currEnd:
-                ans += 1
-                currEnd = currFarthest
 
-        return ans
+            
