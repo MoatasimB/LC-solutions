281496b652356078c3000b019fe9eea4a04c1c4b
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 21:06:14 -0500

    Runtime: 0 ms (100.00%), Memory: 17.2 MB (97.10%) - Gitcode

--- 50-powx-n/50. Pow(x, n).py
+++ 50-powx-n/50. Pow(x, n).py
@@ -1,21 +1,20 @@
 class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        
-        def dfs(x, n):
-            if n == 0:
-                return 1
-            
-            ans = 0
-            if n % 2 == 0:
-                ans += dfs(x * x, n // 2) 
-            else:
-                ans += x * dfs(x * x, n // 2)
-            
-            return ans
-        
-        neg = False
+    def binaryExp(self, x: float, n: int) -> float:
+        # Base case, to stop recursive calls.
+        if n == 0:
+            return 1
+
+        # Handle case where, n < 0.
         if n < 0:
-            neg = True
-        final = dfs(x, abs(n))
-        
-        return 1/final if neg else final
+            return 1.0 / self.binaryExp(x, -1 * n)
+
+        # Perform Binary Exponentiation.
+        # If 'n' is odd we perform Binary Exponentiation on 'n - 1' and multiply result with 'x'.
+        if n % 2 == 1:
+            return x * self.binaryExp(x * x, (n - 1) // 2)
+        # Otherwise we calculate result by performing Binary Exponentiation on 'n'.
+        else:
+            return self.binaryExp(x * x, n // 2)
+
+    def myPow(self, x: float, n: int) -> float:
+        return self.binaryExp(x, n)
