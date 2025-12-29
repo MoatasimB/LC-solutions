c80f8960bd2afe813ebe23a9352483e6ee85244c
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-28 20:10:14 -0500

    Runtime: 0 ms (100.00%), Memory: 17.3 MB (99.76%) - Gitcode

--- 90-subsets-ii/90. Subsets II.py
+++ 90-subsets-ii/90. Subsets II.py
@@ -1,24 +1,24 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
         nums.sort()
-        ans = []
 
+        ans = []
+        
         def dfs(i, curr):
-            print(curr)
             if i == len(nums):
                 ans.append(curr[:])
                 return 
-
             
             curr.append(nums[i])
-            dfs(i+1, curr)
+            dfs(i + 1, curr)
             curr.pop()
 
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
             
-            while i+1 <len(nums) and nums[i] == nums[i+1]:
-                i +=1
-            
-            dfs(i+1, curr)
+            dfs(i + 1, curr)
         
-        dfs(0,[])
+        dfs(0, [])
+
         return ans
