930202c6c6c3b461d86b2f4542634c89ad219123
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-30 01:14:44 -0500

    Runtime: 0 ms (100.00%), Memory: 17.2 MB (92.23%) - Gitcode

--- 796-rotate-string/796. Rotate String.py
+++ 796-rotate-string/796. Rotate String.py
@@ -13,14 +13,14 @@ class Solution:
 
 
 
-        q = deque()
+        # q = deque()
 
-        for ch in s:
-            q.append(ch)
-        while "".join(q) != goal:
-            ch = q.popleft()
-            q.append(ch)
-            if "".join(q) == s:
-                return False
+        # for ch in s:
+        #     q.append(ch)
+        # while "".join(q) != goal:
+        #     ch = q.popleft()
+        #     q.append(ch)
+        #     if "".join(q) == s:
+        #         return False
         
-        return True
+        # return True
