4ae7bf8d48cd15ed9a5835131da0b868081f25b9
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-30 00:12:16 -0500

    Runtime: 183 ms (58.16%), Memory: 42.3 MB (5.34%) - Gitcode

--- 295-find-median-from-data-stream/295. Find Median from Data Stream.py
+++ 295-find-median-from-data-stream/295. Find Median from Data Stream.py
@@ -1,38 +1,36 @@
 class MedianFinder:
 
     def __init__(self):
-        self.small = [] #max Heap            2 3  5
-        self.large = [] #min Heap           4 6 7    
+        self.left = [] #maxHeap
+        self.right = [] #minHeap
+        
 
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.large, num)
+        heapq.heappush(self.left, -num)
 
-        if len(self.large) > len(self.small) + 1:
-            x = -heapq.heappop(self.large)
-            heapq.heappush(self.small, x)
+        if len(self.left) > len(self.right) + 1:
+            val = -heapq.heappop(self.left)
+            heapq.heappush(self.right, val)
+
+        if self.left and self.right and abs(self.left[0]) > self.right[0]:
+            val = -heapq.heappop(self.left)
+            heapq.heappush(self.right, val)
         
-        if self.small and -self.small[0] > self.large[0]:
-            x = -heapq.heappop(self.large)
-            heapq.heappush(self.small, x)
-        
-        if len(self.small) > len(self.large):
-            x = -heapq.heappop(self.small)
-            heapq.heappush(self.large, x)
-
-
+        if len(self.right) > len(self.left):
+            val = -heapq.heappop(self.right)
+            heapq.heappush(self.left, val)
 
     def findMedian(self) -> float:
-        if len(self.small) == len(self.large):
-            return (-self.small[0] + self.large[0]) / 2
-        
-        return self.large[0]
+        total = len(self.left) + len(self.right)
+
+        if total % 2 == 0:
+            return (-self.left[0] + self.right[0]) / 2
+        else:
+            return -self.left[0]
 
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
-
-
-#  s = -3          l = -2 -1 
