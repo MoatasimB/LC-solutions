f4e460d53aaa9bde7b92972031b46cf2315fea4a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-30 02:00:45 -0500

    Runtime: 51 ms (90.91%), Memory: 21.2 MB (30.89%) - Gitcode

--- 127-word-ladder/127. Word Ladder.py
+++ 127-word-ladder/127. Word Ladder.py
@@ -5,17 +5,14 @@ class Solution:
         if endWord not in wordList:
             return 0
         
-        def getNeighbors(word):
-            neighbors = []
+        mpp = defaultdict(list)
 
+        for word in wordList:
             for i in range(len(word)):
-                for ch in "abcdefghijklmnopqrstuvwxyz":
-                    new_word = word[:i] + ch + word[i+1:]
-                    if new_word in wordList:
-                        neighbors.append(new_word)
-            return neighbors
+                mpp[word[:i] + "*" + word[i + 1:]].append(word)
         
-
+        
+        
         q = deque()
         seen = set()
         q.append([beginWord, 1])
@@ -26,10 +23,11 @@ class Solution:
 
             if word == endWord:
                 return steps
-            
-            for nei in getNeighbors(word):
-                if nei not in seen:
-                    seen.add(nei)
-                    q.append([nei, steps + 1])
+
+            for i in range(len(word)):
+                for nei in mpp[word[:i] + "*" + word[i + 1:]]:
+                    if nei not in seen:
+                        seen.add(nei)
+                        q.append([nei, steps + 1])
         
         return 0
