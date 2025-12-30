e02b50a6329b113c815be071f96738b0cb43403f
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-30 00:23:01 -0500

    Runtime: 11 ms (70.16%), Memory: 17.5 MB (97.37%) - Gitcode

--- 51-n-queens/51. N-Queens.py
+++ 51-n-queens/51. N-Queens.py
@@ -1,40 +1,33 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         
-        grid = [["."] * n for _ in range(n)]
-
-        rows = set()
-        cols = set()
-        pos = set()
-        neg = set()
+        negDiag = set()
+        posDiag = set()
+        col = set()
         ans = []
-       
+        board = [["."] * n for _ in range(n)]
         def dfs(r):
             if r == n:
-                ans.append(["".join(row) for row in grid])
+                ans.append(["".join(row) for row in board[:]])
                 return
             
             for c in range(n):
-                if r not in rows and c not in cols and (r+c) not in pos and (r-c) not in neg:
-                    rows.add(r)
-                    cols.add(c)
-                    pos.add(r+c)
-                    neg.add(r-c)
+                if c not in col and r + c not in posDiag and r - c not in negDiag:
+                    col.add(c)
+                    posDiag.add(r + c)
+                    negDiag.add(r - c)
 
-                    grid[r][c] = "Q"
+                    board[r][c] = "Q"
 
-                    dfs(r+1)
+                    dfs(r + 1)
 
-                    grid[r][c] = "."
-                    rows.remove(r)
-                    cols.remove(c)
-                    pos.remove(r+c)
-                    neg.remove(r-c)
+                    col.remove(c)
+                    posDiag.remove(r + c)
+                    negDiag.remove(r - c)
+
+                    board[r][c] = "."
         
-
         dfs(0)
-
         return ans
-
 
 
