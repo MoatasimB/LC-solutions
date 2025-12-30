89213e7ac2e983a4d47ea299bec99b71bee8b55c
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-30 02:29:47 -0500

    Runtime: 477 ms (58.42%), Memory: 113.6 MB (11.79%) - Gitcode

--- 44-wildcard-matching/44. Wildcard Matching.py
+++ 44-wildcard-matching/44. Wildcard Matching.py
@@ -2,7 +2,7 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
         new_p = []
-        
+
         for ch in p:
             if not new_p:
                 new_p.append(ch)
@@ -13,31 +13,25 @@ class Solution:
                 new_p.append(ch)
         
         p = "".join(new_p)
+
         memo = {}
         def dfs(i, j):
-            if (i,j) in memo:
-                return memo[(i,j)]
-            if (i == len(s) and j == len(p)) or (i == len(s) and p[j:] == "*"):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if i == len(s) and (j == len(p) or p[j:] == "*"):
                 return True
-            # if i == len(s) and (p[j] == "*" and j == len(p) - 1):
-            #     return True
-            if j == len(p) or i == len(s):
+            if i == len(s) or j == len(p):
                 return False
-            
-            
+            memo[(i, j)] = False
             if s[i] == p[j] or p[j] == "?":
-                memo[(i,j)] = dfs(i+1, j+1)
-                return memo[(i,j)]
+                memo[(i, j)] = dfs(i + 1, j + 1)
+            elif p[j] == "*":
+                memo[(i, j)] = dfs(i + 1, j) or dfs(i, j + 1)
             
-            if p[j] == "*":
-                memo[(i,j)] = dfs(i+1, j) or dfs(i, j+1)
-                return memo[(i,j)]
-
-            
-            memo[(i,j)] = False
-            return False
+            return memo[(i, j)]
         
-        return dfs(0,0)
-    
+        return dfs(0, 0)
 
-        
+
+
+
