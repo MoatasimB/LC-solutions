ef173919ace8f6b567f5d8783b9b7e5383d3798e
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-30 02:24:49 -0500

    Runtime: 429 ms (61.14%), Memory: 113.6 MB (11.79%) - Gitcode

--- 44-wildcard-matching/44. Wildcard Matching.py
+++ 44-wildcard-matching/44. Wildcard Matching.py
@@ -2,33 +2,42 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
         new_p = []
-
-        for ch in p:
-            if not new_p or ch != "*":
-                new_p.append(ch)
-            elif new_p[-1] != "*":
-                new_p.append(ch)
-        memo = {}
-
-        def dfs(s, p):
-
-            if (s,p) in memo:
-                return memo[(s,p)]
-            
-            memo[(s,p)] = False
-            if s == p or p == "*":
-                memo[(s,p)] = True
-            elif s == "" or p == "":
-                memo[(s,p)] = False
-            elif s[0] == p[0] or p[0] == "?":
-                memo[(s,p)] = dfs(s[1:], p[1:])
-            elif p[0] == "*":
-                memo[(s,p)] = dfs(s[1:], p) or dfs(s, p[1:])
-            
-            return memo[(s,p)]
         
+        for ch in p:
+            if not new_p:
+                new_p.append(ch)
+            elif ch == "*":
+                if new_p[-1] != "*":
+                    new_p.append(ch)
+            else:
+                new_p.append(ch)
+        
+        p = "".join(new_p)
+        memo = {}
+        def dfs(i, j):
+            if (i,j) in memo:
+                return memo[(i,j)]
+            if (i == len(s) and j == len(p)) or (i == len(s) and p[j:] == "*"):
+                return True
+            # if i == len(s) and (p[j] == "*" and j == len(p) - 1):
+            #     return True
+            if j == len(p) or i == len(s):
+                return False
+            
+            
+            if s[i] == p[j] or p[j] == "?":
+                memo[(i,j)] = dfs(i+1, j+1)
+                return memo[(i,j)]
+            
+            if p[j] == "*":
+                memo[(i,j)] = dfs(i+1, j) or dfs(i, j+1)
+                return memo[(i,j)]
 
-        return  dfs(s, "".join(new_p))
+            
+            memo[(i,j)] = False
+            return False
+        
+        return dfs(0,0)
+    
 
-
-   
+        
