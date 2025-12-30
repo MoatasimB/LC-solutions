f0f4528b9011b868fd97b478d7b6b66841a02ec8
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-30 00:16:41 -0500

    Runtime: 55 ms (91.93%), Memory: 17.7 MB (97.20%) - Gitcode

--- 76-minimum-window-substring/76. Minimum Window Substring.py
+++ 76-minimum-window-substring/76. Minimum Window Substring.py
@@ -1,37 +1,35 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
-            return ""
+        
+        freq = defaultdict(int)
 
-        need = len(t)
-        needDic = defaultdict(int)
         for ch in t:
-            needDic[ch] +=1
+            freq[ch] += 1
+        
+        req = len(freq)
 
-        have = 0
         l = 0
-        currDic = defaultdict(int)
         length = float("inf")
-        start = 0
+        startIdx = -1
         for r in range(len(s)):
+            ch = s[r]
 
-            if s[r] in needDic:
-                currDic[s[r]] +=1
-                if currDic[s[r]] <= needDic[s[r]]:
-                    have +=1
-
-            while have == need:
-                if length > r - l + 1:
-                    length = r-l+1
-                    start = l
-                if s[l] in needDic:
-                    currDic[s[l]] -=1
-                    if currDic[s[l]] < needDic[s[l]]:
-                        have -=1
-                l+=1
-        return s[start: start + length] if length != float('inf') else ""
+            if ch in freq:
+                freq[ch] -= 1
+                if freq[ch] == 0:
+                    req -= 1
             
+            while req == 0:
+                if r - l + 1 < length:
+                    length = r - l + 1
+                    startIdx = l
+                
+                ch = s[l]
+                if ch in freq:
+                    freq[ch] += 1
+                    if freq[ch] - 1 == 0:
+                        req += 1
+                l += 1
+        
 
-
-
-
+        return s[startIdx: startIdx + length] if length != float("inf") else ""
