30bdfab0eacac2d295595cbc346783a88d725768
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-31 00:10:51 -0500

    Runtime: 127 ms (30.69%), Memory: 43.8 MB (19.80%) - Gitcode

--- 2534-time-taken-to-cross-the-door/2534. Time Taken to Cross the Door.py
+++ 2534-time-taken-to-cross-the-door/2534. Time Taken to Cross the Door.py
@@ -1,61 +1,62 @@
 class Solution:
     def timeTaken(self, arrival: List[int], state: List[int]) -> List[int]:
-        
+        n = len(arrival)
+        enter = deque() #[time, idx]
+        exit = deque() #[time, idx]
         q = deque()
-        ans = [0] * len(arrival)
-        for i in range(len(arrival)):
+        ans = [0] * n
+        for i in range(n):
             q.append([arrival[i], i])
+
+        prev = None #0 enter, 1 exit
+        time = 0
         
-        t = 0
-        prev = -1
-        enter = deque()
-        exit = deque()
         while q or enter or exit:
-            while q and q[0][0] <= t:
+            while q and q[0][0] <= time:
                 if state[q[0][1]] == 0:
                     enter.append(q.popleft())
                 else:
                     exit.append(q.popleft())
-            # print(t)
-            # print(enter)
-            # print(exit)
-            if len(enter) + len(exit) == 1:
-                if len(enter) == 1:
+
+            enterSize = len(enter)
+            exitSize = len(exit)
+
+            if enterSize + exitSize == 1:
+                if enterSize == 1:
+                    enterTime, enterIdx = enter.popleft()
+                    ans[enterIdx] = time
                     prev = 0
-                    ans[enter.popleft()[1]] = t
                 else:
+                    exitTime, exitIdx = exit.popleft()
+                    ans[exitIdx] = time
                     prev = 1
-                    ans[exit.popleft()[1]] = t
-                t += 1
-            elif len(enter) > 1 and len(exit) == 0:
+                time += 1
+            elif enterSize > 1 and exitSize == 0:
                 prev = 0
                 while enter:
-                    ans[enter.popleft()[1]] = t
-                    t += 1
-
-            elif len(enter) == 0 and len(exit) > 1:
+                    enterTime, enterIdx = enter.popleft()
+                    ans[enterIdx] = time
+                    time += 1
+            
+            elif enterSize == 0 and exitSize > 1:
                 prev = 1
                 while exit:
-                    ans[exit.popleft()[1]] = t
-                    t += 1
-            elif len(enter) >= 1 and len(exit) >= 1:
-                if prev == -1:
-                    ans[exit.popleft()[1]] = t
+                    exitTime, exitIdx = exit.popleft()
+                    ans[exitIdx] = time
+                    time += 1 
+            
+            elif enterSize >= 1 and exitSize >= 1:
+                if prev == None or prev == 1:
                     prev = 1
-
-
-                elif prev == 0:
-                    ans[enter.popleft()[1]] = t
-                
+                    exitTime, exitIdx = exit.popleft()
+                    ans[exitIdx] = time
                 else:
-                    ans[exit.popleft()[1]] = t
-                
-                t += 1
+                    enterTime, enterIdx = enter.popleft()
+                    ans[enterIdx] = time
+                time += 1
             else:
-                prev = -1
-                t += 1
-        
+                time += 1
+                prev = None
+
         return ans
 
-
-            
