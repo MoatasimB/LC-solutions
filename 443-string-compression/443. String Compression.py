539fade93b2882d5faa7e1fbbb3cb1a0be3316b6
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-31 00:49:46 -0500

    Runtime: 3 ms (40.35%), Memory: 17.6 MB (88.38%) - Gitcode

--- 443-string-compression/443. String Compression.py
+++ 443-string-compression/443. String Compression.py
@@ -9,11 +9,11 @@ class Solution:
 
         j = 0
 
-        while i < len(chars):
+        while i <= len(chars):
             if not prev:
                 prev = chars[i]
                 curr = 1
-            elif prev and (chars[i] != prev):
+            elif prev and (i == len(chars) or chars[i] != prev):
                 if curr == 1:
                     chars[j] = prev
                     j += 1
@@ -25,20 +25,20 @@ class Solution:
                         j += 1
 
 
-                prev = chars[i]
+                prev = chars[i] if i < len(chars) else None
                 curr = 1
             else:
                 curr += 1
             
             i += 1
         
-        chars[j] = prev
-        if curr == 1:
-            j += 1
-        else:
-            j += 1
-            for ch in str(curr):
-                chars[j] = ch
-                j += 1
+        # chars[j] = prev
+        # if curr == 1:
+        #     j += 1
+        # else:
+        #     j += 1
+        #     for ch in str(curr):
+        #         chars[j] = ch
+        #         j += 1
 
         return j
