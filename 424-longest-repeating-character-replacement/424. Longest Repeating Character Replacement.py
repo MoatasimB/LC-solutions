104da1254e2c06b0203ac141eb0686b330e6b735
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-31 23:03:56 -0500

    Runtime: 217 ms (16.76%), Memory: 18 MB (39.63%) - Gitcode

---
+++ 424-longest-repeating-character-replacement/424. Longest Repeating Character Replacement.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        freq = defaultdict(int)
+
+        l = 0
+        maxF = 0
+        ans = 0
+        for r in range(len(s)):
+            freq[s[r]] += 1
+            if freq[s[r]] > maxF:
+                maxF = freq[s[r]]
+            
+            while (r - l + 1) - maxF > k:
+                freq[s[l]] -= 1
+                l += 1
+                maxF = 0
+                for val in freq.values():
+                    maxF = max(maxF, val)
+            
+            ans = max(ans, r - l + 1)
+
+        return ans
