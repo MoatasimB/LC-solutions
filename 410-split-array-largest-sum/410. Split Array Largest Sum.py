ef2a7011fe7b499c3863b97038faa4acfd92302f
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2025-12-31 22:54:14 -0500

    Runtime: 3 ms (67.36%), Memory: 17.3 MB (91.27%) - Gitcode

---
+++ 410-split-array-largest-sum/410. Split Array Largest Sum.py
@@ -0,0 +1,35 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        
+        def check(mid):
+            #mid is the largest sum we can have
+            splits = 0
+
+            curr = 0
+            for num in nums:
+                if num + curr > mid:
+                    curr = 0
+                    splits += 1
+                
+                curr += num
+                if curr > mid:
+                    return False
+            
+            return splits + 1 <= k
+
+
+        l = min(nums)
+        r = sum(nums)
+
+        ans = float("inf")
+        while l <= r:
+            mid = (l + r) // 2
+
+            if check(mid):
+                ans = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        
+        return ans
+
