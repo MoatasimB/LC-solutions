dbd7727fe96310ffa920763074a10f7cbaabf600
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-01-07 20:29:35 -0500

    Runtime: 0 ms (100.00%), Memory: 18 MB (26.98%) - Gitcode

--- 162-find-peak-element/162. Find Peak Element.py
+++ 162-find-peak-element/162. Find Peak Element.py
@@ -1,21 +1,11 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        
         l = 0
         r = len(nums) - 1
-        
         while l < r:
             mid = (l + r) // 2
-            
-            prev = nums[mid - 1] if (mid - 1 >= 0) else float("-inf")
-            n = nums[mid + 1] if (mid + 1 < len(nums)) else float("inf")
-            
-            if prev < nums[mid] and nums[mid] > n:
-                return mid
-            if nums[mid] < nums[mid + 1]:
-                l = mid + 1
-            else:
+            if nums[mid] > nums[mid + 1]:
                 r = mid
-        
-        
-        return l
+            else:
+                l = mid + 1
+        return r
