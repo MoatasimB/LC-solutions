34c5af6640ad0bc4f35a076e27f49ca9e8c7250a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-01-07 20:24:21 -0500

    Runtime: 813 ms (6.88%), Memory: 20 MB (10.77%) - Gitcode

--- 18-4sum/18. 4Sum.py
+++ 18-4sum/18. 4Sum.py
@@ -1,37 +1,48 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        
         nums.sort()
-
-        def twoSum(l, target):
-            r = len(nums) - 1
-            while l < r:
-
-                if nums[l] + nums[r] == target:
-                    ans.append(quad + [nums[l]] + [nums[r]])
-                    l += 1
-                    r -= 1
-                    while l < r and nums[l - 1] == nums[l]:
-                        l += 1
-                elif nums[l] + nums[r] > target:
-                    r -= 1
-                else:
-                    l += 1
-        
-        ans = []
         quad = []
-        def kSum(l, k, target):
-            if k == 2:
-                return twoSum(l, target)
+        ans = []
+        def twoSum(l):
+            r = len(nums) - 1
             
-            i = 0
+            while l < r:
+                if quad[0] + quad[1] + nums[l] + nums[r] == target:
+                    ans.append([quad[0], quad[1], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                elif quad[0] + quad[1] + nums[l] + nums[r] < target:
+                    l += 1
+                else:
+                    r -= 1
 
-            for i in range(l, len(nums) - k + 1):
-                if i > l and nums[i] == nums[i - 1]:
-                    continue
-                quad.append(nums[i])
-                kSum(i + 1, k - 1, target - nums[i])
-                quad.pop()
 
-        kSum(0, 4, target)
+
+
+
+        def kSum(k,l):
+            if k == 2:
+                twoSum(l)
+                return
+            
+            # i = l
+            # while i < len(nums):
+            #     quad.append(nums[i])
+            #     kSum(k - 1, i + 1)
+            #     quad.pop()
+                
+            #     i += 1
+            #     while i < len(nums) and nums[i] == nums[i - 1]:
+            #         i += 1
+            
+
+            for i in range(l, len(nums)):
+                if i == l or nums[i] != nums[i - 1]:
+                    quad.append(nums[i])
+                    kSum(k - 1, i + 1)
+                    quad.pop()
+
+        kSum(4, 0)
         return ans
