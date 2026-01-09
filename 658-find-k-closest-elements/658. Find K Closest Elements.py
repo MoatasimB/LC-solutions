c8554ca0e0ae77a2e153912db69eda684014da77
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-01-08 23:09:06 -0500

    Runtime: 11 ms (55.06%), Memory: 20.7 MB (5.27%) - Gitcode

--- 658-find-k-closest-elements/658. Find K Closest Elements.py
+++ 658-find-k-closest-elements/658. Find K Closest Elements.py
@@ -1,15 +1,36 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         
-        l = 0 
+        #find x in arr
+        n = len(arr)
+        l = 0
         r = len(arr) - 1
 
-        while r - l + 1 > k:
-            distL = abs(x - arr[l])
-            distR = abs(x - arr[r])
-            if distL <= distR:
-                r -= 1
-            else:
-                l += 1
+        while l <= r:
+            mid = (l + r) // 2
 
-        return arr[l:r + 1]
+            if arr[mid] <= x:
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        pos = r
+
+        l = r
+        r = r + 1
+
+        # l = ans - 1
+        # r = ans
+
+        while k > 0:
+            distL = abs(arr[l] - x) if l >= 0 else float("inf")
+            distR = abs(arr[r] - x) if r < n else float("inf")
+            if distL <= distR:
+                k -= 1
+                l -= 1
+            else:
+                k -= 1
+                r += 1
+        
+        return arr[l+1 : r]
+            
