3af03d05fa22a4cc69fc267206065ab4d3aee73c
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-01-13 17:34:54 -0500

    Runtime: 0 ms (100.00%), Memory: 17.5 MB (69.58%) - Gitcode

--- 198-house-robber/198. House Robber.py
+++ 198-house-robber/198. House Robber.py
@@ -9,11 +9,15 @@ class Solution:
         if len(nums) == 2:
             return max(nums[0], nums[1])
         
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-
+        # dp[0] = nums[0]
+        # dp[1] = max(nums[0], nums[1])
+        two = nums[0]
+        one = max(nums[0], nums[1])
         for i in range(2, len(nums)):
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+            curr = max(nums[i] + two, one)
+            two = one
+            one = curr 
+            # dp[i] = max(nums[i] + dp[i-2], dp[i-1])
         
-        return dp[len(nums) - 1]
+        return one
 
