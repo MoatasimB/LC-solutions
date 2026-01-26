dd75dbf64333fa6269726a7d9b550f13c74d1457
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-01-26 17:57:53 -0500

    Runtime: 40 ms (8.96%), Memory: 20.2 MB (8.41%) - Gitcode

--- 1244-design-a-leaderboard/1244. Design A Leaderboard.py
+++ 1244-design-a-leaderboard/1244. Design A Leaderboard.py
@@ -1,22 +1,35 @@
 class Leaderboard:
 
     def __init__(self):
-        self.mpp = {}
-        self.pq = []
+        self.maxHeap = []
+        self.players = defaultdict(int)
 
     def addScore(self, playerId: int, score: int) -> None:
-        if playerId not in self.mpp:
-            self.mpp[playerId] = score
-        else:
-            self.mpp[playerId] += score        
+        self.players[playerId] += score
+        heapq.heappush(self.maxHeap, [-self.players[playerId], playerId])
+        
 
     def top(self, K: int) -> int:
-        x = sorted(self.mpp.values(), reverse=True)
+        topK = []
+        final = 0
+        seen = set()
+        while self.maxHeap and len(topK) != K:
+            topScore, playerId = heapq.heappop(self.maxHeap)
+            topScore *= -1
+            if self.players[playerId] == topScore and playerId not in seen:
+                seen.add(playerId)
+                topK.append([-topScore, playerId])
+                final += topScore
         
-        return sum(x[:K])
+
+        for s, i in topK:
+            heapq.heappush(self.maxHeap, [s, i])
+        return final
+        
 
     def reset(self, playerId: int) -> None:
-        self.mpp[playerId] = 0
+        self.players[playerId] = 0
+        
 
 
 # Your Leaderboard object will be instantiated and called as such:
@@ -24,3 +37,6 @@ class Leaderboard:
 # obj.addScore(playerId,score)
 # param_2 = obj.top(K)
 # obj.reset(playerId)
+
+# 1: 2
+# 2: 1
