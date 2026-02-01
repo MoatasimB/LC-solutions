cc7b6fd9e7a5414d9dc1f93873dafc25e6d7778e
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-02-01 16:53:24 -0500

    Runtime: 54 ms (100.00%), Memory: 20.3 MB (100.00%) - Gitcode

--- 3829-design-ride-sharing-system/3829. Design Ride Sharing System.py
+++ 3829-design-ride-sharing-system/3829. Design Ride Sharing System.py
@@ -3,12 +3,12 @@ class RideSharingSystem:
     def __init__(self):
         self.riders = []
         self.drivers = []
-        self.riderIdPeople = {}
+        self.active = set()
         self.t = 0
 
     def addRider(self, riderId: int) -> None:
         heapq.heappush(self.riders, [self.t, riderId])
-        self.riderIdPeople[riderId] = self.t
+        self.active.add(riderId)
         self.t += 1
         
 
@@ -18,7 +18,7 @@ class RideSharingSystem:
         
 
     def matchDriverWithRider(self) -> List[int]:
-        while self.riders and self.riders[0][1] not in self.riderIdPeople:
+        while self.riders and self.riders[0][1] not in self.active:
             heapq.heappop(self.riders)
         
         if not self.riders or not self.drivers:
@@ -27,12 +27,12 @@ class RideSharingSystem:
         t, riderId = heapq.heappop(self.riders)
         dt, driverId = heapq.heappop(self.drivers)
 
-        del self.riderIdPeople[riderId]
+        self.active.remove(riderId)
         return [driverId, riderId]        
 
     def cancelRider(self, riderId: int) -> None:
-        if riderId in self.riderIdPeople:
-            del self.riderIdPeople[riderId]
+        if riderId in self.active:
+            self.active.remove(riderId)
         
 
 
