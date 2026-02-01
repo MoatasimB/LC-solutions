f3d6d365e755eb159ad901f2f417a52abc108c61
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-02-01 17:18:34 -0500

    Runtime: 34 ms (0.71%), Memory: 16.7 MB (100.00%) - Gitcode

--- 362-design-hit-counter/362. Design Hit Counter.py
+++ 362-design-hit-counter/362. Design Hit Counter.py
@@ -1,57 +1,32 @@
 class HitCounter:
 
     def __init__(self):
-        self.nums = []
-        
+        self.q = deque()
+        self.total =0
 
     def hit(self, timestamp: int) -> None:
-        self.nums.append(timestamp)
+        if self.q and timestamp == self.q[-1][1]:
+            x = self.q.pop()
+            count = x[1]
+            t = x[0]
+            count +=1
+            self.q.append((t,count))
+            
+        else:
+            self.q.append((timestamp, 1))
         
-
+        self.total +=1
     def getHits(self, timestamp: int) -> int:
-        start = max(0, timestamp - 300) + 1
-        end = timestamp
-        print(timestamp, start, end, self.nums)
-        #smallet timestamp idx greater than start
-        #largest timestamp idx less than end
-        if not self.nums or self.nums[-1] < start:
-            return 0
-        l = 0
-        r = len(self.nums) - 1
-        firstIdx = r
-        while l <= r:
-            mid = (l + r) // 2
-
-            if self.nums[mid] >= start:
-                firstIdx = mid
-                r = mid - 1
+        while self.q:
+            if timestamp - self.q[0][0] >= 300:
+                x = self.q.popleft()
+                self.total -= x[1]
             else:
-                l = mid + 1
-        
+                break
+        return self.total
 
-        l = 0
-        r = len(self.nums) - 1
-        lastIdx = 0
-        while l <= r:
-            mid = (l + r) // 2
-
-            if self.nums[mid] <= end:
-                lastIdx = mid
-                l = mid + 1
-            else:
-                r = mid - 1
-        
-        return lastIdx - firstIdx + 1
-
-        # start = 1
-        # end = 301
-        # [1 2 3 300]
 
 # Your HitCounter object will be instantiated and called as such:
 # obj = HitCounter()
 # obj.hit(timestamp)
 # param_2 = obj.getHits(timestamp)
- 
-#   304
-# 5 - 304
-#  [2 3 4]
