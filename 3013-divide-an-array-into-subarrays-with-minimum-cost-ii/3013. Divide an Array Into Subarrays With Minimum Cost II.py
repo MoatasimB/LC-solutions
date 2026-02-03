3b700cd9977279e3c3e580f0fb5d45605d32f23e
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-02-02 22:50:41 -0500

    Runtime: 1600 ms (22.08%), Memory: 33.4 MB (49.35%) - Gitcode

---
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013. Divide an Array Into Subarrays With Minimum Cost II.py
@@ -0,0 +1,56 @@
+class Solution:
+    def minimumCost(self, nums: List[int], k: int, dist: int) -> int:
+        
+        class Container:
+            def __init__(self, k):
+                self.k = k
+                self.low = SortedList()
+                self.rest = SortedList()
+                self.sm = 0
+            
+            def balance(self):
+                while len(self.low) < self.k and self.rest:
+                    val = self.rest[0] 
+                    self.rest.remove(val)
+                    self.low.add(val)
+                    self.sm += val
+                while len(self.low) > self.k:
+                    val = self.low[-1]
+                    self.low.remove(val)
+                    self.rest.add(val)
+                    self.sm -= val
+            
+            def add(self, val):
+                if self.rest and val >= self.rest[0]:
+                    self.rest.add(val)
+                else:
+                    self.low.add(val)
+                    self.sm += val
+                self.balance()
+            
+            def remove(self, val):
+                if val in self.rest:
+                    self.rest.remove(val)
+                elif val in self.low:
+                    self.low.remove(val)
+                    self.sm -= val
+                self.balance()
+            
+            def getSum(self):
+                return self.sm
+        n = len(nums)
+        cont = Container(k - 2)
+        for i in range(1, k - 1):
+            cont.add(nums[i])
+        
+        ans = cont.getSum() + nums[k - 1]
+
+        for i in range(k, n):
+            j = i - dist - 1
+            if j > 0:
+                cont.remove(nums[j])
+            cont.add(nums[i - 1])
+
+            ans = min(ans, cont.getSum() + nums[i])
+        
+        return ans + nums[0]
