165b2fff6b6388051d5c5bc4840a930a51c8eae9
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-02-06 22:28:27 -0500

    Runtime: 317 ms (52.05%), Memory: 19.9 MB (63.56%) - Gitcode

--- 1653-minimum-deletions-to-make-string-balanced/1653. Minimum Deletions to Make String Balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653. Minimum Deletions to Make String Balanced.py
@@ -1,26 +1,21 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
         
-        b_count_after = 0
         a_count_after = 0
         for ch in s:
-            if ch == "b":
-                b_count_after += 1
-            else:
+            if ch == "a":
                 a_count_after += 1
+        
         
 
         ans = float("inf")
-        a_count = 0
         b_count = 0
         for ch in s:
             if ch == "a":
                 a_count_after -= 1
                 ans = min(ans, b_count + a_count_after)
-                a_count += 1
 
             else:
-                b_count_after -= 1
                 ans = min(ans, b_count + a_count_after)
                 b_count += 1
         
