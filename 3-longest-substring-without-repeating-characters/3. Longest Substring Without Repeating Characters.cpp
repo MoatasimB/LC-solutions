0d16ff8cd9207a07b98cdfbd5551af1414e11710
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-02-24 19:27:42 -0500

    Runtime: 4 ms (72.11%), Memory: 11.9 MB (45.49%) - Gitcode

---
+++ 3-longest-substring-without-repeating-characters/3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        unordered_map<char, int> mpp;
+
+        int l = 0;
+        int ans = 0;
+        for (int r = 0; r < s.size(); r ++){
+            if (mpp.find(s[r]) != mpp.end()){
+                l = max(l, mpp[s[r]] + 1);
+            }
+            ans = max(ans, r - l + 1);
+            mpp[s[r]] = r;
+
+        }
+
+        return ans;
+        
+    }
+};
