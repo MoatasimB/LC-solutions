08d18347636205ea8be9cef3d3b1c228eb4f50c0
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-02-24 19:51:15 -0500

    Runtime: 15 ms (53.01%), Memory: 22.3 MB (34.13%) - Gitcode

---
+++ 5-longest-palindromic-substring/5. Longest Palindromic Substring.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        int len = 0;
+        std::string ans;
+        for (int i = 0; i < n; i ++){
+            int l = i;
+            int r = i;
+
+            while (l >= 0 && r < n && s[l] == s[r]){
+                if (r - l + 1 > len){
+                    len = r - l + 1;
+                    ans = s.substr(l, len);
+                }
+
+                l--;
+                r++;
+            }
+
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < n && s[l] == s[r]){
+                if (r - l + 1 > len){
+                    len = r - l + 1;
+                    ans = s.substr(l, len);
+                }
+
+                l--;
+                r++;
+            }
+
+
+        }
+
+        return ans;
+        
+    }
+};
