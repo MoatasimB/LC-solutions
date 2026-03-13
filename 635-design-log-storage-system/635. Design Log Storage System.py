6b9c9f065c3d17ef43c6357656bb0eb9c9e0d9d3
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-03-13 00:05:57 -0400

    Runtime: 3 ms (81.38%), Memory: 19.5 MB (94.78%) - Gitcode

--- 635-design-log-storage-system/635. Design Log Storage System.py
+++ 635-design-log-storage-system/635. Design Log Storage System.py
@@ -1,7 +1,7 @@
 class LogSystem:
 
     def __init__(self):
-        self.logs = []
+        self.logs = SortedList()
         self.gran = {
             "Year": 4,
             "Month": 7,
@@ -14,7 +14,7 @@ class LogSystem:
 
     def put(self, id: int, timestamp: str) -> None:
         
-        self.logs.append([timestamp, id])
+        self.logs.add([timestamp, id])
 
         
 
@@ -24,10 +24,41 @@ class LogSystem:
 
         start = start[:i]
         end = end[:i]
-        print(self.logs)
-        print(start, end)
 
-        return [id for log, id in self.logs if start <= log[:i] <= end]
+        first = len(self.logs)
+
+        left = 0
+        right = len(self.logs) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if self.logs[mid][0][:i] >= start:
+                first = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        second = -1
+
+        left = 0
+        right = len(self.logs) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if end >= self.logs[mid][0][:i]:
+                second = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        if second < first:
+            return []
+
+        return [self.logs[i][1] for i in range(first, second + 1)]
+
+        # return [id for log, id in enumerateself.logs if start <= log[:i] <= end]
 
 
         
