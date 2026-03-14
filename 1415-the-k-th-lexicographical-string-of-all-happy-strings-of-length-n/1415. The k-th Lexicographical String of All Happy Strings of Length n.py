045ce3a6a076ec5917e2faf0bbd00db9acfb81d4
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-03-13 23:57:03 -0400

    Runtime: 11 ms (48.44%), Memory: 20.6 MB (29.84%) - Gitcode

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415. The k-th Lexicographical String of All Happy Strings of Length n.py
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415. The k-th Lexicographical String of All Happy Strings of Length n.py
@@ -2,20 +2,21 @@ class Solution:
     def getHappyString(self, n: int, k: int) -> str:
         ans = []
         def dfs(curr):
+            if len(ans) == k:
+                return
             if len(curr) == n:
                 ans.append(curr[:])
-                return
-            if len(ans) == k:
                 return
             for letter in "abc":
                 if not curr or curr[-1] != letter:
                     curr.append(letter)
                     dfs(curr)
                     curr.pop()
-                
+            if len(ans) == k:
+                return 
                 
         
         dfs([])
         print(ans)
-        return "".join(ans[k - 1]) if len(ans) >= k else ""
+        return "".join(ans[-1]) if len(ans) == k else ""
 
