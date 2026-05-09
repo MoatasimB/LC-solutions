9b2d2bffb4cce329c981611cdd75c08cfc1a83ef
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-05-09 00:52:57 -0400

    Runtime: 128 ms (12.86%), Memory: 19.8 MB (2.86%) - Gitcode

---
+++ 1914-cyclically-rotating-a-grid/1914. Cyclically Rotating a Grid.py
@@ -0,0 +1,106 @@
+class Solution:
+    def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+
+        m = len(grid)
+        n = len(grid[0])
+        newGrid = [[0] * n for _ in range(m)]
+        
+        def rev(start, end, cells):
+            while start < end:
+                cells[start], cells[end] = cells[end], cells[start]
+                start += 1
+                end -= 1
+            print(cells)
+        def applyRotation(sR, sC, eR, eC):
+            cells = []
+            # count how many cells in layer
+            for i in range(sR, eR):
+                cells.append(grid[i][sC])
+            
+            for i in range(sC, eC):
+                cells.append(grid[eR][i])
+
+            for i in range(eR, sR, -1):
+                cells.append(grid[i][eC])
+            
+            for i in range(eC, sC, -1):
+                cells.append(grid[sR][i])
+            
+            cell_len = len(cells)
+
+            # mod by k
+            rotations = k % cell_len
+            start = 0
+            end = cell_len - rotations - 1
+            while start < end:
+                cells[start], cells[end] = cells[end], cells[start]
+                start += 1
+                end -= 1
+            start = cell_len - rotations
+            end = cell_len - 1
+            while start < end:
+                cells[start], cells[end] = cells[end], cells[start]
+                start += 1
+                end -= 1
+            start = 0
+            end = cell_len - 1
+            while start < end:
+                cells[start], cells[end] = cells[end], cells[start]
+                start += 1
+                end -= 1
+            # #apply rotation
+            # rev(0, cell_len - rotations - 1, cells)
+            # # print(cells)
+
+            # rev(cell_len - rotations,cell_len - 1, cells)
+            # # print(cells)
+
+            
+
+            # rev(0, cell_len - 1, cells)
+            # print(cells)
+
+            # 30 20 10 40
+
+            # 20 10 40 30
+            idx = 0
+            for i in range(sR, eR):
+                newGrid[i][sC] = cells[idx]
+                idx += 1
+            
+            for i in range(sC, eC):
+                newGrid[eR][i] = cells[idx]
+                idx += 1
+
+            for i in range(eR, sR, -1):
+                newGrid[i][eC] = cells[idx]
+                idx += 1
+            
+            for i in range(eC, sC, -1):
+                newGrid[sR][i] = cells[idx]
+                idx += 1
+            
+            
+        
+        # [10, 6, 7, 11]
+        row = 0
+        col = 0
+
+        # [1 16 .... 4 3 2]
+        while row != (m // 2) and col != (n // 2):
+
+            sR = row
+            sC = col
+            eR = m - row - 1
+            eC = n - col - 1
+            applyRotation(sR, sC, eR, eC)
+           
+
+
+
+            row += 1
+            col += 1
+        
+        return newGrid
+
+       
