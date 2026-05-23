01063c9a8790b16f522df3b7ea6225833b01d691
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-05-22 23:12:15 -0400

    Runtime: 0 ms (100.00%), Memory: 19.2 MB (53.16%) - Gitcode

--- 1752-check-if-array-is-sorted-and-rotated/1752. Check if Array Is Sorted and Rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752. Check if Array Is Sorted and Rotated.py
@@ -1,13 +1,19 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        if len(nums) <=1:
-            return True
-        switch = 0
-
-        for i in range(1, len(nums)):
-            if nums[i] < nums[i-1]:
-                switch +=1
         
+        n = len(nums)
+
+        if n == 1:
+            return True
+        
+        count = 0
+        for i in range(1, n):
+            if nums[i - 1] > nums[i]:
+                count += 1
+        
+
         if nums[-1] > nums[0]:
-            switch +=1
-        return switch == 1 or switch == 0
+            count += 1
+        
+        return count < 2
+            
