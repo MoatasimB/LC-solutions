3efccaf008dbaa9302182f7e3c5f06e0e4f77014
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-06-06 00:31:26 -0400

    Runtime: 64 ms (20.73%), Memory: 19.4 MB (83.33%) - Gitcode

--- 723-candy-crush/723. Candy Crush.py
+++ 723-candy-crush/723. Candy Crush.py
@@ -1,40 +1,56 @@
 class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
-        
+        m = len(board)
+        n = len(board[0])
+
         def crush():
+            cells_to_crush = set()
+            #column
+            for col in range(n):
+                for row in range(1, m - 1):
+                    prev = board[row - 1][col]
+                    curr = board[row][col]
+                    next = board[row + 1][col]
 
-            crushed = set()
+                    if prev == curr == next and curr != 0:
+                        cells_to_crush.add((row - 1, col))
+                        cells_to_crush.add((row, col))
+                        cells_to_crush.add((row + 1, col))
 
-            for r in range(len(board) - 2):
-                for c in range(len(board[0])):
-                    if board[r][c] != 0 and board[r][c] == board[r + 1][c] == board[r+2][c]:
-                        crushed.add((r,c))
-                        crushed.add((r+1, c))
-                        crushed.add((r+2, c))
-            for r in range(len(board)):
-                for c in range(len(board[0]) - 2):
-                    if board[r][c] != 0 and board[r][c] == board[r][c + 1] == board[r][c + 2]:
-                        crushed.add((r, c))
-                        crushed.add((r, c + 1))
-                        crushed.add((r, c + 2))
+
+            #row
+            for row in range(m):
+                for col in range(1, n - 1):
+                    prev = board[row][col - 1]
+                    curr = board[row][col]
+                    next = board[row][col + 1]
+
+                    if prev == curr == next and curr != 0:
+                        cells_to_crush.add((row, col - 1))
+                        cells_to_crush.add((row, col))
+                        cells_to_crush.add((row, col + 1))
+            for r, c in cells_to_crush:
+                board[r][c] = 0
             
-            if len(crushed) > 0:
-                for r,c in crushed:
-                    board[r][c] = 0
-            
+            print(board)
+            return len(cells_to_crush) > 0
+        
+        def moveDown():
+            for col in range(n):
+                i = m
 
-                for c in range(len(board[0])):
-                    last = len(board) - 1
-                    for r in range(len(board) - 1, -1, -1):
-                        if board[r][c] != 0:
-                            board[r][c], board[last][c] = board[last][c], board[r][c]
-                            last -= 1
-            
-            return crushed
-
-        while len(crush()) > 0:
-            crush()
-
-                        
-
+                for row in range(m - 1, -1, -1):
+                    if board[row][col] == 0:
+                        continue
+                    else:
+                        board[row][col], board[i - 1][col] = board[i - 1][col], board[row][col]
+                        i -= 1
+        
+        while True:
+            if not crush():
+                break
+            moveDown()
+        
         return board
+
+       
