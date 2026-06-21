a0154de48392c00f2d0dbae8cf956970891772ff
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-06-21 00:35:25 -0400

    Runtime: 621 ms (5.27%), Memory: 30.4 MB (99.07%) - Gitcode

--- 1833-maximum-ice-cream-bars/1833. Maximum Ice Cream Bars.py
+++ 1833-maximum-ice-cream-bars/1833. Maximum Ice Cream Bars.py
@@ -1,21 +1,18 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
         
-        bars = [0] * (max(costs) + 1)
-
-        for c in costs:
-            bars[c] += 1
-        
         ans = 0
-        print(bars)
-        for i, bar in enumerate(bars):
-            if bar > coins:
-                return ans
-            if i == 0 or bar <= 0:
-                continue
-            take = min(bar, (coins // i))
-            # print(bar, take, coins)
-            coins -= take * i
-            ans += take
-        return ans
+
+        costs = sorted(costs)
+
+        curr = 0
+        for i in range(len(costs)):
+            curr += costs[i]
+
+            if curr > coins:
+                break
             
+            ans +=1
+        
+        return ans
+        
