4defa18c792c69139d3ae38f231352daa503f3e4
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-07-06 21:07:05 -0400

    Runtime: 80 ms (42.82%), Memory: 19.7 MB (29.21%) - Gitcode

---
+++ 3286-find-a-safe-walk-through-a-grid/3286. Find a Safe Walk Through a Grid.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        
+        m = len(grid)
+        n = len(grid[0])
+
+        minHeap = [[grid[0][0], 0, 0]] #pathCost, r, c
+        dists = [[float("inf")] * n for _ in range(m)]
+        dists[0][0] = grid[0][0]
+        dirs = [(0, 1), (1,0), (0,-1), (-1,0)]
+        def valid(r, c):
+            return 0 <= r < m and 0 <= c < n
+
+        while minHeap:
+            pathCost, r, c = heapq.heappop(minHeap)
+
+            if pathCost > dists[r][c]:
+                continue
+                
+            if (r, c) == (m - 1, n - 1):
+                return pathCost <= health
+            
+            for dx, dy in dirs:
+                nr, nc = r + dx, c + dy
+                if valid(nr, nc):
+                    newPath = grid[nr][nc] + pathCost
+                    if newPath < dists[nr][nc] and newPath < health:
+                        dists[nr][nc] = newPath
+                        heapq.heappush(minHeap, [newPath, nr, nc])
+                    
+        
+        return False
