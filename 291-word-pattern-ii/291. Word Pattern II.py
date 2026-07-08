e658d9361812037d02e5994ac35b6951c1e576c6
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-07-08 00:00:58 -0400

    Runtime: 0 ms (100.00%), Memory: 19.4 MB (28.57%) - Gitcode

---
+++ 291-word-pattern-ii/291. Word Pattern II.py
@@ -0,0 +1,35 @@
+class Solution:
+    def wordPatternMatch(self, pattern: str, s: str) -> bool:
+        
+        n = len(pattern)
+        m = len(s)
+
+        def dfs(i, j, mpp):
+            if i == n and j == m:
+                if len(set(mpp.values())) == len(set(pattern)):
+                    print(set(mpp.values()), set(pattern))
+                    return True
+                return False
+            if i == n or j == m:
+                return False
+            
+            currCh = pattern[i]
+            if currCh in mpp:
+                mapping = mpp[currCh]
+                length = len(mapping)
+                if s[j:j + length] != mapping:
+                    return False
+                else:
+                    return dfs(i + 1, j + length, mpp)
+            else:
+                for w in range(j, m):
+                    match = s[j:w+1]
+                    mpp[currCh] = match
+                    # print(currCh, match, mpp, i, j)
+                    if dfs(i + 1, w + 1, mpp):
+                        return True
+                    del mpp[currCh]
+                return False
+        
+
+        return dfs(0, 0, {})
