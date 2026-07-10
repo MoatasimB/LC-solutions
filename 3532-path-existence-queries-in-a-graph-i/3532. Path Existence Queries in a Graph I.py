7037d2fdb56c9bfd512997e2c4b99546e38bdefd
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-07-10 00:38:54 -0400

    Runtime: 90 ms (61.29%), Memory: 49.5 MB (79.84%) - Gitcode

--- 3532-path-existence-queries-in-a-graph-i/3532. Path Existence Queries in a Graph I.py
+++ 3532-path-existence-queries-in-a-graph-i/3532. Path Existence Queries in a Graph I.py
@@ -1,32 +1,4 @@
-class DSU:
-    def __init__(self, size):
-        self.roots = [i for i in range(size)]
-        self.ranks = [0] * size
-    
-    def find(self, x):
-        if self.roots[x] == x:
-            return x
-        
-        self.roots[x] = self.find(self.roots[x])
-        return self.roots[x]
-    
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
 
-        if rootX == rootY:
-            return
-        
-        if self.ranks[rootX] < self.ranks[rootY]:
-            self.roots[rootX] = rootY
-        elif self.ranks[rootY] < self.ranks[rootX]:
-            self.roots[rootY] = rootX
-        else:
-            self.ranks[rootY] += 1
-            self.roots[rootX] = rootY
-    
-    def isConnected(self, x, y):
-        return self.find(x) == self.find(y)
 
 class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
@@ -41,16 +13,4 @@ class Solution:
                 curr += 1
             components[i] = curr
         return [(components[x] == components[y]) for x, y in queries]
-        dsu = DSU(n)
-        for i in range(1, n):
-            prevNum = nums[i - 1]
-            currNum = nums[i]
-
-            if abs(prevNum - currNum) <= maxDiff:
-                dsu.union(i - 1, i)
-        
-
-        return [dsu.isConnected(x, y) for x, y in queries]
-
-
-        
+       
