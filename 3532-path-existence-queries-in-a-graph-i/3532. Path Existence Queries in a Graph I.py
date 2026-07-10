ca1e2b18ad8c9c8d06db222e6641414074f6cdd3
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-07-10 00:38:26 -0400

    Runtime: 77 ms (70.97%), Memory: 50.3 MB (37.90%) - Gitcode

--- 3532-path-existence-queries-in-a-graph-i/3532. Path Existence Queries in a Graph I.py
+++ 3532-path-existence-queries-in-a-graph-i/3532. Path Existence Queries in a Graph I.py
@@ -31,7 +31,16 @@ class DSU:
 class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
 
+        components = [0] * n
+        curr = 0
+        for i in range(1, n):
+            prevNum = nums[i - 1]
+            currNum = nums[i]
 
+            if abs(prevNum - currNum) > maxDiff:
+                curr += 1
+            components[i] = curr
+        return [(components[x] == components[y]) for x, y in queries]
         dsu = DSU(n)
         for i in range(1, n):
             prevNum = nums[i - 1]
