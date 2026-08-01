3005e97bc3d70b5a2c5b94cbc755e54662e0fdb8
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-07-31 20:49:21 -0400

    Runtime: 0 ms (100.00%), Memory: 19.9 MB (20.24%) - Gitcode

--- 486-predict-the-winner/486. Predict the Winner.py
+++ 486-predict-the-winner/486. Predict the Winner.py
@@ -1,18 +1,20 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
         
-
+        memo = {}
         def dfs(i, j):
             if i > j:
                 return 0
             if i == j:
                 return nums[i]
+            if (i, j) in memo:
+                return memo[(i, j)]
             
             score = float("-inf")
             curr = 0
             score = max(score, nums[i] - dfs(i + 1, j))
             score = max(score, nums[j] - dfs(i, j - 1))
-
+            memo[(i, j)] = score
             return score
         
         n = len(nums)
