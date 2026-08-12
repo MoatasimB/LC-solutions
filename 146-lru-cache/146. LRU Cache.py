cd87fd8161765edf9019d5d847f96a421fc5803c
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-08-12 00:51:34 -0400

    Runtime: 154 ms (19.63%), Memory: 77 MB (53.54%) - Gitcode

--- 146-lru-cache/146. LRU Cache.py
+++ 146-lru-cache/146. LRU Cache.py
@@ -1,69 +1,67 @@
 class Node:
     def __init__(self, key, val):
-        self.key = key
-        self.val = val
         self.next = None
         self.prev = None
-
+        self.key = key
+        self.val = val
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.mpp = {} #key : node
-        self.head = Node(-1, -1)
+        self.head = Node(-1,-1)
         self.tail = Node(-1, -1)
+        self.mpp = {}
         self.head.next = self.tail
         self.tail.prev = self.head
-        
 
     def get(self, key: int) -> int:
         if key not in self.mpp:
             return -1
-        
         node = self.mpp[key]
+        ans = node.val
 
-        self.deleteNode(node)
-        self.addNode(node)
+        self.remove(node)
+        self.add(node)
+        return ans
         
-        return node.val
 
     def put(self, key: int, value: int) -> None:
-
         if key in self.mpp:
             node = self.mpp[key]
-            self.deleteNode(node)
+            self.remove(node)
             del self.mpp[key]
         
-        newNode = Node(key, value)
-
-        self.mpp[key] = newNode
-
-        self.addNode(newNode)
+        node = Node(key, value)
+        self.add(node)
+        self.mpp[key] = node
 
         if len(self.mpp) > self.cap:
-            lruNode = self.tail.prev
-            self.deleteNode(lruNode)
-            del self.mpp[lruNode.key]
-        
+            removeNode = self.tail.prev
+            self.remove(removeNode)
+            del self.mpp[removeNode.key]
     
-
-    def addNode(self, node):
-
-        nextNode = self.head.next
-
-        self.head.next = node
-        node.prev = self.head
-
-        node.next = nextNode
-        nextNode.prev = node
-
-    def deleteNode(self, node):
+    def remove(self, node):
         prevNode = node.prev
         nextNode = node.next
 
         prevNode.next = nextNode
         nextNode.prev = prevNode
+    
+    def add(self, node):
+        prevNode = self.head
+        nextNode = self.head.next
+
+        node.prev = prevNode
+        prevNode.next = node
+
+        node.next = nextNode
+        nextNode.prev = node
+
+    
+        
+
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
