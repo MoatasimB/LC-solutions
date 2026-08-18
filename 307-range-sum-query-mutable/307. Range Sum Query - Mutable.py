94cd2ee1f014a06492053336e22fcd085dfa1f3a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-08-17 23:48:27 -0400

    Runtime: 817 ms (30.27%), Memory: 39.8 MB (29.28%) - Gitcode

--- 307-range-sum-query-mutable/307. Range Sum Query - Mutable.py
+++ 307-range-sum-query-mutable/307. Range Sum Query - Mutable.py
@@ -22,7 +22,7 @@ class SegTree:
         self._update(1, 0, self.n - 1, idx, val)
     def _update(self, node_idx, left_idx, right_idx, idx, val):
         if left_idx == right_idx:
-            self.tree[node_idx] = val
+            self.tree[node_idx] += val
             return
         mid = (left_idx + right_idx) // 2
         left_child_idx = node_idx * 2
@@ -47,6 +47,12 @@ class SegTree:
         mid = (left_idx + right_idx) // 2
         left_child_idx = node_idx * 2
         right_child_idx = node_idx * 2 + 1
+
+        if range_right <= mid:
+            return self._findRangeQuery(left_child_idx, left_idx, mid, range_left, range_right)
+        elif mid < range_left:
+            return self._findRangeQuery(right_child_idx, mid + 1, right_idx, range_left, range_right)
+
         left = self._findRangeQuery(left_child_idx, left_idx, mid, range_left, range_right)
         right = self._findRangeQuery(right_child_idx, mid + 1, right_idx, range_left, range_right)
         return left + right
@@ -58,10 +64,15 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.SegTree = SegTree(nums)
+        self.nums = nums
         
 
     def update(self, index: int, val: int) -> None:
-        self.SegTree.update(index, val)
+        curr = self.nums[index]
+        diff = val - curr
+        self.nums[index] = val
+        self.SegTree.update(index, diff)
+
         
 
     def sumRange(self, left: int, right: int) -> int:
