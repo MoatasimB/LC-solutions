5f7f41ce01986a006df79343bd69cddc1872ce2a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-08-24 00:38:04 -0400

    Runtime: 242 ms (60.63%), Memory: 19.3 MB (79.68%) - Gitcode

--- 351-android-unlock-patterns/351. Android Unlock Patterns.py
+++ 351-android-unlock-patterns/351. Android Unlock Patterns.py
@@ -1,72 +1,42 @@
 class Solution:
     def numberOfPatterns(self, m: int, n: int) -> int:
         
-        grid = [[False] * 3 for _ in range(3)]
 
-        graph = {
-            1: [2, 4, 5, 6, 8],
-            2: [1, 3, 4, 5, 6, 7, 9],
-            3: [2, 5, 6, 4, 8],
-            4: [1, 2, 5, 7, 8, 3, 9],
-            5: [1, 2, 3, 4, 6, 7, 8, 9],
-            6: [2, 3, 5, 8, 9, 1, 7],
-            7: [4, 5, 6, 8, 2],
-            8: [4, 5, 6, 7, 9, 1, 3],
-            9: [5, 6, 8, 2, 4],
+        mpp = {
+            1 : [3, 7, 9], 
+            2 : [8], 
+            3 : [1, 7, 9],
+            4 : [6],
+            5 : [],
+            6 : [4],
+            7 : [1, 3, 9],
+            8 : [2],
+            9 : [1, 3, 7]
         }
 
 
-
-        # def valid(r, c):
-        #     return 0<=r<3 and 0<=c<3
-        
-        # dirs = [(0,1), (1,0), (0,-1), (-1,0), (1,1), (1,-1), (-1,1), (-1,-1)]
-
-        def dfs(node, pathLen):
-            count = 0
-            if m <= pathLen <= n:
-                count += 1
-            if pathLen == n:
-                return count
-            for nei in range(1, 10):
-                if nei == node:
+        def dfs(node, count, seen):
+            ans = 0
+            if count > n:
+                return 0
+            if m <= count <= n:
+                ans += 1
+            
+            for neighbor in [1,2,3,4,5,6,7,8,9]:
+                if node == neighbor or neighbor in seen:
                     continue
-                
-                if nei not in seen and pathLen < n and (nei in graph[node] or (nei + node) // 2 in seen):
-                    seen.add(nei)
-                    count += dfs(nei, pathLen + 1)
-                    seen.remove(nei)
-            
-            return count
+                if neighbor not in mpp[node]:
+                    seen.add(neighbor)
+                    ans += dfs(neighbor, count + 1, seen)
+                    seen.remove(neighbor)
+                else:
+                    requiredNode = (node + neighbor) // 2
+                    if requiredNode in seen:
+                        seen.add(neighbor)
+                        ans += dfs(neighbor, count + 1, seen)
+                        seen.remove(neighbor)
 
+            return ans
         
-        # def dfs(r, c, pathLen):
-            # count = 0
-            # if m <= pathLen <= n:
-            #     count += 1
-            # if pathLen == n:
-            #     return count
-            # for dx, dy in dirs:
-            #     nr = r + dx
-            #     nc = c + dy
-
-            #     if valid(nr,nc) and not grid[nr][nc] and pathLen < n:
-            #         grid[nr][nc] = True
-            #         count += dfs(nr, nc, pathLen + 1)
-            #         grid[nr][nc] = False
-            
-            # return count
-        
-        ans = 0
-        # for r in range(3):
-            # for c in range(3):
-            #     grid[r][c] = True
-            #     ans += dfs(r, c, 1)
-            #     grid[r][c] = False
-        seen = set()
-        for i in [1, 2, 5]:
-            seen.add(i)
-            mult = 4 if i != 5 else 1
-            ans += dfs(i, 1) * mult
-            seen.remove(i)
-        return ans
+        final = (4 * dfs(1, 1, set([1]))) + (4 * dfs(2, 1, set([2]))) + dfs(5, 1, set([5]))
+        return final
