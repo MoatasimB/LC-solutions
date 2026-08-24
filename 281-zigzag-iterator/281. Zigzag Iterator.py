abea40715162e2af43a8a353385c4d94e7693830
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-08-23 20:59:38 -0400

    Runtime: 56 ms (48.50%), Memory: 19.4 MB (94.31%) - Gitcode

--- 281-zigzag-iterator/281. Zigzag Iterator.py
+++ 281-zigzag-iterator/281. Zigzag Iterator.py
@@ -1,23 +1,41 @@
 class ZigzagIterator:
     def __init__(self, v1: List[int], v2: List[int]):
-        self.q = deque()
-        if v1:
-            self.q.append([v1, 0])
-        if v2:
-            self.q.append([v2,0])
+        self.indices = [0] * 2
+        self.idxPtr = 0
+        self.v1 = v1
+        self.v2 = v2
+        self.n1 = len(self.v1)
+        self.n2 = len(self.v2)
+        
 
     def next(self) -> int:
-        lst, idx = self.q.popleft()
-        val = lst[idx]
-        idx += 1
-        if idx != len(lst):
-            self.q.append([lst, idx])
-        return val
-            
         
+        if self.idxPtr == 0:
+            idx1 = self.indices[self.idxPtr]
+            if idx1 < self.n1:
+                val = self.v1[idx1]
+                self.indices[self.idxPtr] += 1
+                self.idxPtr = (self.idxPtr + 1 )% 2
+                return val
+            else:
+                self.idxPtr = 1
+                return self.next()
+        else:
+            idx2 = self.indices[self.idxPtr]
+            if idx2 < self.n2:
+                val = self.v2[idx2]
+                self.indices[self.idxPtr] += 1
+                self.idxPtr = (self.idxPtr + 1 )% 2
+                return val
+            else:
+                self.idxPtr = 0
+                return self.next()
+        
+
+
+
     def hasNext(self) -> bool:
-        return len(self.q) != 0
-        
+        return sum(self.indices) < self.n1 + self.n2
 
 # Your ZigzagIterator object will be instantiated and called as such:
 # i, v = ZigzagIterator(v1, v2), []
