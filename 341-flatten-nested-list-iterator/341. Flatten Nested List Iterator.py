9bc08341244b6a1087d683d91feb2abf07bcc306
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-08-31 20:46:28 -0400

    Runtime: 55 ms (83.60%), Memory: 20.3 MB (100.00%) - Gitcode

--- 341-flatten-nested-list-iterator/341. Flatten Nested List Iterator.py
+++ 341-flatten-nested-list-iterator/341. Flatten Nested List Iterator.py
@@ -22,25 +22,35 @@
 
 class NestedIterator:
     def __init__(self, nestedList: [NestedInteger]):
-        self.lst = []
-
-        def dfs(nestedL):
-            for nestedInt in nestedL:
-                if nestedInt.isInteger():
-                    self.lst.append(nestedInt.getInteger())
-                else:
-                    dfs(nestedInt.getList())
-        
-        self.i = -1
-        dfs(nestedList)
+        self.stack = [[nestedList, 0]]
     
+    def makeTopInt(self):
+        while self.stack:
+            currTop = self.stack[-1][0]
+            currIdx = self.stack[-1][1]
+            if len(currTop) == currIdx:
+                self.stack.pop()
+                continue
+            
+            if currTop[currIdx].isInteger():
+                break
+            
+            self.stack[-1][1] += 1
+            self.stack.append([currTop[currIdx].getList(), 0])
+            
+            
     def next(self) -> int:
-        self.i += 1
-        return self.lst[self.i]
-        
+        self.makeTopInt()
+        currTop = self.stack[-1][0]
+        currIdx = self.stack[-1][1]
+        self.stack[-1][1] += 1
+        return currTop[currIdx].getInteger()
     
     def hasNext(self) -> bool:
-         return self.i < len(self.lst) - 1
+        self.makeTopInt()
+        
+        return len(self.stack) > 0
+         
 
 # Your NestedIterator object will be instantiated and called as such:
 # i, v = NestedIterator(nestedList), []
