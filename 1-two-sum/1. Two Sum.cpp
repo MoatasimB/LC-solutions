5d8674c86fb8dca1ddd3c4563021e6092a887b8b
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-04 19:47:32 -0400

    Runtime: 0 ms (100.00%), Memory: 14.8 MB (43.96%) - Gitcode

--- 1-two-sum/1. Two Sum.cpp
+++ 1-two-sum/1. Two Sum.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
-        unordered_map<int, int> mpp;
+        std::unordered_map<int, int> mpp;
 
-        for (int i = 0; i < nums.size(); i++){
-            int goal = target - nums[i];
+        int n = nums.size();
 
-            if (mpp.find(goal) != mpp.end()){
-                return {mpp[goal], i};
+        for (int i{0}; i < n; i ++){
+            if (mpp.find((target - nums[i])) != mpp.end()){
+                return vector<int>{mpp[target-nums[i]], i};
             }
             mpp[nums[i]] = i;
+
         }
         return {};
-        
     }
 };
