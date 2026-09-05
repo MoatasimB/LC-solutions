b88a473f5a84d18d1a5010439c508d7d35870fde
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-05 00:00:36 -0400

    Runtime: 115 ms (93.18%), Memory: 33.1 MB (32.73%) - Gitcode

--- 3904-smallest-stable-index-ii/3904. Smallest Stable Index II.py
+++ 3904-smallest-stable-index-ii/3904. Smallest Stable Index II.py
@@ -2,14 +2,9 @@ class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         
         n = len(nums)
-        mmax = [0] * n
         mmin = [0] * n
         
-        curr = nums[0]
-        mmax[0] = curr
-        for i in range(1, n):
-            curr = max(curr, nums[i])
-            mmax[i] = curr
+
         
         curr = nums[-1]
         mmin[-1] = curr
@@ -17,9 +12,10 @@ class Solution:
             curr = min(curr, nums[i])
             mmin[i] = curr
         
-
+        curr = float("-inf")
         for i in range(n):
-            if mmax[i] - mmin[i] <= k:
+            curr = max(curr, nums[i])
+            if curr - mmin[i] <= k:
                 return i
         
         return -1
