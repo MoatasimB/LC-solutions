4ea5fe0ce1507054729346a74b844d9c388f69c0
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-04 23:50:02 -0400

    Runtime: 0 ms (100.00%), Memory: 16.9 MB (100.00%) - Gitcode

--- 355-design-twitter/355. Design Twitter.py
+++ 355-design-twitter/355. Design Twitter.py
@@ -1,52 +1,30 @@
 class Twitter:
 
     def __init__(self):
-        self.tweets = defaultdict(list) #user: [tweets, time]
-        self.follows = defaultdict(set) #user: {follows}
-        self.time = 0
+        self.dic = defaultdict(set)
+        self.stream = deque()
+        
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.tweets[userId].append([tweetId, self.time])
-        self.time += 1
+        self.stream.appendleft((userId, tweetId))
+        
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        feed = []
-        posts = [] #maxHeap
-        self.follows[userId].add(userId)
-        for person in self.follows[userId]:
-            person_posts = self.tweets[person]
-            idx = len(self.tweets[person]) - 1
-            if idx < 0:
-                continue
-            last_id, time = person_posts[idx]
-            heapq.heappush(posts, [-time, last_id, idx, person])
-        
-        while posts and len(feed) < 10:
-            _, last_id, idx, person = heapq.heappop(posts)
-            feed.append(last_id)
-            person_posts = self.tweets[person]
-            idx -=  1
-            if idx < 0:
-                continue
-            last_id, time = person_posts[idx]            
-            heapq.heappush(posts, [-time, last_id, idx, person])
+        ans = []
 
-        return feed
+        for uId, tweet in self.stream:
+            if uId == userId or uId in self.dic[userId]:
+                ans.append(tweet)
+                if len(ans) == 10:
+                    break
         
-
+        return ans
+            
     def follow(self, followerId: int, followeeId: int) -> None:
-        self.follows[followerId].add(followeeId)
+        self.dic[followerId].add(followeeId)
         
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        if followeeId in self.follows[followerId]:
-            self.follows[followerId].remove(followeeId)
+        if followeeId in self.dic[followerId]:
+            self.dic[followerId].remove(followeeId)
         
-
-
-# Your Twitter object will be instantiated and called as such:
-# obj = Twitter()
-# obj.postTweet(userId,tweetId)
-# param_2 = obj.getNewsFeed(userId)
-# obj.follow(followerId,followeeId)
-# obj.unfollow(followerId,followeeId)
