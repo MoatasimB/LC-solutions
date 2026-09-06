67bc4f1cd22b1d6a39148b79de9ac9a70f223899
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-05 22:35:28 -0400

    Runtime: 731 ms (18.84%), Memory: 217.3 MB (20.94%) - Gitcode

--- 115-distinct-subsequences/115. Distinct Subsequences.py
+++ 115-distinct-subsequences/115. Distinct Subsequences.py
@@ -1,27 +1,6 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        m = len(s)
-        n = len(t)
-        prev = [0] * (n + 1)
-
-        prev[0] = 1
         
-        for i in range(1, m + 1):
-            curr = [0] * (n + 1)
-            curr[0] = 1
-            for j in range(1, n + 1):
-
-                if s[i - 1] == t[j - 1]:
-                    curr[j] = prev[j - 1] + prev[j]
-                else:
-                    curr[j] = prev[j]
-            prev = curr
-        
-        return prev[n]
-        
-        return dp[m][n]
-
-
         memo = {}
         def dfs(i, j):
             if (i, j) in memo:
@@ -32,13 +11,17 @@ class Solution:
                 return 0
             
             ans = 0
-
             if s[i] == t[j]:
-                ans += dfs(i - 1, j - 1) + dfs(i - 1, j)
-            else:
-                ans += dfs(i - 1, j)
-            
+                ans += dfs(i - 1, j - 1)
+
+            ans += dfs(i - 1, j)
             memo[(i, j)] = ans
             return ans
         
+        m = len(s)
+        n = len(t)
         return dfs(m - 1, n - 1)
+            #if s[i] == t[j]
+                #we can take or match later
+            #else:
+                #we have to move i - 1
