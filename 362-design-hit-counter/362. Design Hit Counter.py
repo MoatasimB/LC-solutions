5d23bd53eaa60b53748b4050fcf24bd7011f59ed
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-05 22:21:37 -0400

    Runtime: 0 ms (100.00%), Memory: 19.5 MB (41.95%) - Gitcode

--- 362-design-hit-counter/362. Design Hit Counter.py
+++ 362-design-hit-counter/362. Design Hit Counter.py
@@ -1,29 +1,19 @@
 class HitCounter:
 
     def __init__(self):
-        self.q = deque()
-        self.total =0
+        self.lst = deque()
+        
 
     def hit(self, timestamp: int) -> None:
-        if self.q and timestamp == self.q[-1][1]:
-            x = self.q.pop()
-            count = x[1]
-            t = x[0]
-            count +=1
-            self.q.append((t,count))
-            
-        else:
-            self.q.append((timestamp, 1))
+        self.lst.append(timestamp)
         
-        self.total +=1
+
     def getHits(self, timestamp: int) -> int:
-        while self.q:
-            if timestamp - self.q[0][0] >= 300:
-                x = self.q.popleft()
-                self.total -= x[1]
-            else:
-                break
-        return self.total
+        while self.lst and self.lst[0] <= timestamp - 300:
+            self.lst.popleft()
+        
+        return len(self.lst)
+        
 
 
 # Your HitCounter object will be instantiated and called as such:
