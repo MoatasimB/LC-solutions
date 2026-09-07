0d4cfed03e342d3a183598ba896f26846bb4294a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-07 00:34:45 -0400

    Runtime: 12 ms (40.63%), Memory: 19.6 MB (14.58%) - Gitcode

---
+++ 940-distinct-subsequences-ii/940. Distinct Subsequences II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        n = len(s)
+
+        dp = [1]
+        last = {}
+        for i, ch in enumerate(s):
+            dp.append(dp[-1] * 2)
+            if ch in last:
+                dp[-1] -= dp[last[ch]]
+            last[ch] = i
+        
+        return (dp[-1] - 1) % (10**9 + 7)
