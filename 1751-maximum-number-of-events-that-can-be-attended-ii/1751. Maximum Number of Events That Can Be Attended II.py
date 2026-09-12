ce3d10a80a88bca0b010b372c2529e93b7d34973
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-12 01:36:16 -0400

    Runtime: 3098 ms (5.27%), Memory: 240.2 MB (28.57%) - Gitcode

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751. Maximum Number of Events That Can Be Attended II.py
@@ -0,0 +1,45 @@
+class Solution:
+    def maxValue(self, intervals: List[List[int]], k: int) -> int:
+        
+        n = len(intervals)
+        intervals.sort()
+        def find(i):
+            start, end, _ = intervals[i]
+            l = i + 1
+            r = n - 1
+            ans = n
+            while l <= r:
+                mid = (l + r) // 2
+
+                mS, mE, _ = intervals[mid]
+
+                if mS > end:
+                    ans = mid
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return ans
+
+        memo = {}
+        def dfs(i, count):
+            if i >= n or count >= k:
+                return 0
+            if (i, count) in memo:
+                return memo[(i, count)]
+            
+            score = 0
+            #pick
+            n_idx = find(i)
+            l, r, w = intervals[i]
+            # n_idx = i + 1
+            # while n_idx < n and intervals[n_idx][0] <= r:
+            #     n_idx += 1
+            pick = w + dfs(n_idx, count + 1)
+
+            #skip
+            skip = dfs(i + 1, count)
+            score = max(pick, skip)
+            memo[(i, count)] = score
+            return score
+        
+        return dfs(0, 0)
