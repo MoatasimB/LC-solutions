6bba826940fb53636c450272a712a141045b7a35
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-13 16:19:29 -0400

    Runtime: 215 ms (69.71%), Memory: 20 MB (6.27%) - Gitcode

--- 835-image-overlap/835. Image Overlap.py
+++ 835-image-overlap/835. Image Overlap.py
@@ -1,37 +1,21 @@
 class Solution:
-    def largestOverlap(self, A: List[List[int]], B: List[List[int]]) -> int:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        n = len(img1)
+        pos1 = set()
+        pos2 = set()
 
-        dim = len(A)
-
-        def shift_and_count(x_shift, y_shift, M, R):
-            """ 
-                Shift the matrix M in up-left and up-right directions 
-                  and count the ones in the overlapping zone.
-                M: matrix to be moved
-                R: matrix for reference
-
-                moving one matrix up is equivalent to
-                moving the other matrix down
-            """
-            left_shift_count, right_shift_count = 0, 0
-            for r_row, m_row in enumerate(range(y_shift, dim)):
-                for r_col, m_col in enumerate(range(x_shift, dim)):
-                    if M[m_row][m_col] == 1 and M[m_row][m_col] == R[r_row][r_col]:
-                        left_shift_count += 1
-                    if M[m_row][r_col] == 1 and M[m_row][r_col] == R[r_row][m_col]:
-                        right_shift_count += 1
-
-            return max(left_shift_count, right_shift_count)
-
-        max_overlaps = 0
-        # move one of the matrice up and left and vice versa.
-        # (equivalent to move the other matrix down and right)
-        for y_shift in range(0, dim):
-            for x_shift in range(0, dim):
-                # move the matrix A to the up-right and up-left directions
-                max_overlaps = max(max_overlaps, shift_and_count(x_shift, y_shift, A, B))
-                # move the matrix B to the up-right and up-left directions
-                #  which is equivalent to moving A to the down-right and down-left directions 
-                max_overlaps = max(max_overlaps, shift_and_count(x_shift, y_shift, B, A))
-
-        return max_overlaps
+        for r in range(n):
+            for c in range(n):
+                if img1[r][c]:
+                    pos1.add((r, c))
+                if img2[r][c]:
+                    pos2.add((r, c))
+        
+        deltas = defaultdict(int)
+        ans = 0
+        for r1, c1 in pos1:
+            for r2, c2 in pos2:
+                dx, dy = r2 - r1, c2 - c1
+                deltas[(dx, dy)] += 1
+                
+        return max(deltas.values()) if deltas.values() else 0
