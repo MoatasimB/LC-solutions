039110e88a16f3e88ed65be86d9a0272a588e6a0
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-14 20:31:28 -0400

    Runtime: 383 ms (70.55%), Memory: 37.6 MB (97.39%) - Gitcode

--- 307-range-sum-query-mutable/307. Range Sum Query - Mutable.py
+++ 307-range-sum-query-mutable/307. Range Sum Query - Mutable.py
@@ -1,82 +1,51 @@
-class SegTree:
+class BitTree:
     def __init__(self, arr):
-        self.n = len(arr)
         self.arr = arr
-        self.tree = [0] * (4 * self.n)
-        self.buildTree(1, 0, self.n - 1)
-    
-    def buildTree(self, node_idx, left_idx, right_idx):
-        if left_idx == right_idx:
-            self.tree[node_idx] = self.arr[left_idx]
-            return
-        
-        mid = (left_idx + right_idx) // 2
-        left_child_idx = node_idx * 2
-        right_child_idx = node_idx * 2 + 1
-        self.buildTree(left_child_idx, left_idx, mid)
-        self.buildTree(right_child_idx, mid + 1, right_idx)
+        self.n = len(arr)
+        self.tree = [0] * (self.n + 1)
 
-        self.tree[node_idx] = self.tree[left_child_idx] + self.tree[right_child_idx]
-    
-    def update(self, idx, val):
-        self._update(1, 0, self.n - 1, idx, val)
-    def _update(self, node_idx, left_idx, right_idx, idx, val):
-        if left_idx == right_idx:
-            self.tree[node_idx] += val
-            return
-        mid = (left_idx + right_idx) // 2
-        left_child_idx = node_idx * 2
-        right_child_idx = node_idx * 2 + 1
-        if idx <= mid:
-            self._update(left_child_idx, left_idx, mid, idx, val)
-        else:
-            self._update(right_child_idx, mid + 1, right_idx, idx, val)
-        
-        self.tree[node_idx] = self.tree[left_child_idx] + self.tree[right_child_idx]
-    
-    def rangeQuery(self, left, right):
-        return self._findRangeQuery(1, 0, self.n - 1, left, right)
-    
-    def _findRangeQuery(self, node_idx, left_idx, right_idx, range_left, range_right):
-        if range_right < left_idx or range_left > right_idx:
-            return 0
-        
-        if range_left <= left_idx <= right_idx <= range_right:
-            return self.tree[node_idx]
-        
-        mid = (left_idx + right_idx) // 2
-        left_child_idx = node_idx * 2
-        right_child_idx = node_idx * 2 + 1
+        for i, val in enumerate(self.arr):
+            self.update(i, val)
+        print(self.tree)
+    def update(self, i, val):
 
-        if range_right <= mid:
-            return self._findRangeQuery(left_child_idx, left_idx, mid, range_left, range_right)
-        elif mid < range_left:
-            return self._findRangeQuery(right_child_idx, mid + 1, right_idx, range_left, range_right)
-
-        left = self._findRangeQuery(left_child_idx, left_idx, mid, range_left, range_right)
-        right = self._findRangeQuery(right_child_idx, mid + 1, right_idx, range_left, range_right)
-        return left + right
+        i += 1
+        while i <= self.n:
+            self.tree[i] += val
+            i += i & (-i)
     
+    def prefix(self, i):
+        i += 1
+        ans = 0
+        while i > 0:
+            ans += self.tree[i]
+            i -= i & (-i)
+        return ans
+    
+    def query(self, l, r):
+        if l == 0:
+            return self.prefix(r)
+        
+        return self.prefix(r) - self.prefix(l - 1)
 
 
 
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.SegTree = SegTree(nums)
+        self.bitTree = BitTree(nums)
         self.nums = nums
         
 
     def update(self, index: int, val: int) -> None:
-        curr = self.nums[index]
-        diff = val - curr
+        original = self.nums[index]
+        delta = val - original
         self.nums[index] = val
-        self.SegTree.update(index, diff)
-
-        
+        self.bitTree.update(index, delta)
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.SegTree.rangeQuery(left, right)
+        return self.bitTree.query(left, right)
+        
 
 
 # Your NumArray object will be instantiated and called as such:
