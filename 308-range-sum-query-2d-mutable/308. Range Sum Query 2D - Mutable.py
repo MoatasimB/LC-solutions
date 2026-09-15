d022d487bd76cb9ce0ecbc5a22891065cb035fb7
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-14 20:47:45 -0400

    Runtime: 311 ms (47.95%), Memory: 29.1 MB (80.65%) - Gitcode

--- 308-range-sum-query-2d-mutable/308. Range Sum Query 2D - Mutable.py
+++ 308-range-sum-query-2d-mutable/308. Range Sum Query 2D - Mutable.py
@@ -1,93 +1,56 @@
-class SegTree:
-    def __init__(self, arr):
-        self.n = len(arr)
-        self.arr = arr
-        self.tree = [0] * (4 * self.n)
-        self.buildTree(1, 0, self.n - 1)
+class BitTree:
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.m = len(matrix)
+        self.n = len(matrix[0])
+        self.tree = [[0] * (self.n + 1) for _ in range(self.m + 1)]
+
+        for r in range(self.m):
+            for c in range(self.n):
+                self.update(r, c, self.matrix[r][c])
     
-    def buildTree(self, treeIdx, l, r):
-        if l == r:
-            self.tree[treeIdx] = self.arr[l]
-            return self.arr[l]
-        
-        mid = (l + r) // 2
-        leftChildIdx = treeIdx * 2
-        rightChildIdx = treeIdx * 2 + 1
+    def update(self, i, j, val):
 
-        left = self.buildTree(leftChildIdx, l, mid)
-        right = self.buildTree(rightChildIdx, mid + 1, r)
-
-        self.tree[treeIdx] = left + right
-        return self.tree[treeIdx]
+        i += 1
+        while i <= self.m:
+            c = j + 1
+            while c <= self.n:
+                self.tree[i][c] += val
+                c += c & -c
+            i += i &-i
     
-
-    def query(self, l, r):
-        return self._query(1, 0, self.n - 1, l, r)
-    
-    def _query(self, treeIdx, l, r, qLeft, qRight):
-        
-        if qLeft > r or qRight < l:
-            return 0
-        
-        if qLeft <= l  <= r <= qRight:
-            return self.tree[treeIdx]
-        
-        mid = (l + r) // 2
-        leftChildIdx = treeIdx * 2
-        rightChildIdx = treeIdx * 2 + 1
-
-        left = self._query(leftChildIdx, l, mid, qLeft, qRight)
-        right = self._query(rightChildIdx, mid + 1, r, qLeft, qRight)
-
-        return left + right
-    
-    def update(self, idx, val):
-        self._update(1, 0, self.n - 1, idx, val)
-    
-    def _update(self, treeIdx, l, r, idx, val):
-        if l == r:
-            self.tree[treeIdx] = val
-            return
-        
-        mid = (l + r) // 2
-        leftChildIdx = treeIdx * 2
-        rightChildIdx = treeIdx * 2 + 1
-
-        if idx <= mid:
-            self._update(leftChildIdx, l, mid, idx, val)
-        else:
-            self._update(rightChildIdx, mid + 1, r, idx, val)
-        
-        self.tree[treeIdx] = self.tree[leftChildIdx] + self.tree[rightChildIdx]
-                
+    def prefix(self, r, c):
+        i = r + 1
+        ans = 0
+        while i > 0:
+            j = c + 1
+            while j > 0:
+                ans += self.tree[i][j]
+                j -= j & -j
+            i -= i & -i
+        return ans
 
 
+    def query(self, r1, c1, r2, c2):
+        return self.prefix(r2, c2) - self.prefix(r1 - 1, c2) - self.prefix(r2, c1 - 1) + self.prefix(r1 - 1, c1 - 1)
 
 
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.m = len(matrix)
-        self.n = len(matrix[0])
-        self.trees = {}
-        for i, row in enumerate(matrix):
-            self.trees[i] = SegTree(row)
+        self.bitTree = BitTree(matrix)
+        self.matrix = matrix
 
     def update(self, row: int, col: int, val: int) -> None:
-        segTree = self.trees[row]
-        segTree.update(col, val)
+        
+        og = self.matrix[row][col]
+        delta = val - og
+        self.matrix[row][col] = val
+        self.bitTree.update(row, col, delta)
         
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        ans = 0
-        for r in range(row1, row2 + 1):
-            segTree = self.trees[r]
-            val = segTree.query(col1, col2)
-            ans += val
-        
-        return ans
-
-        
+        return self.bitTree.query(row1, col1, row2, col2)
 
 
 # Your NumMatrix object will be instantiated and called as such:
