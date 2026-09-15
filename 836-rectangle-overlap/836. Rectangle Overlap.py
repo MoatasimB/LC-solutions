c5bdcf5222fea95a0ba7bb0ddee876394d830d95
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-14 20:01:57 -0400

    Runtime: 0 ms (100.00%), Memory: 19.3 MB (18.98%) - Gitcode

--- 836-rectangle-overlap/836. Rectangle Overlap.py
+++ 836-rectangle-overlap/836. Rectangle Overlap.py
@@ -6,6 +6,16 @@ class Solution:
 
         r1, w1, r2, w2 = rec2
 
+        def intersect(p_left, p_right, q_left, q_right):
+            return min(p_right, q_right) > max(p_left, q_left)
+        
+        return (intersect(x1, x2, r1, r2) and # width > 0
+                intersect(y1, y2, w1, w2))    # height > 0
+
+            # min(x2, r2) > max(x1, r1)
+
+            #         x2, y2
+            # x1, y1
         #check if rectangle above
             # top y1 cord is less than bottom y2 cord 
 
