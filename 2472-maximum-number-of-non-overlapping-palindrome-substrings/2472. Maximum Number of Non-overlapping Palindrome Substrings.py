b91245abbccc80936c82fdef225dee8203f9475e
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-16 19:19:07 -0400

    Runtime: 2795 ms (21.05%), Memory: 50.7 MB (11.65%) - Gitcode

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472. Maximum Number of Non-overlapping Palindrome Substrings.py
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472. Maximum Number of Non-overlapping Palindrome Substrings.py
@@ -3,6 +3,28 @@ class Solution:
         
         n = len(s)
 
+        isPali = [[False] * n for _ in range(n)]
+
+        for length in range(1, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+
+                if s[i] == s[j]:
+                    if length <= 2 or isPali[i + 1][j - 1]:
+                        isPali[i][j] = True
+                    
+        dp = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            dp[i] = dp[i - 1]
+
+            for j in range(i - k + 1):
+                if isPali[j][i - 1]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return dp[n]
+
+
         def isPalindrome(l, r):
             while l <= r:
                 if s[l] != s[r]:
