d710cf0d9c26419c7d70865a484e2090f4e87448
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-16 02:19:58 -0400

    Runtime: 54 ms (37.27%), Memory: 31.7 MB (74.55%) - Gitcode

--- 1762-buildings-with-an-ocean-view/1762. Buildings With an Ocean View.py
+++ 1762-buildings-with-an-ocean-view/1762. Buildings With an Ocean View.py
@@ -2,20 +2,12 @@ class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
         
 
-        # 
-        #       #
-        #   #   #
-        #   #   #   #
-
-
-        #mono_dec
-
         stack = []
 
         for i, h in enumerate(heights):
             while stack and heights[stack[-1]] <= h:
                 stack.pop()
-            
-            stack.append(i)
 
+            stack.append(i)
+        
         return stack
