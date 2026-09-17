c0a0dcfe47581eb5f07ee5c7a71eb01d7f643269
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-17 00:10:42 -0400

    Runtime: 176 ms (47.48%), Memory: 42.5 MB (25.70%) - Gitcode

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477. Find Two Non-overlapping Sub-arrays Each With Target Sum.py
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477. Find Two Non-overlapping Sub-arrays Each With Target Sum.py
@@ -1,63 +1,20 @@
 class Solution:
     def minSumOfLengths(self, arr: List[int], target: int) -> int:
-        
-
-        #subarray {} [i..j] : length
-        # (i1, j1), (i2, j2)
-
-
-        subarrays = []
-
+        sums = {0 : -1}
         n = len(arr)
-        l = 0
+        dp = [float("inf")] * (n + 1)
         curr = 0
+        ans = float("inf")
         for i in range(n):
             curr += arr[i]
+            dp[i + 1] = dp[i]
+            if (curr - target) in sums:
+                #this is a valid subarray
+                idx = sums[curr - target]
+                length = i - idx
+                ans = min(ans, length + dp[idx + 1])
 
-            while curr > target:
-                curr -= arr[l]
-                l += 1
-            if curr == target:
-                subarrays.append((l, i, i - l + 1))
-        
-
-        subarrays.sort(key=lambda x: x[0])
-        m = len(subarrays)
-        if m == 1:
-            return -1
-        l = 0
-        r = m - 1
-
-        ans = float("inf")
-        print(subarrays)
-        while l < r:
-            s1, e1, l1 = subarrays[l]
-            s2, e2, l2 = subarrays[r]
-
-            if s2 <= e1:
-                l += 1
-                r -= 1
-            else:
-                ans = min(ans, l1 + l2)
-                if l1 > l2:
-                    l += 1
-                else:
-                    r -= 1
-        
-        l = 0
-        r = m - 1
-        while l < r:
-            s1, e1, l1 = subarrays[l]
-            s2, e2, l2 = subarrays[r]
-
-            if s2 <= e1:
-                l += 1
-                r -= 1
-            else:
-                ans = min(ans, l1 + l2)
-                if l1 >= l2:
-                    l += 1
-                else:
-                    r -= 1
+                dp[i + 1] = min(dp[i], length)
+            sums[curr] = i
         
         return ans if ans != float("inf") else -1
