6489be8cf7e3ab94314aa0c0ed80667686d56f43
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-17 22:42:53 -0400

    Runtime: 1085 ms (15.48%), Memory: 20.3 MB (70.80%) - Gitcode

---
+++ 1520-maximum-number-of-non-overlapping-substrings/1520. Maximum Number of Non-Overlapping Substrings.py
@@ -0,0 +1,48 @@
+class Solution:
+    def maxNumOfSubstrings(self, s: str) -> list[str]:
+        
+
+        substrings = set()
+        n = len(s)
+        first = {}
+        for i, ch in enumerate(s):
+            if ch not in first:
+                first[ch] = i
+        last = {ch: i for i, ch in enumerate(s)}
+
+        for ch in first:
+            begin = first[ch]
+            end = last[ch]
+            idx = begin
+            while idx < end:
+                letter = s[idx]
+                end = max(end, last[letter])
+                if first[letter] < begin:
+
+                    begin = min(begin, first[letter])
+                    idx = begin
+                    continue
+
+                idx += 1
+            substrings.add((begin, end, end - begin + 1))
+        
+        substrings.add((0, n - 1, n))
+        substrings = list(substrings)
+        substrings.sort(key=lambda x:(x[1], x[2]))
+        ans = []
+        
+        for start, end, length in substrings:
+            if not ans:
+                ans.append([start, end])
+            else:
+                prevS, prevE = ans[-1]
+                if start <= prevE:
+                    continue
+                ans.append([start, end])
+
+        
+        final = []
+        for start, end in ans:
+            final.append(s[start: end + 1])
+        
+        return final
