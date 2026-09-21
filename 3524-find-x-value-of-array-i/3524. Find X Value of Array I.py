96935a17fb30e17a5de68b4f4f694898bae1bbb0
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-21 00:52:32 -0400

    Runtime: 494 ms (21.05%), Memory: 46.3 MB (19.74%) - Gitcode

--- 3524-find-x-value-of-array-i/3524. Find X Value of Array I.py
+++ 3524-find-x-value-of-array-i/3524. Find X Value of Array I.py
@@ -5,16 +5,15 @@ class Solution:
         dp = [[0] * k for _ in range(n)]
 
         ans = [0] * k
-        dp[0][nums[0] % k] += 1
 
-        for i in range(1, n):
+        for i in range(n - 1):
             num = nums[i]
             rem = num % k
             dp[i][rem] += 1
 
             for j in range(k):
-                dp[i][(j * num) % k] += dp[i - 1][j]
-        
+                dp[i + 1][(j * nums[i + 1]) % k] += dp[i][j]
+        dp[n - 1][nums[n-1] % k] += 1
         for i in range(n):
             for j in range(k):
                 ans[j] += dp[i][j]
@@ -25,3 +24,4 @@ class Solution:
 
                 
 
+#dp[i][r] = #of subarrays ending at idx i that have remainder r
