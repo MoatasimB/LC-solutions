d2bc33133b9978d5892d53953259ea4738f56b6a
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-22 21:45:47 -0400

    Runtime: 88 ms (51.18%), Memory: 30.8 MB (71.36%) - Gitcode

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658. Minimum Operations to Reduce X to Zero.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minOperations(self, nums: list[int], x: int) -> int:
+   
+        n = len(nums)
+
+        l = 0
+        total = sum(nums)
+        if total == x:
+            return n
+        curr = 0
+        ans = float("inf")
+        for r in range(n):
+            curr += nums[r]
+            while l < r and total - curr < x:
+                curr -= nums[l]
+                l += 1
+            
+            if total - curr == x:
+                left = l
+                right = n - r - 1
+                ans = min(ans, left + right)
+        return ans if ans != float("inf") else -1
+
+
+
