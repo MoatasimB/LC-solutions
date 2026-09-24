4459f37f79dce944a3f5e91ed3b701d0fd667836
MoatasimB <89359264+MoatasimB@users.noreply.github.com>
2026-09-24 00:15:28 -0400

    Runtime: 202 ms (8.95%), Memory: 45.1 MB (51.72%) - Gitcode

--- 2050-parallel-courses-iii/2050. Parallel Courses III.py
+++ 2050-parallel-courses-iii/2050. Parallel Courses III.py
@@ -1,53 +1,52 @@
 class Solution:
-    def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
+    def minimumTime(self, n: int, relations: list[list[int]], time: list[int]) -> int:
         
-
+        if n == 1:
+            return time[0]
         graph = defaultdict(list)
-        indeg = [0] * n
-        earliest = [0] * n
-        for a, b in relations:
-            graph[a - 1].append(b - 1)
-            indeg[b - 1] += 1
-        
+        indeg = [0] * (n + 1)
+        timeForNode = defaultdict(int)
+        for i in range(len(relations)):
+            a, b = relations[i]
+            graph[a].append(b)
+            indeg[b] += 1
+            timeForNode[b] = max(timeForNode[b], time[a - 1])
+
         q = deque()
-        for i in range(n):
+        print(indeg)
+        print(time)
+        for i in range(1, n + 1):
             if indeg[i] == 0:
-                earliest[i] = time[i]
-                q.append([i, time[i]])
+                q.append([i, 0]) #node, time to this node
+        ans = 0
         
-        ans = max(time)
         while q:
-            node, curr = q.popleft()
-            
+            node, t = q.popleft()
+            timeOfNode = time[node - 1]
+            ans = max(ans, t +timeOfNode )
             for nei in graph[node]:
+                #time to neighbor will be max of time to prevNode + timeOfNode
                 indeg[nei] -= 1
-                earliest[nei] = max(earliest[nei], curr)
+                timeForNode[nei] = max(timeForNode[nei], t + timeOfNode)
                 if indeg[nei] == 0:
-                    earliest[nei] += time[nei]
-                    q.append([nei, earliest[nei]])
-
-        return max(earliest)
-
-
-
+                    q.append([nei, timeForNode[nei]])
+        return ans
+        # print(q)
+        # while q:
+        #     print("q", q)
         #     q_len = len(q)
-        #     levelTime = 0
-        #     neighbors = set()
+        #     max_t = 0
         #     for _ in range(q_len):
-        #         node, curr = q.popleft()
-        #         levelTime = max(levelTime, curr)
-
-        #         for nei in graph[node]:
+        #         course = q.popleft()
+        #         max_t = max(max_t, time[course - 1])
+        #         print("course", course)
+        #         for nei in graph[course]:
         #             indeg[nei] -= 1
-        #             neighbors.add(nei)
+        #             if indeg[nei] == 0:
+        #                 q.append(nei)
             
-        #     for nei in neighbors:
-        #         if indeg[nei] == 0:
-        #             q.append([nei, levelTime + time[nei]])
-        #             ans = max(ans, levelTime + time[nei])
-        # return ans
-
-
-        # #max time of my prereqs + my time = earliest time I can finish my course
-
+        #     ans += max_t
+        #     print(ans)
         
+        # return ans if sum(indeg) == 0 else -1
+
